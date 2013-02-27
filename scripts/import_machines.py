3c3cd3e5db82c82823dae6e2f43441b170385870
Steven Gregory <contact@steve-gregory.com>
2013-02-27 15:58:31 -0700

    Tags are imported in machines. Admin has been updated for some core models to be more useful

--- scripts/import_machines.py
+++ scripts/import_machines.py
@@ -2,15 +2,18 @@
 # Takes a single command-line argument, a .json file generated from `./manage.py dumpdata service_old.machine &> machines.json`
 import sys
 import json
-from core.models import Machine, Provider, ProviderMachine
+from core.models import Machine, Provider, ProviderMachine, Tag
 from django.contrib.auth.models import User
 from datetime import datetime
 from pytz import timezone
 
 
+#Tags are global, can be added to each machine
+machine_tag_map = {}
+
+
 def convert_old_machine(machine_obj):
     old_machine = machine_obj['fields']
-    print old_machine
     created_by = User.objects.filter(username=old_machine['image_ownerid'])
     if not created_by:
         created_by = User.objects.get(username='admin')
@@ -29,6 +32,13 @@ def convert_old_machine(machine_obj):
         'start_date': old_machine['registered_at'],
         'end_date': old_machine['image_end_date'],
     }
+    if machine_tag_map and old_machine['machine_tags']:
+        #Machine_tag_map contains OLD ID -> Name
+        #Get/Create a tag with the same name and apply it to the machine
+        new_machine.update({
+            'tags': [ Tag.objects.get_or_create(name=machine_tag_map[tag_id])[0] for tag_id in old_machine['machine_tags'] if machine_tag_map.has_key(tag_id)]
+        })
+
     new_provider_machine = {
         'identifier': old_machine['image_id']
     }
@@ -52,14 +62,18 @@ def update_provider_machine(provider_machine, machine_obj):
     machine.end_date = format_date(machine_obj['end_date'])
     machine.featured = machine_obj['featured']
     machine.private = machine_obj['private']
+    if machine_obj.has_key('tags'):
+        machine.tags = machine_obj['tags']
     machine.save()
-    if machine.featured:
-        print machine
 
 def main(filename):
+    global machine_tag_map
     f = open(filename, 'r')
     contents = f.read()
-    old_machines = json.loads(contents)
+    old_data = json.loads(contents)
+    machine_tag_map = {jObj['pk']:jObj['fields']['tag_name'] for jObj in old_data if jObj['model'] == 'service_old.machine_tag'}
+    #print [len(jObj['fields']['tag_name']) for jObj in machine_tag_map]
+    old_machines = [jObj for jObj in old_data if jObj['model'] == 'service_old.machine']
     new_machines = map(convert_old_machine, old_machines)
     # new_machines is now a list of two-tuples of the form (core.models.Machine, core.models.ProviderMachine)
 
@@ -69,7 +83,6 @@ def main(filename):
         #Does the ProviderMachine exist?
         if not machine_dict or not provider_machine_dict:
             continue
-        print machine_dict, provider_machine_dict
         provider_machine = ProviderMachine.objects.filter(identifier=provider_machine_dict['identifier'])
         if provider_machine:
             update_provider_machine(provider_machine[0], machine_dict)
