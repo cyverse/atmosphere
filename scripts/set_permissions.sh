e336a4ba47b57903bb184cae38f25b51dd2568c0
Steve Gregory <contact@steve-gregory.com>
2013-04-16 09:32:46 -0700

    Bugfix on volume serializer

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -10,8 +10,10 @@ chmod 755 ${ATMOSPHERE_HOME}/extras/ssh
 
 chmod -R 600 ${ATMOSPHERE_HOME}/extras/ssh/id_rsa
 
-chown -R www-data:core-services ${ATMOSPHERE_HOME}
+chown -R www-data:www-data ${ATMOSPHERE_HOME}
 
 chown -R root:root ${ATMOSPHERE_HOME}/extras/apache
+
+chown -R root:root ${ATMOSPHERE_HOME}/extras/ssh
 
 
