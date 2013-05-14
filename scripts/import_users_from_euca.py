cce5230466e20e3a69f8e24d422467b0d78b959c
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-05-14 11:30:33 -0700

    Removed references to OpenStack in import_users_from_euca script.

--- scripts/import_users_from_euca.py
+++ scripts/import_users_from_euca.py
@@ -2,23 +2,16 @@
 import time, requests
 
 from django.contrib.auth.models import User
-
 from threepio import logger
 
 from atmosphere import settings
-
 from service.accounts.eucalyptus import AccountDriver as EucaAccountDriver
-from service.accounts.openstack import AccountDriver as OSAccountDriver
-
 
 def main():
     """
     TODO: Add argparse, --delete : Deletes existing users in eucalyptus (Never use in PROD)
     """
     euca_driver = EucaAccountDriver()
-    os_driver = OSAccountDriver()
-    found = 0
-    create = 0
     all_users = euca_driver.list_users()
     for user_dict in all_users.values():
         #Here we don't call create_account to reduce
@@ -26,7 +19,6 @@ def main():
         euca_driver.create_identity(user_dict)
         #os_driver.create_account(user, admin_role=False)
     print "Total users added:%s" % len(all_users)
-
 
 if __name__ == "__main__":
     main()
