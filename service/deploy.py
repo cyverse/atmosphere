80c689ee30a9d875ebaa52366a28a21dd61abcd8
J. Matt Peterson <jmatt@jmatt.org>
2013-08-27 15:46:30 -0700

    PEP8 compliance. 	modified:   service/deploy.py

--- service/deploy.py
+++ service/deploy.py
@@ -13,24 +13,28 @@ from rtwo.drivers.common import LoggedScriptDeployment
 def sync_instance():
     return ScriptDeployment("sync", name="./deploy_sync_instance.sh")
 
+
 def get_distro(distro='ubuntu'):
     return ScriptDeployment("cat /etc/*-release",
-                             name="./deploy_get_distro.sh")
+                            name="./deploy_get_distro.sh")
+
 
 def build_script(script_input, name=None):
     return ScriptDeployment(script_input, name=name)
 
+
 def install_util_linux(distro='ubuntu'):
     return ScriptDeployment(
-        "%s install -qy utils-linux"\
-	 % 'apt-get' if 'ubuntu' in distro.to_lower() else 'yum',
-         name="./deploy_freeze_instance.sh")
-    
+        "%s install -qy utils-linux"
+        % 'apt-get' if 'ubuntu' in distro.to_lower() else 'yum',
+        name="./deploy_freeze_instance.sh")
+
 
 def freeze_instance(sleep_time=45):
     return ScriptDeployment(
         "fsfreeze -f / && sleep %s && fsfreeze -u /" % sleep_time,
         name="./deploy_freeze_instance.sh")
+
 
 def mount_volume(device, mount_location):
     return ScriptDeployment("mkdir -p %s\n" % (mount_location)
@@ -41,7 +45,6 @@ def mount_volume(device, mount_location):
 def check_mount():
     return ScriptDeployment("mount",
                             name="./deploy_check_mount.sh")
-    
 
 
 def check_volume(device):
@@ -52,6 +55,7 @@ def check_volume(device):
 def mkfs_volume(device):
     return ScriptDeployment("mkfs.ext3 %s" % (device),
                             name="./deploy_mkfs_volume.sh")
+
 
 def umount_volume(mount_location):
     return ScriptDeployment("umount %s" % (mount_location),
