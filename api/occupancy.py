b8855b7733f9402c229b51c18b43db674253e7b7
Steve Gregory <contact@steve-gregory.com>
2013-09-18 11:46:22 -0700

    Updated Occupancy API to work properly in multi-provider mode

--- api/occupancy.py
+++ api/occupancy.py
@@ -8,7 +8,7 @@ from rest_framework.response import Response
 
 from authentication.decorators import api_auth_token_required
 
-from core.models.identity import Identity
+from core.models.provider import Provider
 from core.models.size import convert_esh_size
 
 from api import get_esh_driver
@@ -25,8 +25,9 @@ class Occupancy(APIView):
         return occupancy data for the specific provider
         """
         #Get meta for provider to call occupancy
-        driver = get_esh_driver(Identity.objects.filter(
-            provider__id=provider_id)[0])
+        provider = Provider.objects.get(id=provider_id)
+        ident = provider.identity_set.all()[0]
+        driver = get_esh_driver(ident)
         meta_driver = driver.meta()
         esh_size_list  = meta_driver.occupancy()
         #Formatting..
