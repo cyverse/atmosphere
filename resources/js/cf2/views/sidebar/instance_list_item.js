a4f4f7e0d0432d85cd18bdcac3577f2ac2dd9991
Chris La Rose <cjlarose@iplantcollaborative.org>
2013-10-01 12:11:58 -0700

    Fixed instance sidebar update fail

--- resources/js/cf2/views/sidebar/instance_list_item.js
+++ resources/js/cf2/views/sidebar/instance_list_item.js
@@ -31,13 +31,13 @@ Atmo.Views.SidebarInstanceListItem = Backbone.View.extend({
 			// Don't want a huge, flashy re-render
 
 			// Update name or id, if needed
-			if (this.$el.find('.bd b').html() != this.model.get('name_or_id')) {
-				this.$el.find('.bd b').html(this.model.get('name_or_id'));
+			if (this.$el.find('.media-body b').html() != this.model.get('name_or_id')) {
+				this.$el.find('.media-body b').html(this.model.get('name_or_id'));
 			}
 
 			// Update IP address, if needed
-			if (this.$el.find('.bd span').html() != this.model.get('public_dns_name')) {
-				this.$el.find('.bd span').html(this.model.get('public_dns_name'));
+			if (this.$el.find('.media-body span').html() != this.model.get('public_dns_name')) {
+				this.$el.find('.media-body span').html(this.model.get('public_dns_name'));
 			}
 			
 			// Update running state if needed
@@ -227,7 +227,7 @@ Atmo.Views.SidebarInstanceListItem = Backbone.View.extend({
 			graph_bar.css('width', ''+percent+'%');
 		}
 		else {
-			this.$el.find('.bd').append($('<div>', {
+			this.$el.find('.media-body').append($('<div>', {
 				class: 'graphBar',
 				style: 'height: 16px'
 			}));
