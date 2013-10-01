d465d34d5db62f01ba8c2dfe8ed1941387c88afe
J. Matt Peterson <jmatt@jmatt.org>
2013-09-30 17:50:57 -0700

    Fix datetime in core.email. Ignore AWS providers in test instances. 	modified:   core/email.py 	modified:   core/tasks/instance.py

--- core/tasks/instance.py
+++ core/tasks/instance.py
@@ -24,7 +24,9 @@ def get_all_instances():
         identity_list = None
         try:
             identity_list = Identity.objects.filter(provider=provider)
-            if identity_list and provider.type.name != "":
+            if identity_list\
+               and provider.type.name != ""\
+               and provider.type.name != "Amazon EC2":
                 identity = identity_list[0]
                 driver = get_esh_driver(identity)
                 meta_driver = driver.provider.metaCls(driver)
