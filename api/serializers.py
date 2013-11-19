f80c42e11ccdc8cac74dfcd4c9e71e3252b1c84c
Steve Gregory <contact@steve-gregory.com>
2013-11-18 17:13:24 -0700

    Quick bugfix

--- api/serializers.py
+++ api/serializers.py
@@ -112,8 +112,7 @@ class InstanceHistorySerializer(serializers.ModelSerializer):
     start_date = serializers.DateTimeField(read_only=True)
     end_date = serializers.DateTimeField(read_only=True)
     active_time = serializers.DateTimeField(read_only=True, source='get_active_time')
-    provider = serializers.Field(read_only=True,
-                                 source='provider_machine__provider')
+    provider = serializers.Field(source='provider_machine__provider')
     #Writeable fields
     name = serializers.CharField()
     tags = TagRelatedField(slug_field='name', source='tags', many=True)
