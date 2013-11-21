74b372ad323f7a2bac53aa819bc525bdfa14fc78
Steve Gregory <contact@steve-gregory.com>
2013-11-20 17:23:08 -0700

    Phase 1 of test coverage for atmosphere APIs

--- service/tests.py
+++ service/tests.py
@@ -9,6 +9,7 @@ from service.accounts.openstack import AccountDriver as OSAccounts
 from service.accounts.eucalyptus import AccountDriver as EucaAccounts
 
 from core.models import ProviderCredential, ProviderType, Provider, Identity
+from core.tests import create_euca_provider, create_os_provider
 
 class ServiceTests(TestCase):
     '''
@@ -16,15 +17,16 @@ class ServiceTests(TestCase):
     '''
 
     def setUp(self):
-        euca_admin_id = self.create_euca_provider()
-        os_admin_id = self.create_os_provider()
+        self.euca_admin_id = create_euca_provider()
+        self.euca_provider = self.euca_admin_id.provider
+        self.os_admin_id = create_os_provider()
+        self.os_provider = self.os_admin_id.provider
 
     def tearDown(self):
         pass
 
     def test_openstack_account_creation(self):
-        provider = Provider.objects.get(location='OPENSTACK')
-        os_accounts = OSAccounts(provider)
+        os_accounts = OSAccounts(self.os_provider)
         new_identity = os_accounts.create_account('test_user', 'test_pass',
         'test_project')
         credentials = new_identity.credential_set.all()
@@ -46,14 +48,13 @@ class ServiceTests(TestCase):
         self.assertIsNone(
                 os_accounts.get_project('test_user'),
                 'A usergroup was created when project was specified')
-
         os_accounts.delete_account('test_user', 'test_project')
+        #Test account was deleted
 
     def test_eucalyptus_identity_creation(self):
         pass
         #username = 'estevetest03'
-        #provider = Provider.objects.get(location='EUCALYPTUS')
-        #accounts = EucaAccounts(provider)
+        #accounts = EucaAccounts(self.euca_provider)
         #user = accounts.get_user(username)
         #self.assertIsNotNone(
         #        user, 'Expected user %s exists in eucalyptus, got None' % username)
@@ -64,53 +65,3 @@ class ServiceTests(TestCase):
         #accounts.delete_identity(username)
         #self.assertEqual(1,1,"Account deleted succesfully")
 
-    def create_euca_provider(self):
-        provider_type = ProviderType.objects.get_or_create(name='Eucalyptus')[0]
-        euca = Provider.objects.get_or_create(location='EUCALYPTUS',
-                                              type=provider_type)[0]
-        ProviderCredential.objects.get_or_create(
-            key='ec2_url', value=settings.EUCA_EC2_URL, provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='s3_url', value=settings.EUCA_S3_URL, provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='euca_cert_path', value=settings.EUCALYPTUS_CERT_PATH,
-             provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='pk_path', value=settings.EUCA_PRIVATE_KEY,
-             provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='ec2_cert_path', value=settings.EC2_CERT_PATH,
-             provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='account_path', value='/services/Accounts',
-             provider=euca)
-        ProviderCredential.objects.get_or_create(
-            key='config_path', value='/services/Configuration',
-             provider=euca)
-        identity = Identity.create_identity('admin', euca.location,
-            account_admin=True,
-            cred_key=settings.EUCA_ADMIN_KEY,
-            cred_secret=settings.EUCA_ADMIN_SECRET)
-        return identity
-
-    def create_os_provider(self):
-        provider_type = ProviderType.objects.get_or_create(name='OpenStack')[0]
-        openstack = Provider.objects.get_or_create(
-                location='OPENSTACK',
-                type=provider_type)[0]
-        ProviderCredential.objects.get_or_create(key='auth_url',
-                value=settings.OPENSTACK_AUTH_URL, provider=openstack)
-        ProviderCredential.objects.get_or_create(key='admin_url',
-                value=settings.OPENSTACK_ADMIN_URL, provider=openstack)
-        ProviderCredential.objects.get_or_create(key='router_name',
-                value=settings.OPENSTACK_DEFAULT_ROUTER, provider=openstack)
-        ProviderCredential.objects.get_or_create(key='region_name',
-                value=settings.OPENSTACK_DEFAULT_REGION, provider=openstack)
-        identity = Identity.create_identity(
-            settings.OPENSTACK_ARGS['username'],
-            openstack.location, account_admin=True,
-            cred_key=settings.OPENSTACK_ADMIN_KEY,
-            cred_secret=settings.OPENSTACK_ADMIN_SECRET,
-            cred_ex_tenant_name=settings.OPENSTACK_ADMIN_TENANT,
-            cred_ex_project_name=settings.OPENSTACK_ADMIN_TENANT)
-        return identity
