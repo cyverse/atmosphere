25ed0755ea3a11a8e56b0c47d1621b35befe7005
Andre Mercer <amercer1@email.arizona.edu>
2014-01-14 12:27:43 -0700

    Fixed custom field value to string

--- core/fields.py
+++ core/fields.py
@@ -72,4 +72,4 @@ class VersionNumberField(models.Field):
 
     def value_to_string(self, obj):
         value = self._get_val_from_obj(obj)
-        return self.get_db_prep_value(value)
+        return self.get_prep_value(value)
