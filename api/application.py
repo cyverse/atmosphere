c988bd22daaafabd4245e2d9596126606528b00e
Steve Gregory <contact@steve-gregory.com>
2014-01-16 11:31:27 -0700

    Created new API endpoint for Applications, including reverse-lookup to providermachines using the application

---
+++ api/application.py
@@ -0,0 +1,32 @@
+
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from api.permissions import InMaintenance
+from api.serializers import ApplicationSerializer
+
+from core.models import Application
+
+class ApplicationListNoAuth(APIView):
+    """
+    Represents:
+        A Manager of Machine
+        Calls to the Machine Class
+    TODO: POST when we have programmatic image creation/snapshots
+    """
+
+    permission_classes = (InMaintenance,)
+    # NOTE-ABLY ABSENT.. API AUTH NOT REQUIRED FOR THIS LINE
+    #@api_auth_token_required
+    def get(self, request):
+        """
+        Using provider and identity, getlist of machines
+        TODO: Cache this request
+        """
+        applications = Application.objects.filter(private=False)
+        serialized_data = ApplicationSerializer(applications,
+                                                many=True).data
+        response = Response(serialized_data)
+        return response
--- core/models/application.py
+++ core/models/application.py
@@ -10,68 +10,80 @@ from core.models.tag import Tag, updateTags
 from core.metadata import _get_admin_owner
 
 class Application(models.Model):
-  """
-  An application is a collection of providermachines, where each
-  providermachine represents a single revision, together forming a linear
-  sequence of versions. The created_by field here is used for logging only;
-  do not rely on it for permissions; use ApplicationMembership instead.
-  """
-  uuid = models.CharField(max_length=36, unique=True)
-  name = models.CharField(max_length=256)
-  description = models.TextField(null=True, blank=True)
-  tags = models.ManyToManyField(Tag, blank=True)
-  icon = models.ImageField(upload_to="machine_images", null=True, blank=True)
-  private = models.BooleanField(default=False)
-  featured = models.BooleanField(default=False)
-  start_date = models.DateTimeField(default=timezone.now)
-  end_date = models.DateTimeField(null=True, blank=True)
-  # User/Identity that created the application object
-  created_by = models.ForeignKey('AtmosphereUser')
-  created_by_identity = models.ForeignKey(Identity, null=True)
-  
-  def update(self, *args, **kwargs):
-      """
-      Allows for partial updating of the model
-      """
-      #Upload args into kwargs
-      for arg in args:
-          for (key, value) in arg.items():
-              kwargs[key] = value
-      #Update the values
-      for key in kwargs.keys():
-          if key == 'tags':
-              if type(kwargs[key]) != list:
-                  tags_list = kwargs[key].split(",")
-              else:
-                  tags_list = kwargs[key]
-              updateTags(self, tags_list)
-              continue
-          setattr(self, key, kwargs[key])
-      self.save()
-      return self
+    """
+    An application is a collection of providermachines, where each
+    providermachine represents a single revision, together forming a linear
+    sequence of versions. The created_by field here is used for logging only;
+    do not rely on it for permissions; use ApplicationMembership instead.
+    """
+    uuid = models.CharField(max_length=36, unique=True)
+    name = models.CharField(max_length=256)
+    description = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+    icon = models.ImageField(upload_to="machine_images", null=True, blank=True)
+    private = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(null=True, blank=True)
+    # User/Identity that created the application object
+    created_by = models.ForeignKey('AtmosphereUser')
+    created_by_identity = models.ForeignKey(Identity, null=True)
 
-  def __unicode__(self):
-      return "%s" % (self.name,)
+    def icon_url(self):
+        return self.icon.url if self.icon else None
 
-  class Meta:
-      db_table = 'application'
-      app_label = 'core'
+    def get_provider_machines(self):
+        pms = self.providermachine_set.all()
+        return [{
+            "start_date":pm.start_date,
+            "end_date":pm.end_date,
+            "alias":pm.identifier,
+            "version":pm.version,
+            "provider":pm.provider.id} for pm in pms]
+
+    def update(self, *args, **kwargs):
+        """
+        Allows for partial updating of the model
+        """
+        #Upload args into kwargs
+        for arg in args:
+            for (key, value) in arg.items():
+                kwargs[key] = value
+        #Update the values
+        for key in kwargs.keys():
+            if key == 'tags':
+                if type(kwargs[key]) != list:
+                    tags_list = kwargs[key].split(",")
+                else:
+                    tags_list = kwargs[key]
+                updateTags(self, tags_list)
+                continue
+            setattr(self, key, kwargs[key])
+        self.save()
+        return self
+
+    def __unicode__(self):
+        return "%s" % (self.name,)
+
+    class Meta:
+        db_table = 'application'
+        app_label = 'core'
 
 
 class ApplicationMembership(models.Model):
-  """
-  Members of a private image can view & launch its respective machines. If the
-  can_modify flag is set, then members also have ownership--they can make
-  changes. The unique_together field ensures just one of those states is true.
-  """
-  application = models.ForeignKey(Application)
-  group = models.ForeignKey('Group')
-  can_edit = models.BooleanField(default=False)
+    """
+    Members of a private image can view & launch its respective machines. If the
+    can_modify flag is set, then members also have ownership--they can make
+    changes. The unique_together field ensures just one of those states is true.
+    """
+    application = models.ForeignKey(Application)
+    group = models.ForeignKey('Group')
+    can_edit = models.BooleanField(default=False)
 
-  class Meta:
-      db_table = 'application_membership'
-      app_label = 'core'
-      unique_together = ('application', 'group')
+    class Meta:
+        db_table = 'application_membership'
+        app_label = 'core'
+        unique_together = ('application', 'group')
 
 
 def get_application(identifier, app_uuid=None):
