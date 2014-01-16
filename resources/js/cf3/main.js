b945ce5b4c37fc9f0ad32504c26c428321126e7c
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-01-16 10:07:18 -0700

    Update backbone 1.1.0

--- resources/js/cf3/main.js
+++ resources/js/cf3/main.js
@@ -2,7 +2,7 @@ require.config({
     baseUrl: '/resources/js/cf3',
     paths: {
         'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery',
-        'backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.9/backbone-min',
+        'backbone': '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min',
         'underscore': '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.3/underscore-min',
         'google': 'https://www.google.com/jsapi',
         'bootstrap': '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/js/bootstrap.min',
