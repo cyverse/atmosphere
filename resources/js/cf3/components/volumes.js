3c2068d023dfb80566595c4ce61e6580c007390a
Chris La Rose <cjlarose@iplantcollaborative.org>
2014-01-23 11:46:56 -0700

    Volume list styling

--- resources/js/cf3/components/volumes.js
+++ resources/js/cf3/components/volumes.js
@@ -1,19 +1,59 @@
 define(['react', 'components/identity_select', 'backbone', 'utils', 
-    'components/page_header'], 
-    function(React, IdentitySelect, Backbone, Utils, PageHeader) {
+    'components/page_header', 'components/relative_time', 'components/glyphicon'],
+    function(React, IdentitySelect, Backbone, Utils, PageHeader, RelativeTime, Glyphicon) {
+
+    var Volume = React.createClass({
+        render: function() {
+            var attach_info = [];
+            if (this.props.volume.get('status') == 'attaching')
+                attach_info = "Attaching";
+            else if (this.props.volume.get('status') == 'detaching')
+                attach_info = "Detaching";
+            else if (this.props.volume.get('status') == 'in-use')
+                attach_info = [
+                    "Device location: ",
+                    this.props.volume.get('attach_data').device,
+                    React.DOM.br(),
+                    "Attached : ",
+                    RelativeTime({date: this.props.volume.get('attach_data').attachTime})
+                ];
+
+
+            return React.DOM.li({},
+                Glyphicon({name: 'hdd'}),
+                React.DOM.strong({}, 
+                    this.props.volume.get('name_or_id')
+                ),
+                " (" + this.props.volume.get('size') + " GB)",
+                React.DOM.br(),
+                "Created: ",
+                RelativeTime({date: this.props.volume.get('create_time')}),
+                React.DOM.br(),
+                attach_info
+            );
+        }
+    });
 
     var VolumeList = React.createClass({
         render: function() {
+            console.log(this.props.volumes);
             var list_items = this.props.volumes.map(function(volume) {
-                return React.DOM.li({},
-                    volume.get('name_or_id') + " (" + volume.get('size') + " GB)",
-                    "Created: " + volume.get('start_date'));
-            }.bind(this));
+                return Volume({volume: volume})
+            });
 
             if (list_items.length)
                 return React.DOM.ul({id: 'volume-list'}, list_items);
             else
                 return React.DOM.div({}, "No volumes");
+        }
+    });
+
+    var Button = React.createClass({
+        getDefaultProps: function() {
+            return {type: 'default'};
+        },
+        render: function() {
+            return React.DOM.a({className: 'btn btn-' + this.props.type}, this.props.children);
         }
     });
 
@@ -37,6 +77,7 @@ define(['react', 'components/identity_select', 'backbone', 'utils',
             return React.DOM.div({style: {display: this.props.visible ? 'block' : 'none'}},
                 PageHeader({title: "Volumes"}),
                 IdentitySelect({identities: this.props.profile.get('identities'), onSelect: this.onSelect}),
+                Button({onClick: this.showCreateVolumeModal, type: 'primary'}, "Create Volume"),
                 //React.DOM.h2({}, "Provider " + this.state.identity.get('provider_id') + ", Identity " + this.state.identity.get('id')),
                 VolumeList({volumes: this.state.identity.get('volumes')})
             );
