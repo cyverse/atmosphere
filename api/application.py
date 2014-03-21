8da1e99fc839a4ed78f8313022a2b5896626affe
Steve Gregory <contact@steve-gregory.com>
2014-03-21 13:02:23 -0700

    Forgot to add /api/applciation file

--- api/application.py
+++ api/application.py
@@ -5,12 +5,14 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from core.models import Application
+from core.models.application import visible_applications, public_applications
 
+from authentication.decorators import api_auth_token_optional
 from api.permissions import InMaintenance
 from api.serializers import ApplicationSerializer
 
 
-class ApplicationListNoAuth(APIView):
+class ApplicationList(APIView):
     """
     Represents:
         A Manager of Machine
@@ -19,14 +21,19 @@ class ApplicationListNoAuth(APIView):
     """
 
     permission_classes = (InMaintenance,)
-    # NOTE-ABLY ABSENT.. API AUTH NOT REQUIRED FOR THIS LINE
-    #@api_auth_token_required
-    def get(self, request):
+
+    @api_auth_token_optional
+    def get(self, request, **kwargs):
         """
         Using provider and identity, getlist of machines
         TODO: Cache this request
         """
-        applications = Application.objects.filter(private=False)
+        request_user = kwargs.get('request_user')
+        applications = public_applications()
+        #Concatenate 'visible'
+        if request_user:
+            my_apps = visible_applications(request_user)
+            applications.extend(my_apps)
         serialized_data = ApplicationSerializer(applications,
                                                 many=True).data
         response = Response(serialized_data)
