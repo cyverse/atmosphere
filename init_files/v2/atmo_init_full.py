cbad0197e19a28e949b734cb1f77b8ae29179563
Steve Gregory <contact@steve-gregory.com>
2014-03-21 13:25:30 -0700

    Swapped vncserver test to /usr/bin/X instead of /usr/bin/xterm

--- init_files/v2/atmo_init_full.py
+++ init_files/v2/atmo_init_full.py
@@ -439,7 +439,7 @@ def running_process(proc_name, user=None):
 
 def vnc(user, distro, license=None):
     try:
-        if not os.path.isfile('/usr/bin/xterm'):
+        if not os.path.isfile('/usr/bin/X'):
             logging.debug("Could not find a GUI on this machine, "
                           "Skipping VNC Install.")
             return
