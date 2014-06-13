e1c9966f61d365c045a0ce69dd25e35b51136e78
Steve Gregory <contact@steve-gregory.com>
2014-06-13 10:41:45 -0700

    Added VolumeSnapshot to API\n\nall CRUD operations available.

--- api/volume.py
+++ api/volume.py
@@ -23,6 +23,143 @@ from api.permissions import InMaintenance, ApiAuthRequired
 from api.serializers import VolumeSerializer
 
 
+class VolumeSnapshot(APIView):
+    """
+    Initialize and view volume snapshots
+    """
+    permission_classes = (ApiAuthRequired,)
+    
+    def get(self, request, provider_id, identity_id):
+        """
+        """
+        user = request.user
+        esh_driver = prepare_driver(request, provider_id, identity_id)
+        if not esh_driver:
+            return invalid_creds(provider_id, identity_id)
+        esh_snapshots = esh_driver._connection.ex_list_snapshots()
+        snapshot_data = []
+        for ss in esh_snapshots:
+            snapshot_data.append({
+                'id': ss.id,
+                'name': ss.extra['name'],
+                'size': ss.size,
+                'description': ss.extra['description'],
+                'created': ss.extra['created'],
+                'status': ss.extra['status'],
+                'volume_id': ss.extra['volume_id'],})
+
+        response = Response(snapshot_data)
+        return response
+
+    def post(self, request, provider_id, identity_id):
+        """
+        Updates DB values for volume
+        """
+        user = request.user
+        data = request.DATA
+
+        missing_keys = valid_snapshot_post_data(data)
+        if missing_keys:
+            return keys_not_found(missing_keys)
+        #Required
+        size = data.get('size')
+        volume_id = data.get('volume_id')
+        display_name = data.get('display_name')
+        #Optional
+        description = data.get('description')
+        metadata = data.get('metadata')
+        snapshot_id = data.get('snapshot_id')
+
+        #STEP 0 - Existence tests
+        esh_driver = prepare_driver(request, provider_id, identity_id)
+        if not esh_driver:
+            return invalid_creds(provider_id, identity_id)
+        esh_volume = esh_driver.get_volume(volume_id)
+        #TODO: Put quota tests at the TOP so we dont over-create resources!
+        #STEP 1 - Reuse/Create snapshot
+        if snapshot_id:
+            snapshot = esh_driver._connection.get_snapshot(snapshot_id)
+            if not snapshot:
+                return failure_response(
+                    status.HTTP_400_BAD_REQUEST,
+                    "Snapshot %s not found. Process aborted."
+                    % snapshot_id)
+        else:
+            #Normal flow, create a snapshot from the volume
+            if not esh_volume:
+                return volume_not_found(volume_id)
+            if esh_volume.extra['status'].lower() != 'available':
+               return failure_response(
+                       status.HTTP_400_BAD_REQUEST,
+                       "Volume status must be 'available'. "
+                       "Did you detach the volume?")
+
+            snapshot = esh_driver._connection.ex_create_snapshot(
+                    esh_volume, display_name, description)
+            if not snapshot:
+                return failure_response(
+                    status.HTTP_400_BAD_REQUEST,
+                    "Snapshot not created. Process aborted.")
+        #STEP 2 - Create volume from snapshot
+        try:
+            success, esh_volume = create_volume(esh_driver, identity_id,
+                                                display_name, size,
+                                                description, metadata,
+                                                snapshot=snapshot)
+            if not success:
+                return failure_response(
+                    status.HTTP_500_INTERNAL_SERVER_ERROR,
+                    'Volume creation failed. Contact support')
+            # Volume creation succeeded
+            core_volume = convert_esh_volume(esh_volume, provider_id,
+                                             identity_id, user)
+            serialized_data = VolumeSerializer(core_volume,
+                                               context={'request':request}).data
+            return Response(serialized_data, status=status.HTTP_201_CREATED)
+        except OverQuotaError, oqe:
+            return over_quota(oqe)
+        except InvalidCredsError:
+            return invalid_creds(provider_id, identity_id)
+
+class VolumeSnapshotDetail(APIView):
+    """Details of specific volume on Identity."""
+    permission_classes = (ApiAuthRequired,)
+    
+    def get(self, request, provider_id, identity_id, snapshot_id):
+        """
+        """
+        user = request.user
+        esh_driver = prepare_driver(request, provider_id, identity_id)
+        if not esh_driver:
+            return invalid_creds(provider_id, identity_id)
+        snapshot = esh_driver._connection.get_snapshot(snapshot_id)
+        if not snapshot:
+            return snapshot_not_found(snapshot_id)
+        response = Response(snapshot)
+        return response
+
+    def delete(self, request, provider_id, identity_id, snapshot_id):
+        """
+        Destroys the volume and updates the DB
+        """
+        user = request.user
+        #Ensure volume exists
+        esh_driver = prepare_driver(request, provider_id, identity_id)
+        if not esh_driver:
+            return invalid_creds(provider_id, identity_id)
+        snapshot = esh_driver._connection.get_snapshot(snapshot_id)
+        if not snapshot:
+            return snapshot_not_found(snapshot_id)
+        delete_success = esh_driver._connection.ex_delete_snapshot(snapshot)
+        #NOTE: Always false until icehouse...
+        #if not delete_success:
+        #    return failure_response(
+        #        status.HTTP_400_BAD_REQUEST,
+        #        "Failed to delete snapshot %s. Please try again later."
+        #        % snapshot_id)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class VolumeList(APIView):
     """List all volumes on Identity"""
 
@@ -48,7 +185,7 @@ class VolumeList(APIView):
                                                identity_id, user)
                             for volume in esh_volume_list]
         serializer = VolumeSerializer(core_volume_list,
-                                      context={'user':request.user}, many=True)
+                                      context={'request':request}, many=True)
         response = Response(serializer.data)
         return response
 
@@ -61,7 +198,7 @@ class VolumeList(APIView):
         if not esh_driver:
             return invalid_creds(provider_id, identity_id)
         data = request.DATA
-        missing_keys = valid_post_data(data)
+        missing_keys = valid_volume_post_data(data)
         if missing_keys:
             return keys_not_found(missing_keys)
         #Pass arguments
@@ -83,7 +220,7 @@ class VolumeList(APIView):
         core_volume = convert_esh_volume(esh_volume, provider_id,
                                          identity_id, user)
         serialized_data = VolumeSerializer(core_volume,
-                                           context={'user':request.user}).data
+                                           context={'request':request}).data
         return Response(serialized_data, status=status.HTTP_201_CREATED)
 
 
@@ -104,7 +241,7 @@ class Volume(APIView):
         core_volume = convert_esh_volume(esh_volume, provider_id,
                                          identity_id, user)
         serialized_data = VolumeSerializer(core_volume,
-                                           context={'user':request.user}).data
+                                           context={'request':request}).data
         response = Response(serialized_data)
         return response
 
@@ -124,7 +261,7 @@ class Volume(APIView):
         core_volume = convert_esh_volume(esh_volume, provider_id,
                                          identity_id, user)
         serializer = VolumeSerializer(core_volume, data=data, 
-                                      context={'user':request.user},
+                                      context={'request':request},
                 
                 partial=True)
         if serializer.is_valid():
@@ -152,7 +289,7 @@ class Volume(APIView):
         core_volume = convert_esh_volume(esh_volume, provider_id,
                                          identity_id, user)
         serializer = VolumeSerializer(core_volume, data=data,
-                                      context={'user':request.user},
+                                      context={'request':request},
                 
                 )
         if serializer.is_valid():
@@ -184,27 +321,42 @@ class Volume(APIView):
         core_volume.save()
         #Return the object
         serialized_data = VolumeSerializer(core_volume,
-                                           context={'user':request.user},
+                                           context={'request':request},
                 
                 ).data
         response = Response(serialized_data)
         return response
 
 
-def valid_post_data(data):
+def valid_snapshot_post_data(data):
+    """
+    Return any missing required post key names.
+    """
+    required = ['display_name', 'volume_id', 'size']
+    return [key for key in required
+            #Key must exist and have a non-empty value.
+            if key not in data or (type(data[key]) == str and len(data[key]) > 0)]
+
+def valid_volume_post_data(data):
     """
     Return any missing required post key names.
     """
     required = ['name', 'size']
     return [key for key in required
             #Key must exist and have a non-empty value.
-            if not ( key in data and len(data[key]) > 0)]
+            if key not in data or (type(data[key]) == str and len(data[key]) > 0)]
 
 
 def keys_not_found(missing_keys):
     return failure_response(
         status.HTTP_400_BAD_REQUEST,
         'Missing required POST datavariables : %s' % missing_keys)
+
+
+def snapshot_not_found(snapshot_id):
+    return failure_response(
+        status.HTTP_404_NOT_FOUND,
+        'Snapshot %s does not exist' % snapshot_id)
 
 
 def volume_not_found(volume_id):
--- service/volume.py
+++ service/volume.py
@@ -4,17 +4,20 @@ from service.exceptions import OverQuotaError
 from core.models.identity import Identity
 
 
-def create_volume(esh_driver, identity_id, name, size, description=None):
+def create_volume(esh_driver, identity_id, name, size, description=None,
+        metadata=None, snapshot=None):
     identity = Identity.objects.get(id=identity_id)
     quota = get_quota(identity_id)
     if not has_storage_quota(esh_driver, quota, size):
         raise OverQuotaError(
-                "Maximum total size of Storage Volumes Exceeded")
+                message="Maximum total size of Storage Volumes Exceeded")
     if not has_storage_count_quota(esh_driver, quota, 1):
         raise OverQuotaError(
-                "Maximum # of Storage Volumes Exceeded")
+                message="Maximum # of Storage Volumes Exceeded")
     success, esh_volume = esh_driver.create_volume(
-        name=name,
         size=size,
-        description=description)
+        name=name,
+        description=description,
+        metadata=metadata,
+        snapshot=snapshot)
     return success, esh_volume
