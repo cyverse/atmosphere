2f0586f1c6edb5aa8a0750a073c0b7989552c5f9
J. Matt Peterson <jmatt@jmatt.org>
2014-06-18 16:36:26 -0700

    Fix feedback post. Consolidate and refactor. PEP8 Compliance. 	modified:   api/email.py

--- api/email.py
+++ api/email.py
@@ -1,6 +1,7 @@
 """
 Atmosphere api email
 """
+from django.core import urlresolvers
 from django.utils.timezone import datetime
 
 from rest_framework.views import APIView
@@ -11,9 +12,10 @@ from libcloud.common.types import InvalidCredsError
 
 from threepio import logger
 
-from api.permissions import InMaintenance, ApiAuthRequired
 from authentication.protocol.ldap import lookupEmail
 
+from core.email import email_admin
+from core.models.group import IdentityMembership
 from core.models.provider import AccountProvider
 from core.models.volume import convert_esh_volume
 
@@ -21,29 +23,51 @@ from service.volume import create_volume
 from service.exceptions import OverQuotaError
 
 from api import prepare_driver, failure_response, invalid_creds
+from api.permissions import InMaintenance, ApiAuthRequired
 
-from web.emails import feedback_email, quota_request_email, support_email
 
 class Feedback(APIView):
     """
     Post feedback via RESTful API
     """
+
     permission_classes = (ApiAuthRequired,)
+
     def post(self, request):
         """
-        Creates a new feedback email and sends it to admins
+        Creates a new feedback email and sends it to admins.
         """
-        data = request.DATA
-        required = ['message',]
-        missing_keys = valid_post_data(data, required)
+        required = ["message",]
+        missing_keys = check_missing_keys(request.DATA, required)
         if missing_keys:
             return keys_not_found(missing_keys)
-        #Pass arguments
-        user = request.user
-        message = data['message']
-        user_email = lookupEmail(user.username)
-        result = feedback_email(request, user.username, user_email, message)
+        result = self._email(request,
+                             request.user.username,
+                             lookupEmail(request.user.username),
+                             request.DATA["message"])
         return Response(result, status=status.HTTP_201_CREATED)
+
+    def _email(self, request, username, user_email, message):
+        """
+        Sends an email Bto support based on feedback from a client machine
+
+        Returns a response.
+        """
+        subject = "Subject: Atmosphere Client Feedback from %s" % username
+        message = "---\nFeedback: %s\n---" % message
+        email_success = email_admin(request, subject, message)
+        if email_success:
+            resp = {"result":
+                    {"code": "success",
+                     "meta": "",
+                     "value": "Thank you for your feedback! "
+                     + "Support has been notified."}}
+        else:
+            resp = {"result":
+                    {"code": "failed",
+                     "meta": "",
+                     "value": "Failed to send feedback!"}}
+        return resp
 
 
 class QuotaEmail(APIView):
@@ -55,17 +79,38 @@ class QuotaEmail(APIView):
         """
         Creates a new Quota Request email and sends it to admins
         """
-        data = request.DATA
-        required = ['quota', 'reason']
-        missing_keys = valid_post_data(data, required)
+        required = ["quota", "reason"]
+        missing_keys = check_missing_keys(request.DATA, required)
         if missing_keys:
             return keys_not_found(missing_keys)
-        #Pass arguments
-        username = request.user.username
-        quota = data['quota']
-        reason = data['reason']
-        result = quota_request_email(request, username, quota, reason)
+        result = self._email(request,
+                             request.user.username,
+                             request.DATA["quota"],
+                             request.DATA["reason"])
         return Response(result, status=status.HTTP_201_CREATED)
+
+    def _email(self, request, username, new_quota, reason):
+        """
+        Processes Increase Quota request. Sends email to atmo@iplantc.org
+
+        Returns a response.
+        """
+        user = User.objects.get(username=username)
+        membership = IdentityMembership.objects.get(identity=user.select_identity(),
+                                                    member__in=user.group_set.all())
+        admin_url = urlresolvers.reverse("admin:core_identitymembership_change",
+                                         args=(membership.id,))
+        message = """
+        Username : %s
+        Quota Requested: %s
+        Reason for Quota Increase: %s
+        URL for Quota Increase:%s
+        """ % (username, new_quota, reason, 
+               request.build_absolute_uri(admin_url))
+        subject = "Atmosphere Quota Request - %s" % username
+        logger.info(message)
+        email_success = email_admin(request, subject, message, cc_user=False)
+        return {"email_sent": email_success}
 
 
 class SupportEmail(APIView):
@@ -75,33 +120,44 @@ class SupportEmail(APIView):
 
     def post(self, request):
         """
-        Creates a new support email and sends it to admins
-
+        Creates a new support email and sends it to admins.
 
         Post Support Email via RESTful API
         """
-        data = request.DATA
-        required = ['message','subject']
-        missing_keys = valid_post_data(data, required)
+        required = ["message", "subject"]
+        missing_keys = check_missing_keys(request.DATA, required)
         if missing_keys:
             return keys_not_found(missing_keys)
-        #Pass arguments
-        subject = data['subject']
-        message = data['message']
-        result = support_email(request, subject, message)
+        result = self._email(request,
+                             request.DATA["subject"],
+                             request.DATA["message"])
         return Response(result, status=status.HTTP_201_CREATED)
 
+    def _email(self, request, subject, message):
+        """
+        Sends an email to support.
 
-def valid_post_data(data, required_keys):
+        POST Params expected:
+        * user
+        * message
+        * subject
+
+        Returns a response.
+        """
+        email_success = email_admin(request, subject, message)
+        return {"email_sent": email_success}
+
+
+def check_missing_keys(data, required_keys):
     """
     Return any missing required post key names.
     """
-    return [key for key in required
+    return [key for key in required_keys
             #Key must exist and have a non-empty value.
-            if not ( key in data and len(data[key]) > 0)]
+            if not (key in data and len(data[key]) > 0)]
 
 
 def keys_not_found(missing_keys):
     return failure_response(
         status.HTTP_400_BAD_REQUEST,
-        'Missing required POST data variables : %s' % missing_keys)
+        "Missing required POST data variables : %s" % missing_keys)
