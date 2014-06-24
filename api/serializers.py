51bec79012258a4ec008de4a38d5d5e782319470
Steve Gregory <contact@steve-gregory.com>
2014-06-23 17:30:02 -0700

    Provider machines should be ignored if their providers are disabled.

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import AnonymousUser
 
-from core.models import only_current
 from core.models.application import Application, ApplicationScore,\
         ApplicationBookmark
 from core.models.credential import Credential
 from core.models.group import get_user_group
+from core.models.group import Group
 from core.models.group import IdentityMembership
 from core.models.identity import Identity
 from core.models.instance import Instance
@@ -15,12 +15,12 @@ from core.models.maintenance import MaintenanceRecord
 from core.models.profile import UserProfile
 from core.models.project import Project
 from core.models.provider import ProviderType, Provider
+from core.models.query import only_current
 from core.models.size import Size
 from core.models.step import Step
 from core.models.tag import Tag, find_or_create_tag
 from core.models.user import AtmosphereUser
 from core.models.volume import Volume
-from core.models.group import Group
 
 from rest_framework import serializers
 
