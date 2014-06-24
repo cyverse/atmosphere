6d932bb064378e6807fad5b5b767e3c7760263b3
J. Matt Peterson <jmatt@jmatt.org>
2014-06-23 19:56:36 -0700

    Move query out of the models directory, since it's not a model.
    Refactor other references to it.
    
    	modified:   api/serializers.py
    	modified:   core/models/application.py
    	renamed:    core/models/query.py -> core/query.py

--- core/query.py
+++ core/query.py
@@ -1,7 +1,7 @@
 from django.db.models import Q
 from django.utils import timezone
 
-def only_current():
+def only_active():
     """
     Use this query on any model with 'end_date'
     to limit the objects to those
