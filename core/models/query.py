da768ec8bb29edd5c4891ba4ea5474f6698e43e5
Steve Gregory <contact@steve-gregory.com>
2014-06-23 17:56:07 -0700

    Add missing import 'core.models.query'

---
+++ core/models/query.py
@@ -0,0 +1,11 @@
+from django.db.models import Q
+from django.utils import timezone
+
+def only_current():
+    """
+    Use this query on any model with 'end_date'
+    to limit the objects to those
+    that have not past their end_date
+    """
+    return Q(end_date=None) | Q(end_date__gt=timezone.now())
+
