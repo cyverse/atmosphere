313b19ddd07453cc667442a04740ca3545bfda3e
Steve Gregory <contact@steve-gregory.com>
2014-07-03 09:30:29 -0700

    Bad varname

--- service/tasks/driver.py
+++ service/tasks/driver.py
@@ -648,7 +648,7 @@ def check_process_task(driverCls, provider, identity,
         logger.debug("check_process_task finished at %s." % datetime.now())
     except Instance.DoesNotExist:
         logger.warn("check_process_task failed: Instance %s no longer exists"
-                    % instance_id)
+                    % instance_alias)
     except Exception as exc:
         logger.exception(exc)
         check_process_task.retry(exc=exc)
