c38bcc80ee35b4d785497199b6872990daf9c626
Steve Gregory <contact@steve-gregory.com>
2014-07-07 10:07:11 -0700

    Remove 'email_hash' from profile

--- api/serializers.py
+++ api/serializers.py
@@ -526,7 +526,7 @@ class ProfileSerializer(serializers.ModelSerializer):
     """
     #TODO:Need to validate provider/identity membership on id change
     username = serializers.CharField(read_only=True, source='user.username')
-    email = serializers.CharField(read_only=True, source='email_hash')
+    email = serializers.CharField(read_only=True, source='user.email')
     groups = serializers.CharField(read_only=True, source='user.groups.all')
     is_staff = serializers.BooleanField(source='user.is_staff')
     is_superuser = serializers.BooleanField(source='user.is_superuser')
