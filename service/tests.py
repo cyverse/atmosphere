6a9ea5b1c0668bcf06beab2730d15fd7fe698ec5
Steve Gregory <contact@steve-gregory.com>
2014-08-27 12:27:18 -0700

    Updated apache conf to avoid 502 Proxy Errors

--- service/tests.py
+++ service/tests.py
@@ -15,57 +15,34 @@ from core.tests import create_euca_provider, create_os_provider
 class ServiceTests(TestCase):
     '''
     Test service.*
+
+    Methods to write:
+    -----------------
+    Instance Launch:
+    * Identity_over_quota
+    * invalid_provider_size
+    * invalid_provider_machine
+    * instance_launch_success
+    * instance_launch_fail_*
+
+    Instance Volume Attach:
+    * Instance_terminated
+    * Instance_suspended
+    * Attach_failed
+    * check_volume_failed
+    * mount_failed
+
+    Instance Action tests:
+    * suspend success
+    * stop success
+    * reboot success
+    * terminate
+
     '''
 
     def setUp(self):
-        self.euca_admin_id = create_euca_provider()
-        self.euca_provider = self.euca_admin_id.provider
-        self.os_admin_id = create_os_provider()
-        self.os_provider = self.os_admin_id.provider
+        pass
 
     def tearDown(self):
         pass
 
-    #def test_openstack_account_creation(self):
-    #    os_accounts = OSAccounts(self.os_provider)
-    #    new_identity = os_accounts.create_account(
-    #            'test_user', 'test_pass', 'test_project')
-    #    credentials = new_identity.credential_set.all()
-    #    self.assertEqual(
-    #            credentials.get(key='key').value, 'test_user',
-    #            'Key credential does not match username')
-    #    self.assertEqual(
-    #            credentials.get(key='secret').value, 'test_pass',
-    #            'Secret credential does not match password')
-    #    self.assertEqual(
-    #            credentials.get(key='ex_project_name').value, 'test_project',
-    #            'ex_project_name credential does not match project name')
-    #    self.assertIsNotNone(
-    #            os_accounts.get_user('test_user'),
-    #            'Expected user, got None')
-    #    self.assertIsNotNone(
-    #            os_accounts.get_project('test_project'),
-    #            'Expected project, got None')
-    #    self.assertIsNone(
-    #            os_accounts.get_project('test_user'),
-    #            'A usergroup was created when project was specified')
-    #    #TODO: This line fails with: Unauthorized: Could not find token:
-    #    # 91c0731932f04ec5aa6d17c9a7974fba (HTTP 401)
-    #    os_accounts.delete_account('test_user', 'test_project')
-    #    #Test account was deleted
-
-    def test_eucalyptus_identity_creation(self):
-        pass
-        #username = 'estevetest03'
-        #accounts = EucaAccounts(self.euca_provider)
-        #user = accounts.get_user(username)
-        #self.assertIsNotNone(
-        #        user,
-        #        'Expected user %s exists in eucalyptus, got None' % username)
-        #identity = accounts.delete_user(username)
-        #self.assertIsNotNone(
-        #        identity,
-        #        'Expected new identity for %s, got None' % identity)
-        #identity = accounts.create_identity(user)
-        #accounts.delete_identity(username)
-        #self.assertEqual(1,1,"Account deleted succesfully")
