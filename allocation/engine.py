c459b276ddc349d49faa8f3665ff6d6842fe9cd8
Steve Gregory <contact@steve-gregory.com>
2014-10-27 11:52:51 -0700

    Temporary commit, working on refactoring 'inputs' from Warlock to Python classes

--- allocation/engine.py
+++ allocation/engine.py
@@ -13,10 +13,12 @@ from allocation.models import AllocationResult, InstanceResult
 from datetime import timedelta, datetime
 import calendar, pytz
 
-def _to_datestring(date_obj):
-    return date_obj.strftime("%Y-%m-%dT%H:%M:%SZ")
+### Utils ###
 
-def _datestring_to_date(date_string):
+def _to_datestring(dt):
+    return dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def _to_datetime(date_string):
     return datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
 
 def _days_in_month(dt):
@@ -24,9 +26,9 @@ def _days_in_month(dt):
 
 def _get_current_date_utc():
     return datetime.utcnow().replace(tzinfo = pytz.utc)
-### v2.0 ###
+
+### Main ###
 def calculate_allocation(allocation):
-    time_allowed = _get_time_allowed(allocation)
     instance_results = _get_instance_results(allocation)
 
     seconds_used = burn_rate = 0
@@ -34,9 +36,11 @@ def calculate_allocation(allocation):
         seconds_used += result.used_allocation
         burn_rate += result.burn_rate
 
+    time_allowed = _get_time_allowed(allocation)
     seconds_allowed = time_allowed.total_seconds()
+
     seconds_remaining = seconds_allowed-seconds_used
-    end_date = _datestring_to_date(allocation.end_date)
+    end_date = _to_datetime(allocation.end_date)
     time_to_zero = _get_ttz(end_date, seconds_remaining, burn_rate)
 
     return AllocationResult(
@@ -47,60 +51,54 @@ def calculate_allocation(allocation):
             time_to_zero=_to_datestring(time_to_zero),
             instance_results=instance_results)
 
-## Burn Rate
-def _get_ttz(end_date, seconds_remaining, burned_per_second):
-    return end_date + timedelta(
-            seconds=seconds_remaining/burned_per_second)
-
-## Time Used
 def _get_instance_results(allocation):
-    start_date = _datestring_to_date(allocation.start_date)
-    end_date = _datestring_to_date(allocation.end_date)
+    start_date = _to_datetime(allocation.start_date)
+    end_date = _to_datetime(allocation.end_date)
     instance_results = []
     for instance in allocation.instances:
         instance_results.append(
-                _calculate_instance_result(
+                _calculate_instance_result_cumulatively(
                     instance, allocation.rules, start_date, end_date))
     return instance_results
 
-def _calculate_instance_result(instance, rules, start_date, end_date):
+def _calculate_instance_result_cumulatively(instance, rules, start_date, end_date):
     """
-    TODO: We may want to combine multipliers when we have more than 2 results..
-    Example of when this matters:
-
     time_used = 500 seconds
-    time_used * 2 CPU   = 1000 Seconds
-    time_used * 4GB RAM = 2000 Seconds
-                          ============
-                          3000 Seconds
-    Vs.
-    time_used = 500 seconds
-    time_used * 2 CPU * 4GB RAM = 4000 Seconds
+    time_used * 2 CPU * 4GB RAM * 0 Burn Rate = 0 Seconds
     """
     burn_rate = time_used = timedelta(0)
     one_second_prior = end_date - timedelta(seconds=1)
 
     print "History of instance:%s" % instance.identifier
+    # Calculate time used by applying rules to each history and keeping a
+    # running total
     for history in instance.history:
-        for rule in rules:
-            if rule.type == 'increase_allocation':
-                continue
-            time_used += _calculate_rule(history, rule, start_date, end_date)
-            #Using ONLY the last history object, looking at one second before
-            # end date of the history, run over all rules to determine if 
-            # the instance is still 'burning' time.
-            if history == instance.history[-1]:
-                burn_rate += _calculate_rule(history, rule, one_second_prior, end_date)
+        if history.status != 'active':
+            continue
+        time_used += _apply_rules(
+                history, rules,
+                start_date, end_date)
+
+    # Calculate burn rate by applying rules to last history
+    # (given a one second delta) to determine how much time is burned/second
+    last_history = instance.history[-1]
+    burn_rate = _apply_rules(
+            last_history, rules,
+            one_second_prior, end_date)
     return InstanceResult(
             used_allocation=time_used.total_seconds(),
             burn_rate=burn_rate.total_seconds())
 
-def _calculate_rule(history, rule, start_date, end_date):
-    if history.status != 'active':
-        return timedelta(0)
-
+def _apply_rules(history, rules, start_date, end_date):
     time_used = _calculate_time_used(history, start_date, end_date)
+    for rule in rules:
+        if rule.type == 'increase_allocation':
+            continue
+        multiplier = _get_multiplier(rule, history)
+        time_used *= multiplier * rule.amount
+    return time_used
 
+def _get_multiplier(rule, history):
     if rule.type == 'size_ram':
         multiplier = history.size.ram
     elif rule.type == 'size_cpu':
@@ -111,36 +109,11 @@ def _calculate_rule(history, rule, start_date, end_date):
         multiplier = 1
     else:
         raise Exception("Cannot calcualte Rule. Unknown Type: %s " % rule.type)
+    return multiplier
 
-    result = time_used * multiplier * rule.amount
-    print "%s * %s(%s) * %s = %s  | FROM:%s TO:%s"\
-            % (time_used, multiplier,
-               rule.type, rule.amount, result, start_date, end_date)
-    return result
 
-def _calculate_time_used(instance_history, start_date, end_date):
-    instance_start_date = _datestring_to_date(instance_history.start_date)
-    instance_end_date = _datestring_to_date(instance_history.end_date)
-    #Safety Check -- Return 0 if the history should not be counted.
-    if instance_end_date < start_date:
-        return timedelta(0)
 
-    if instance_start_date >= start_date:
-        #Use instance start date IF later than beginning of range
-        use_start = instance_start_date
-    else:
-        use_start = start_date
-
-    if instance_end_date <= end_date:
-        #Use instance end date IF earlier than end of range
-        use_end = instance_end_date
-    else:
-        use_end = end_date
-
-    time_used = use_end - use_start
-    return time_used
-
-## Time Allowed
+## Time Allowed ##
 def _get_time_allowed(allocation):
     time_allowed = timedelta(0)
     for rule in allocation.rules:
@@ -171,3 +144,96 @@ def _increase_by_amount(amount, unit):
     else:
         raise Exception("Conversion failed: Invalid value '%s'" % unit)
     return timedelta(days=day_amount, seconds=second_amount)
+
+## Time Used ##
+def _calculate_time_used(instance_history, start_date, end_date):
+    instance_start_date = _to_datetime(instance_history.start_date)
+    instance_end_date = _to_datetime(instance_history.end_date)
+
+    # Expiry Check -- If this instance history 
+    # ended prior to the beginning of
+    # the 'check range' it should not be counted.
+    if instance_end_date < start_date:
+        return timedelta(0)
+
+    if instance_start_date >= start_date:
+        #Use instance start date IF later than beginning of range
+        use_start = instance_start_date
+    else:
+        use_start = start_date
+
+    if instance_end_date <= end_date:
+        #Use instance end date IF earlier than end of range
+        use_end = instance_end_date
+    else:
+        use_end = end_date
+
+    time_used = use_end - use_start
+    return time_used
+
+
+## Burn Rate
+def _get_ttz(end_date, seconds_remaining, burned_per_second):
+    if burned_per_second >= 1:
+        #If the BurnRate is non-zero, we divide the amount burned per second by
+        # the amount of seconds the user has left. The result is the # of
+        # seconds from the end date until the user is OUT of allocation, with
+        # all things remaining constant.
+        remaining_time = seconds_remaining / burned_per_second
+    elif seconds_remaining <= 0:
+        #If the burn rate is zero and we are OUT of time we can still calculate
+        # how long ago one ran out of allocation (When they should have been
+        # stopped from using the machine).
+        remaining_time = seconds_remaining
+    else:
+        #The burn rate is zero but there is remaining time to be used.
+        # In these cases it is impossible to calculate a valid result
+        # TODO: Return the epoch or some "NIL" date string instead?
+        return None
+    return end_date + timedelta(seconds=remaining_time)
+
+#######
+#OLD DO NOT USE .. FOR REFERENCE ONLY!
+#######
+
+def _calculate_instance_result_individually(instance, rules, start_date, end_date):
+    """
+    TODO: We may want to combine multipliers when we have more than 2 results..
+    Example of when this matters:
+
+    time_used = 500 seconds
+    time_used * 0 BurnRate =    0 Seconds
+    time_used * 2 CPU      = 1000 Seconds
+    time_used * 4GB RAM    = 2000 Seconds
+                             ============
+                             3000 Seconds
+    """
+    burn_rate = time_used = timedelta(0)
+    one_second_prior = end_date - timedelta(seconds=1)
+
+    print "History of instance:%s" % instance.identifier
+    for history in instance.history:
+        for rule in rules:
+            if rule.type == 'increase_allocation':
+                continue
+            time_used += _calculate_rule(rule, history, start_date, end_date)
+            #Using ONLY the last history object, looking at one second before
+            # end date of the history, run over all rules to determine if 
+            # the instance is still 'burning' time.
+            if history == instance.history[-1]:
+                burn_rate += _calculate_rule(rule, history, one_second_prior, end_date)
+    return InstanceResult(
+            used_allocation=time_used.total_seconds(),
+            burn_rate=burn_rate.total_seconds())
+
+def _calculate_rule(rule, history, start_date, end_date):
+    if history.status != 'active':
+        return timedelta(0)
+
+    time_used = _calculate_time_used(history, start_date, end_date)
+    multiplier = _get_multiplier(rule, history)
+    result = time_used * multiplier * rule.amount
+    print "%s * %s(%s) * %s = %s  | FROM:%s TO:%s"\
+            % (time_used, multiplier,
+               rule.type, rule.amount, result, start_date, end_date)
+    return result
--- allocation/models.py
+++ allocation/models.py
@@ -1,54 +1,111 @@
 #from django.db import models
 #
 # NOTE: These are "warlock" models, NOT django models.
+from abc import ABCMeta, abstractmethod
 import warlock
 
-## INPUTS
-allocation_schema = {
-    "name":"Allocation",
-    "properties": {
-        "start_date" : { "type" : "string" },
-        "end_date" : { "type" : "string" },
-        "rules" : { "type" : "array"},
-        "instances" : { "type " : "array" },
-    },
-}
-rule_schema = {
-    "name":"Rule",
-    "properties": {
-        "name" : { "type" : "string"},
-        "type" : { "type" : "string"},
-        "amount" : { "type" : "number"},
-        "unit" : { "type" : "string"},
-    },
-}
-instance_schema = {
-    "name":"Instance",
-    "properties": {
-        "identifier": { "type": "string" },
-        "provider": { "type": "integer" },
-        "machine": { "type": "string" },
-        "history" : { "type " : "array" },
-    }
-}
-instance_history_schema = {
-    "name":"InstanceHistory",
-    "properties": {
-        "start_date" : { "type" : "string" },
-        "end_date" : { "type" : "string" },
-        "status" : { "type" : "string" },
-        "size" : { "type" : "object" },
-    },
-}
-size_schema = {
-    "name":"Size",
-    "properties": {
-        "id" : { "type" : "string" },
-        "cpu" : {"type" : "number" },
-        "ram" : {"type" : "number" },
-        "disk" : {"type" : "number" },
-    },
-}
+class TimeUnit:
+    #TODO: If using enums:
+    # pip install enum34
+    # class Unit(Enum):
+    second = 0
+    minute = 1
+    hour = 2
+    day = 3
+    week = 4
+    month = 5
+    year = 6
+
+
+class Rule():
+    name = None
+
+
+class UnitAndAmountRule(Rule):
+    """
+    These rules have a name, a unit, & an amount
+    Unit = The scope represented by 'amount' (Default:TimeUnit.second)
+    Amount to increase is ALWAYS an integer
+    """
+    amount = 0
+    unit = TimeUnit.second
+
+
+class InstanceCountingRule(Rule):
+    """
+    Each sub-class represents a way of 'counting time' 
+    """
+    __metaclass__ = ABCMeta
+    pass
+
+
+class InstanceMultiplierRule(InstanceCountingRule):
+    """
+    Instance Multiplier Rules.. All rules of this type will be applied
+    multiplicatively to an instance.
+    Ex: BurnTime(.5) + SizeCPU(1)
+    Instance 1 : 10 hours used * .5 Burn Time * (1 * )4 CPUs = 20 Hours
+    """
+    multiplier = None
+
+
+class MultiplyBurnTime(InstanceMultiplierRule):
+    pass
+
+
+class MultiplySizeCPU(InstanceMultiplierRule):
+    pass
+
+
+class MultiplySizeDisk(InstanceMultiplierRule):
+    pass
+
+
+class MultiplySizeRAM(InstanceMultiplierRule):
+    pass
+
+
+class IncreaseAllocation(UnitAndAmountRule):
+    pass
+
+#################
+
+class Provider():
+    name = None
+    identifier = None
+
+
+class Machine():
+    name = None
+    identifier = None
+
+
+class Size():
+    identifier = None
+    cpu = None
+    ram = None
+    disk = None
+
+
+class Instance():
+    provider = None
+    machine = None
+    identifier = None
+    history = []
+
+
+class InstanceHistory():
+    start_date = None
+    end_date = None
+    status = None
+    size = None
+
+
+class Allocation():
+    start_date = None
+    end_date = None
+    rules = []
+    instances = []
 
 ## OUTPUTS
 instance_result_schema = {
@@ -69,10 +126,5 @@ allocation_result_schema = {
         "instance_results": { "type": "array" },
     }
 }
-Allocation = warlock.model_factory(allocation_schema)
-Rule = warlock.model_factory(rule_schema)
-Instance = warlock.model_factory(instance_schema)
-InstanceHistory = warlock.model_factory(instance_history_schema)
-Size = warlock.model_factory(size_schema)
 InstanceResult = warlock.model_factory(instance_result_schema)
 AllocationResult = warlock.model_factory(allocation_result_schema)
