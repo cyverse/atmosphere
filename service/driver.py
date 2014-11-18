6a8306125f68b0b9c2d7e41fd8053c0867faf33c
Steve Gregory <contact@steve-gregory.com>
2014-11-18 11:23:54 -0700

    Remove initialize -- No longer used in rtwo >0.2.0

--- service/driver.py
+++ service/driver.py
@@ -14,8 +14,6 @@ def get_driver(driverCls, provider, identity, **provider_credentials):
     """
     Create a driver object from a class, provider and identity.
     """
-    from rtwo import compute
-    compute.initialize()
     if not provider_credentials:
         provider_credentials = provider.options
     driver = driverCls(provider, identity, **provider_credentials)
