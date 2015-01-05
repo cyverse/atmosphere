5ffca40a8b65622f1d542732934a123b627eceb0
Steve Gregory <contact@steve-gregory.com>
2015-01-05 11:34:07 -0700

    changed ID reference to UUID

--- service/task.py
+++ service/task.py
@@ -48,9 +48,9 @@ def add_floating_ip_task(driver, instance, *args, **kwargs):
                           *args, **kwargs)
 
 
-def destroy_instance_task(instance, identity_id, *args, **kwargs):
+def destroy_instance_task(instance, identity_uuid, *args, **kwargs):
     return destroy_instance.delay(
-            instance.alias, identity_id, *args, **kwargs)
+            instance.alias, identity_uuid, *args, **kwargs)
 
 
 def detach_volume_task(driver, instance_id, volume_id, *args, **kwargs):
