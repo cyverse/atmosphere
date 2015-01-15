42fbd01dcd5565d7390b6f24f37b073e1d9fb520
Evan Briones <erbriones@gmail.com>
2015-01-15 12:00:23 -0700

    add quota support for identity membership
    Add support for GET, PUT, PATCH methods.
    
    	new file:   api/quota.py
    	modified:   api/urls.py

---
+++ api/quota.py
@@ -0,0 +1,103 @@
+"""
+Atmosphere quota rest api.
+"""
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from api import failure_response
+from api.permissions import ApiAuthRequired
+from api.serializers import QuotaSerializer
+
+from core.models import Identity, IdentityMembership, Quota
+
+
+class QuotaMembership(APIView):
+    permission_classes = (ApiAuthRequired,)
+
+    def get(self, request, provider_uuid, identity_uuid):
+        """
+        """
+        membership = None
+
+        try:
+            identity = Identity.objects.get(uuid=identity_uuid)
+            membership = IdentityMembership.objects.get(identity=identity)
+        except Identity.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "Identity not found.")
+        except IdentityMembership.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "IdentityMembership not found.")
+
+        serialized_data = QuotaSerializer(membership.quota).data
+        return Response(serialized_data)
+
+    def put(self, request, provider_uuid, identity_uuid):
+        """
+        """
+        data = request.DATA
+        membership = None
+        serializer = QuotaSerializer(data=data)
+
+        try:
+            identity = Identity.objects.get(uuid=identity_uuid)
+            membership = IdentityMembership.objects.get(identity=identity)
+        except Identity.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "Identity not found.")
+        except IdentityMembership.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "IdentityMembership not found.")
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Create or fetch quota
+        fields = serializer.data
+        (quota, created) = Quota.objects.get_or_create(**fields)
+
+        # Update memberships quota
+        membership.quota = quota
+        membership.save()
+
+        if created:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, provider_uuid, identity_uuid):
+        """
+        """
+        data = request.DATA
+        membership = None
+
+        try:
+            identity = Identity.objects.get(uuid=identity_uuid)
+            membership = IdentityMembership.objects.get(identity=identity)
+        except Identity.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "Identity not found.")
+        except IdentityMembership.DoesNotExist:
+            return failure_response(status.HTTP_400_BAD_REQUEST,
+                                    "IdentityMembership not found.")
+
+        serializer = QuotaSerializer(membership.quota, data=data, partial=True)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        # Create or fetch quota
+        fields = serializer.data
+        (quota, created) = Quota.objects.get_or_create(**fields)
+
+        # Update memberships quota
+        membership.quota = quota
+        membership.save()
+
+        if created:
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.data, status=status.HTTP_200_OK)
--- api/urls.py
+++ api/urls.py
@@ -37,6 +37,7 @@ from api.project import ProjectInstanceList, ProjectInstanceExchange,\
         ProjectVolumeList, ProjectVolumeExchange
 from api.profile import Profile
 from api.provider import ProviderList, Provider
+from api.quota import QuotaMembership
 from api.size import SizeList, Size
 from api.hypervisor import HypervisorList, HypervisorDetail
 from api.step import StepList, Step
@@ -300,6 +301,9 @@ public_apis = format_suffix_patterns(patterns(
 
     url(identity_specific + r'/allocation$',
         AllocationMembership.as_view(), name='allocation'),
+
+    url(identity_specific + r'/quota$',
+        QuotaMembership.as_view(), name='quota'),
 
     url(r'version$', Version.as_view()),
     url(r'^maintenance$',
