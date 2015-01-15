0aa763f512a372a0d587e78d38aa661ca1c4fc5a
Steve Gregory <contact@steve-gregory.com>
2015-01-15 11:58:06 -0700

    Temporary-save suspend all script in progress

--- scripts/admin_suspend_all.py
+++ scripts/admin_suspend_all.py
@@ -1,14 +1,55 @@
 #!/usr/bin/env python
-from collections import OrderedDict
+import argparse
 import time
 
-from api import get_esh_driver
+from collections import OrderedDict
 from core.models import Provider, Identity
-from service.driver import get_admin_driver
+from service.driver import get_admin_driver, get_esh_driver
 from service.instance import suspend_instance
+SLEEP_MIN=30
+SLEEP_MAX=2*60
 
-def suspend_all_instances():
-    admin_driver = get_admin_driver(Provider.objects.get(id=4))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--provider-list", action="store_true",
+                        help="List of provider names and IDs")
+    parser.add_argument("--provider-id", type=int,
+                        help="Atmosphere provider ID"
+                        " to use when importing users.")
+    parser.add_argument("--action",
+                        help="Atmosphere Action to take [Suspend/Stop]")
+    parser.add_argument("--dry-run", action="store_true",
+                        help="A 'dry-run' so you know what will happen,"
+                             " before it happens")
+    args = parser.parse_args()
+    dry_run = args.dry_run
+    action = "suspend"
+    provider = None
+    if args.provider_list:
+        print "ID\tName"
+        for p in Provider.objects.all().order_by('id'):
+            print "%d\t%s" % (p.id, p.location)
+        return
+    #Debugging args
+    if dry_run:
+        print "Dry run initialized.."
+
+    if not args.provider_id:
+        print "ERROR: provider-id is required. To get a list of providers use"\
+            " --provider-list"
+    provider = Provider.objects.get(id=args.provider_id)
+    print "Provider Selected:%s" % provider
+
+    if args.action and 'stop' in args.action.lower():
+        action = 'stop'
+        print "Begin stopping Instances"
+    else:
+        print "Begin suspending Instances"
+    suspend_all_instances(provider, action, dry_run)
+
+
+def suspend_all_instances(provider, action, dry_run=False):
+    admin_driver = get_admin_driver(provider)
     all_insts = admin_driver.meta(admin_driver=admin_driver).all_instances()
     users = []
     bad_instances = []
@@ -29,14 +70,45 @@ def suspend_all_instances():
         instances = driver.list_instances()
         print "Found %s instances for %s" % (len(instances), user)
         for inst in instances:
-            if inst._node.extra['status'] == 'active':
-                print "Attempt to suspend Instance %s in state %s" % (inst.id, inst._node.extra['status'])
-                try:
-                    suspend_instance(driver, inst, ident.provider.id, ident.id, ident.created_by)
-                    print "Suspended Instance %s.. Sleep 2min" % (inst.id,)
-                    time.sleep(2*60)
-                except Exception, err:
-                    print "WARN: Could not suspend instance %s. Error: %s" % (inst.id, err)
+            _execute_action(ident, inst, action, dry_run)
+
+def _execute_action(ident, inst, action, dry_run=False):
+    status = inst._node.extra['status']
+    if 'suspend' in action.lower():
+        _execute_suspend(ident, inst, status, dry_run)
+    elif 'stop' in action.lower():
+        _execute_stop(ident, inst, status, dry_run)
+    else:
+        raise Exception("Unknown Action : %s" % action)
+
+def _execute_stop(ident, inst, status, dry_run=False):
+    if status == 'active':
+        print "Attempt to Stop Instance %s in state %s" % (inst.id, status)
+        try:
+            if not dry_run:
+                suspend_instance(driver, inst, ident.provider.id, ident.id, ident.created_by)
+                sleep_time = random.uniform(SLEEP_MIN,SLEEP_MAX)
+            else:
+                #Dry runs move faster..
+                sleep_time = 1
+            print "Shutoff Instance %s.. Sleep %s seconds" % (inst.id,sleep_time)
+            time.sleep(sleep_time)
+        except Exception, err:
+            print "WARN: Could not shut off instance %s. Original Status:%s Error: %s" % (inst.id, status, err)
+
+def _execute_suspend(ident, inst, status, dry_run=False):
+    if status == 'active':
+        print "Attempt to suspend Instance %s in state %s" % (inst.id, inst._node.extra['status'])
+        try:
+            if not dry_run:
+                suspend_instance(driver, inst, ident.provider.id, ident.id, ident.created_by)
+                sleep_time = random.uniform(SLEEP_MIN,SLEEP_MAX)
+            else:
+                sleep_time = 1
+            print "Suspended Instance %s.. Sleep %s seconds" % (inst.id,sleep_time)
+            time.sleep(sleep_time)
+        except Exception, err:
+            print "WARN: Could not suspend instance %s. Original Status:%s Error: %s" % (inst.id, status, err)
 
 if __name__ == "__main__":
     suspend_all_instances()
