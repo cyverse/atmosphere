0edacde1e958b602fe51b81a0ccba53f42e0c2e3
Evan Briones <erbriones@gmail.com>
2015-01-22 13:27:32 -0700

    add permission class for cloud admins
    	modified:   api/permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -3,8 +3,10 @@ Atmosphere API's extension of DRF permissions.
 """
 from rest_framework import permissions
 
+from threepio import logger
 
-#NOTE: It would be better to use Object-level permissions here.
+
+# NOTE: It would be better to use Object-level permissions here.
 class ProjectOwnerRequired(permissions.BasePermission):
     def has_permission(self, request, view):
         auth_user = request.user
@@ -13,21 +15,34 @@ class ProjectOwnerRequired(permissions.BasePermission):
             logger.warn("Could not find kwarg:'project_id'")
             return False
         return any(
-                group for group in auth_user.group_set.all()
-                if group.projects.filter(id=project_id))
+            group for group in auth_user.group_set.all()
+            if group.projects.filter(id=project_id))
+
 
 class ApiAuthRequired(permissions.BasePermission):
     def has_permission(self, request, view):
         auth_user = request.user.is_authenticated()
         return auth_user
 
+
+class CloudAdminRequired(permissions.BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+
+        if not user.is_authenticated():
+            return False
+
+        return user.is_staff or user.is_superuser
+
+
 class ApiAuthOptional(permissions.BasePermission):
     def has_permission(self, request, view):
-        #Allow access to GET/OPTIONS/HEAD operations.
+        # Allow access to GET/OPTIONS/HEAD operations.
         if request.method in permissions.SAFE_METHODS:
             return True
-        #ASSERT: User trying to POST/PUT/PATCH, must be authenticated
+        # ASSERT: User trying to POST/PUT/PATCH, must be authenticated
         return request.user.is_authenticated()
+
 
 class InMaintenance(permissions.BasePermission):
     def has_permission(self, request, view):
