f23377d8f9fe26a46daf4882ebac72d732521917
jchansen <jason_hansen@outlook.com>
2015-01-29 13:35:47 -0700

    break out TagRelatedField

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -3,3 +3,4 @@ from .get_projects_for_obj import get_projects_for_obj
 from .projects_field import ProjectsField
 from .new_threshold_field import NewThresholdField
 from .app_bookmark_field import AppBookmarkField
+from .tag_related_field import TagRelatedField
---
+++ api/serializers/tag_related_field.py
@@ -0,0 +1,23 @@
+from core.models.identity import Identity
+from core.models.tag import Tag, find_or_create_tag
+from rest_framework import serializers
+
+
+class TagRelatedField(serializers.SlugRelatedField):
+
+    def to_native(self, tag):
+        return super(TagRelatedField, self).to_native(tag)
+
+    def field_from_native(self, data, files, field_name, into):
+        value = data.get(field_name)
+        if value is None:
+            return
+        try:
+            tags = []
+            for tagname in value:
+                tag = find_or_create_tag(tagname, None)
+                tags.append(tag)
+            into[field_name] = tags
+        except Identity.DoesNotExist:
+            into[field_name] = None
+        return
