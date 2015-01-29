6ba369bf8200fded57fce293bc56e66e0f5bf976
Steve Gregory <contact@steve-gregory.com>
2015-01-28 21:57:29 -0700

    Preliminary model for 'CloudAdministrator'. Migrations withheld until Django1.7 branch matures

--- core/models/provider.py
+++ core/models/provider.py
@@ -264,3 +264,28 @@ class AccountProvider(models.Model):
     class Meta:
         db_table = 'provider_admin'
         app_label = 'core'
+
+class CloudAdministrator(models.Model):
+    """
+    This model is reserved exclusively for users who are in control of an
+    entire 'cloud' provider.
+    CloudAdministrators have access to:
+    * Any 'AccountProvider' identities for that provider
+      * Allows access to all 'visible' tenants w/o giving 'emulate/identity'
+        access
+    * Perform special actions:
+      * Create/Approve/Deny/Check status of [Allocation/Quota/Machine]Requests
+      * Create/Update VM State Policies (What counts for/against allocation)
+      * Create/Update VM actions on over Allocation
+        (Suspend/Shutoff/Email/etc.)
+    This class only applies to Private clouds!
+    """
+    user = models.ForeignKey("AtmosphereUser")
+    provider = models.ForeignKey(Provider)
+
+    def __unicode__(self):
+        return "Cloud Administrator: %s Provider:%s" % (self.user, self.provider)
+
+    class Meta:
+        db_table = 'cloud_administrator'
+        app_label = 'core'
