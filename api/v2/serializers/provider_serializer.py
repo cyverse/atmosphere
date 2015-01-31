b13ab307009e457767b2acbdd22b4e9b77612c5e
jchansen <jason_hansen@outlook.com>
2015-01-30 22:44:13 -0700

    improve provider endpoint

--- api/v2/serializers/provider_serializer.py
+++ api/v2/serializers/provider_serializer.py
@@ -1,10 +1,13 @@
 from core.models import Provider
 from rest_framework import serializers
-
+from .provider_type_serializer import ProviderTypeSerializer
+from .platform_type_serializer import PlatformTypeSerializer
 
 class ProviderSerializer(serializers.ModelSerializer):
     name = serializers.CharField(source='location')
+    type = ProviderTypeSerializer()
+    virtualization = PlatformTypeSerializer()
 
     class Meta:
         model = Provider
-        fields = ('id', 'name', 'description', 'public')
+        fields = ('id', 'name', 'description', 'public', 'active', 'type', 'virtualization', 'start_date', 'end_date', )
