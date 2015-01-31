ba7c06a41c374f3ade64050a1ef353e12145b418
jchansen <jason_hansen@outlook.com>
2015-01-30 23:20:13 -0700

    make pep8 compliant

--- api/v2/serializers/instance_serializer.py
+++ api/v2/serializers/instance_serializer.py
@@ -4,6 +4,7 @@ from .identity_summary_serializer import IdentitySummarySerializer
 from .user_serializer import UserSerializer
 from .provider_summary_serializer import ProviderSummarySerializer
 
+
 class InstanceSerializer(serializers.ModelSerializer):
     identity = IdentitySummarySerializer(source='created_by_identity')
     user = UserSerializer(source='created_by')
