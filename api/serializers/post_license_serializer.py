0de07da9f7adc282cba2de2e89359357844d42c9
jchansen <jason_hansen@outlook.com>
2015-01-30 18:57:18 -0700

    upgrade POST_LicenseSerializer to DRF3

--- api/serializers/post_license_serializer.py
+++ api/serializers/post_license_serializer.py
@@ -1,10 +1,12 @@
-from core.models.license import License
+from core.models.license import License, LicenseType
+from core.models.user import AtmosphereUser
 from rest_framework import serializers
 
 
 class POST_LicenseSerializer(serializers.ModelSerializer):
-    created_by = serializers.SlugRelatedField(slug_field='username')
-    type = serializers.SlugRelatedField(source='license_type', slug_field='name')
+    created_by = serializers.SlugRelatedField(slug_field='username', queryset=AtmosphereUser.objects.all())
+    type = serializers.SlugRelatedField(source='license_type', slug_field='name', queryset=LicenseType.objects.all())
+
     class Meta:
         model = License
         exclude = ("license_type",)
