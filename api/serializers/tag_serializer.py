0f3e42ca657c3f5835d2c5a31e0061657a1b56d2
jchansen <jason_hansen@outlook.com>
2015-01-30 18:57:56 -0700

    upgrade TagSerializer to DRF3

--- api/serializers/tag_serializer.py
+++ api/serializers/tag_serializer.py
@@ -1,9 +1,10 @@
 from core.models.tag import Tag
+from core.models import AtmosphereUser
 from rest_framework import serializers
 
 
 class TagSerializer(serializers.ModelSerializer):
-    user = serializers.SlugRelatedField(slug_field='username')
+    user = serializers.SlugRelatedField(slug_field='username', queryset=AtmosphereUser.objects.all())
     description = serializers.CharField(required=False)
 
     class Meta:
