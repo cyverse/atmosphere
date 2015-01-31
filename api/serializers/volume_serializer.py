8dbc8ffe9ee3092547b0505dec271051a6e13862
jchansen <jason_hansen@outlook.com>
2015-01-30 19:08:59 -0700

    more DRF3 upgrades to VolumeSerializer

--- api/serializers/volume_serializer.py
+++ api/serializers/volume_serializer.py
@@ -7,9 +7,9 @@ from .get_context_user import get_context_user
 
 class VolumeSerializer(serializers.ModelSerializer):
     status = serializers.CharField(read_only=True, source='get_status')
-    attach_data = serializers.Field(source='esh_attach_data')
+    attach_data = serializers.ReadOnlyField(source='esh_attach_data')
     #metadata = serializers.Field(source='esh_metadata')
-    mount_location = serializers.Field()
+    mount_location = serializers.ReadOnlyField()
     created_by = serializers.SlugRelatedField(slug_field='username',
                                               read_only=True)
     provider = serializers.ReadOnlyField(source="provider.uuid")
