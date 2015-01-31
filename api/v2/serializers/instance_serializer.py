45db21e2b4093cbda7976189327467ca3aebe1a3
jchansen <jason_hansen@outlook.com>
2015-01-30 22:23:04 -0700

    add instance serializer

---
+++ api/v2/serializers/instance_serializer.py
@@ -0,0 +1,13 @@
+from core.models import Instance
+from rest_framework import serializers
+from .identity_summary_serializer import IdentitySummarySerializer
+from .user_serializer import UserSerializer
+
+
+class InstanceSerializer(serializers.ModelSerializer):
+    identity = IdentitySummarySerializer(source='created_by_identity')
+    user = UserSerializer(source='created_by')
+
+    class Meta:
+        model = Instance
+        fields = ('id', 'name', 'ip_address', 'shell', 'vnc', 'start_date', 'end_date', 'identity', 'user')
