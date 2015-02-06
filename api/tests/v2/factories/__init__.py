07b876740419d34323497a3bea95c4b3dbc9b1b5
jchansen <jason_hansen@outlook.com>
2015-02-05 23:47:51 -0700

    add tag and user factories

---
+++ api/tests/v2/factories/__init__.py
@@ -0,0 +1,2 @@
+from .tag_factory import TagFactory
+from .user_factory import UserFactory, AnonymousUserFactory
---
+++ api/tests/v2/factories/user_factory.py
@@ -0,0 +1,15 @@
+import factory
+from core.models import AtmosphereUser as User
+from django.contrib.auth.models import AnonymousUser
+
+
+class UserFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: 'user%d' % n)
+
+
+class AnonymousUserFactory(factory.Factory):
+    class Meta:
+        model = AnonymousUser
