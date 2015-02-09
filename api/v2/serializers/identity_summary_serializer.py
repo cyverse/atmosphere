ce2d29c6652e17cfb2a567cb4e456efb2beb7d5a
jchansen <jason_hansen@outlook.com>
2015-02-08 21:47:02 -0700

    hyperlink identity summary serializer

--- api/v2/serializers/identity_summary_serializer.py
+++ api/v2/serializers/identity_summary_serializer.py
@@ -2,7 +2,8 @@ from core.models import Identity
 from rest_framework import serializers
 
 
-class IdentitySummarySerializer(serializers.ModelSerializer):
+class IdentitySummarySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Identity
+        view_name = 'api_v2:identity-detail'
         fields = ('id',)
