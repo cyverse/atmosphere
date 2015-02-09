d6e52283f0dbae1d19fc64a904153a62974198a9
jchansen <jason_hansen@outlook.com>
2015-02-08 21:58:07 -0700

    hyperlink volume action serializer

--- api/v2/serializers/volume_action_serializer.py
+++ api/v2/serializers/volume_action_serializer.py
@@ -2,7 +2,8 @@ from core.models import VolumeAction
 from rest_framework import serializers
 
 
-class VolumeActionSerializer(serializers.ModelSerializer):
+class VolumeActionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = VolumeAction
+        view_name = 'api_v2:volumeaction-detail'
         # fields = ('id', 'name', 'description')
