af56a44daf2e4d4401a5de53b5d63156acb0c546
jchansen <jason_hansen@outlook.com>
2015-02-08 20:42:07 -0700

    hyperlink user serializer

--- api/v2/serializers/user_serializer.py
+++ api/v2/serializers/user_serializer.py
@@ -2,7 +2,8 @@ from core.models.user import AtmosphereUser
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = AtmosphereUser
-        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser')
+        view_name = 'api_v2:atmosphereuser-detail'
+        fields = ('id', 'url', 'username', 'first_name', 'last_name', 'email', 'is_staff', 'is_superuser')
