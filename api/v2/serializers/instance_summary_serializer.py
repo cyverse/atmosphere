7565bde6a4a4e7a9a208962098bcb177ad9db8f4
jchansen <jason_hansen@outlook.com>
2015-02-08 21:57:33 -0700

    hyperlink instance summary serializer

--- api/v2/serializers/instance_summary_serializer.py
+++ api/v2/serializers/instance_summary_serializer.py
@@ -2,7 +2,8 @@ from core.models import Instance
 from rest_framework import serializers
 
 
-class InstanceSummarySerializer(serializers.ModelSerializer):
+class InstanceSummarySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Instance
-        fields = ('id', 'name', 'provider_alias', 'ip_address', 'shell', 'vnc', 'start_date')
+        view_name = 'api_v2:instance-detail'
+        fields = ('id', 'url', 'name', 'provider_alias', 'ip_address', 'shell', 'vnc', 'start_date')
