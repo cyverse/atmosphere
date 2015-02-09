a83bbbd5ad1b0f58584de303cba881e6116a85a6
jchansen <jason_hansen@outlook.com>
2015-02-08 20:42:47 -0700

    hyperlink allocation serializer

--- api/v2/serializers/allocation_serializer.py
+++ api/v2/serializers/allocation_serializer.py
@@ -2,7 +2,8 @@ from core.models import Allocation
 from rest_framework import serializers
 
 
-class AllocationSerializer(serializers.ModelSerializer):
+class AllocationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Allocation
+        view_name = 'api_v2:allocation-detail'
         # fields = ('id', 'quota')
