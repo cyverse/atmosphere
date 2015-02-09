ba9eeac32074c994a3058f517fd935ca22cd239c
jchansen <jason_hansen@outlook.com>
2015-02-08 19:08:27 -0700

    add project detail tests

--- api/tests/v2/projects.py
+++ api/tests/v2/projects.py
@@ -47,3 +47,41 @@ class GetProjectListTests(APITestCase):
         self.assertIn('volumes', data)
         self.assertIn('start_date', data)
         self.assertIn('end_date', data)
+
+
+class GetProjectDetailTests(APITestCase):
+    def setUp(self):
+        self.anonymous_user = AnonymousUserFactory()
+        self.user = UserFactory.create()
+        self.group = GroupFactory.create(name=self.user.username)
+        self.project = ProjectFactory.create(owner=self.group)
+
+        user2 = UserFactory.create()
+        group2 = GroupFactory.create(name=user2.username)
+        ProjectFactory.create(owner=group2)
+
+        self.view = ProjectViewSet.as_view({'get': 'retrieve'})
+        factory = APIRequestFactory()
+        url = reverse('api_v2:project-detail', args=(self.project.id,))
+        self.request = factory.get(url)
+
+    def test_is_not_public(self):
+        force_authenticate(self.request, user=self.anonymous_user)
+        response = self.view(self.request, pk=self.project.id)
+        self.assertEquals(response.status_code, 403)
+
+    def test_response_contains_expected_fields(self):
+        force_authenticate(self.request, user=self.user)
+        response = self.view(self.request, pk=self.project.id)
+        data = response.data
+
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(data), 8)
+        self.assertEquals(data['id'], self.project.id)
+        self.assertEquals(data['name'], self.project.name)
+        self.assertEquals(data['description'], self.project.description)
+        self.assertIn('owner', data)
+        self.assertIn('instances', data)
+        self.assertIn('volumes', data)
+        self.assertIn('start_date', data)
+        self.assertIn('end_date', data)
