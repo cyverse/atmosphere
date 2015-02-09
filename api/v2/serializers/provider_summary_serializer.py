f1a89de45a7b474246fea00894f1257a8ae13fa6
jchansen <jason_hansen@outlook.com>
2015-02-08 21:43:30 -0700

    hyperlink provider-summary serializer

--- api/v2/serializers/provider_summary_serializer.py
+++ api/v2/serializers/provider_summary_serializer.py
@@ -3,9 +3,11 @@ from rest_framework import serializers
 from .provider_type_serializer import ProviderTypeSerializer
 from .platform_type_serializer import PlatformTypeSerializer
 
-class ProviderSummarySerializer(serializers.ModelSerializer):
+
+class ProviderSummarySerializer(serializers.HyperlinkedModelSerializer):
     name = serializers.CharField(source='location')
 
     class Meta:
         model = Provider
-        fields = ('id', 'name', 'description', 'public', 'active', 'start_date', 'end_date', )
+        view_name = 'api_v2:provider-detail'
+        fields = ('id', 'url', 'name', 'description', 'public', 'active', 'start_date', 'end_date', )
