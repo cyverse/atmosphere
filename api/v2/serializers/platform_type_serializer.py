50e8835ea9d3f77cb05b738036c6f0d6f27c1029
jchansen <jason_hansen@outlook.com>
2015-02-08 21:57:43 -0700

    hyperlink platform type serializer

--- api/v2/serializers/platform_type_serializer.py
+++ api/v2/serializers/platform_type_serializer.py
@@ -2,6 +2,7 @@ from core.models import PlatformType
 from rest_framework import serializers
 
 
-class PlatformTypeSerializer(serializers.ModelSerializer):
+class PlatformTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PlatformType
+        view_name = 'api_v2:platformtype-detail'
