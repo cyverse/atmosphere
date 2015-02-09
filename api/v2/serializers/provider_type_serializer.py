0fb8e3f5ad706d28ba4c6711acc5f954cdf0ea20
jchansen <jason_hansen@outlook.com>
2015-02-08 21:44:31 -0700

    hyperlink provider-type serializer

--- api/v2/serializers/provider_type_serializer.py
+++ api/v2/serializers/provider_type_serializer.py
@@ -2,6 +2,7 @@ from core.models import ProviderType
 from rest_framework import serializers
 
 
-class ProviderTypeSerializer(serializers.ModelSerializer):
+class ProviderTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProviderType
+        view_name = 'api_v2:providertype-detail'
