c871d4866c5bbd8b0a8ac7587356ca148b6dc020
jchansen <jason_hansen@outlook.com>
2015-02-08 21:57:25 -0700

    hyperlink instance action serializer

--- api/v2/serializers/instance_action_serializer.py
+++ api/v2/serializers/instance_action_serializer.py
@@ -2,7 +2,8 @@ from core.models import InstanceAction
 from rest_framework import serializers
 
 
-class InstanceActionSerializer(serializers.ModelSerializer):
+class InstanceActionSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = InstanceAction
+        view_name = 'api_v2:instanceaction-detail'
         # fields = ('id', 'name', 'description')
