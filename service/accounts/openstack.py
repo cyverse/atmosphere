a4fe83fec9d8b5e3ab23fb7173d9a1f9ddd6d8fb
Steve Gregory <contact@steve-gregory.com>
2015-02-11 22:50:52 -0700

    Out of sync update from D-D. ICMP 0 is valid, ICMP -1 is invalid on Juno

--- service/accounts/openstack.py
+++ service/accounts/openstack.py
@@ -32,7 +32,7 @@ class AccountDriver():
     core_provider = None
 
     MASTER_RULES_LIST = [
-        ("ICMP", -1, 255),
+        ("ICMP", 0, 255),
         #FTP Access
         ("UDP", 20, 20),  # FTP data transfer
         ("TCP", 20, 21),  # FTP control
