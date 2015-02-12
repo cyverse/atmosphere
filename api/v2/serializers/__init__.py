a84adfd4a214d8e6ee5a8ff7c2e7a890b32f66ff
jchansen <jason_hansen@outlook.com>
2015-02-12 15:59:29 -0700

    combine image serializers into single file

--- api/v2/serializers/__init__.py
+++ api/v2/serializers/__init__.py
@@ -5,12 +5,11 @@ from .instance_serializer import InstanceSerializer
 from .instance_summary_serializer import InstanceSummarySerializer
 from .volume_serializer import VolumeSerializer
 from .volume_summary_serializer import VolumeSummarySerializer
-from .image_serializer import ImageSerializer
+from .image import ImageSerializer, ImageSummarySerializer, ImageBookmarkSerializer
 from .provider import ProviderSerializer, ProviderSummarySerializer, ProviderTypeSerializer, PlatformTypeSerializer
 from .identity import IdentitySerializer, IdentitySummarySerializer
 from .quota_serializer import QuotaSerializer
 from .allocation_serializer import AllocationSerializer
 from .provider_machine import ProviderMachineSerializer, ProviderMachineSummarySerializer
-from .image_bookmark_serializer import ImageBookmarkSerializer
 from .size_serializer import SizeSerializer
 from .size_summary_serializer import SizeSummarySerializer
--- api/v2/serializers/image.py
+++ api/v2/serializers/image.py
@@ -1,8 +1,8 @@
-from core.models import Application as Image
+from core.models import Application as Image, ApplicationBookmark as ImageBookmark
 from rest_framework import serializers
 from .user_serializer import UserSerializer
 from .tag_serializer import TagSerializer
-from .provider_machine_summary_serializer import ProviderMachineSummarySerializer
+from .provider_machine import ProviderMachineSummarySerializer
 
 
 class MachineProviderRelatedField(serializers.RelatedField):
@@ -17,21 +17,29 @@ class MachineProviderRelatedField(serializers.RelatedField):
 
 
 class ImageSerializer(serializers.HyperlinkedModelSerializer):
-    # todo: add created by field w/ user
-    # todo: add tags
     created_by = UserSerializer()
     tags = TagSerializer(many=True)
     provider_images = MachineProviderRelatedField(source='providermachine_set', many=True)
-    # machine_count = serializers.SerializerMethodField()
-    machine_count = serializers.IntegerField(
-        source='providermachine_set.count',
-        read_only=True
-    )
-
-    # def get_machine_count(self, obj):
-    #     return obj.providermachine_set.count()
 
     class Meta:
         model = Image
         view_name = 'api_v2:application-detail'
         fields = ('id', 'url', 'uuid', 'name', 'description', 'icon', 'created_by', 'tags', 'start_date', 'end_date', 'provider_images', 'machine_count')
+
+
+class ImageSummarySerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Image
+        view_name = 'api_v2:application-detail'
+        fields = ('id', 'url', 'uuid', 'name', 'description', 'icon', 'start_date', 'end_date')
+
+
+class ImageBookmarkSerializer(serializers.HyperlinkedModelSerializer):
+    image = ImageSummarySerializer(source='application')
+    user = UserSerializer()
+
+    class Meta:
+        model = ImageBookmark
+        view_name = 'api_v2:applicationbookmark-detail'
+        fields = ('id', 'url', 'image', 'user')
