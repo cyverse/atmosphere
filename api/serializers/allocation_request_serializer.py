89fe241690e662f6dc8861d407d43b1da5f25aae
Evan Briones <erbriones@gmail.com>
2015-02-24 17:13:35 -0700

    fix DRF3 error source matching the field name
    	modified:   api/serializers/allocation_request_serializer.py

--- api/serializers/allocation_request_serializer.py
+++ api/serializers/allocation_request_serializer.py
@@ -5,9 +5,9 @@ from rest_framework import serializers
 class AllocationRequestSerializer(serializers.ModelSerializer):
     id = serializers.CharField(read_only=True, source="uuid")
     created_by = serializers.SlugRelatedField(
-        slug_field='username', source='created_by', read_only=True)
+        slug_field='username', read_only=True)
     status = serializers.SlugRelatedField(
-        slug_field='name', source='status', read_only=True)
+        slug_field='name', read_only=True)
 
     class Meta:
         model = AllocationRequest
