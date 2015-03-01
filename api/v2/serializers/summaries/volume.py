cc4189b2ca25dab476f451767a29282da5c5ecf4
jchansen <jason_hansen@outlook.com>
2015-02-28 22:56:57 -0700

    add identity to volume summary serializer

--- api/v2/serializers/summaries/volume.py
+++ api/v2/serializers/summaries/volume.py
@@ -1,8 +1,10 @@
 from core.models import Volume
 from rest_framework import serializers
+from ..summaries import IdentitySummarySerializer
 
 
 class VolumeSummarySerializer(serializers.HyperlinkedModelSerializer):
+    identity = IdentitySummarySerializer(source='instance_source.created_by_identity')
     provider = serializers.PrimaryKeyRelatedField(source='instance_source.provider', read_only=True)
     start_date = serializers.DateTimeField(source='instance_source.start_date')
     end_date = serializers.DateTimeField(source='instance_source.end_date')
@@ -10,4 +12,4 @@ class VolumeSummarySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Volume
         view_name = 'api_v2:volume-detail'
-        fields = ('id', 'url', 'name', 'size', 'provider', 'start_date', 'end_date')
+        fields = ('id', 'url', 'name', 'size', 'identity', 'provider', 'start_date', 'end_date')
