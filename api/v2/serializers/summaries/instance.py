dfb397d76e674ec7b0d11afb36cdbdd98b8d081d
jchansen <jason_hansen@outlook.com>
2015-03-02 01:28:25 -0700

    add image to instance summary serializer

--- api/v2/serializers/summaries/instance.py
+++ api/v2/serializers/summaries/instance.py
@@ -1,7 +1,8 @@
-from core.models import Instance, Size
+from core.models import Instance, Size, Application as Image
 from rest_framework import serializers
 from .identity import IdentitySummarySerializer
 from .size import SizeSummarySerializer
+from .image import ImageSummarySerializer
 
 
 class InstanceSummarySerializer(serializers.HyperlinkedModelSerializer):
@@ -10,12 +11,19 @@ class InstanceSummarySerializer(serializers.HyperlinkedModelSerializer):
     provider = serializers.PrimaryKeyRelatedField(source='created_by_identity.provider', read_only=True)
     status = serializers.CharField(source='esh_status', read_only=True)
     size = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
 
     def get_size(self, obj):
         size_alias = obj.esh_size()
         provider_id = obj.created_by_identity.provider_id
         size = Size.objects.get(alias=size_alias, provider=provider_id)
         serializer = SizeSummarySerializer(size, context=self.context)
+        return serializer.data
+
+    def get_image(self, obj):
+        image_uuid = obj.application_uuid()
+        image = Image.objects.get(uuid=image_uuid)
+        serializer = ImageSummarySerializer(image, context=self.context)
         return serializer.data
 
     class Meta:
@@ -33,6 +41,7 @@ class InstanceSummarySerializer(serializers.HyperlinkedModelSerializer):
             'identity',
             'user',
             'provider',
+            'image',
             'start_date',
             'end_date',
         )
