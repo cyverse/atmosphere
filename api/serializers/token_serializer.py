0763fe8e047878c02c2b5ae43e0de2fd1060b8fb
Steve Gregory <contact@steve-gregory.com>
2015-03-03 15:45:25 -0700

    Missing serializer

---
+++ api/serializers/token_serializer.py
@@ -0,0 +1,11 @@
+from authentication import Token
+from rest_framework import serializers
+
+class TokenSerializer(serializers.ModelSerializer):
+    token = serializers.CharField(read_only=True, source='key')
+    username = serializers.CharField(read_only=True, source='user.username')
+    expires = serializers.CharField(read_only=True, source='get_expired_time')
+
+    class Meta:
+        model = Token
+        fields = ('token', 'username', 'expires')
