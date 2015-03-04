01f6e4061e1d68b91eb206ca1e1b81a075cf8e56
jchansen <jason_hansen@outlook.com>
2015-03-03 21:40:32 -0700

    add relations to project instance/volume serializers

--- api/v2/serializers/details/project_volume.py
+++ api/v2/serializers/details/project_volume.py
@@ -1,15 +1,38 @@
-from core.models import Project
+from core.models import ProjectVolume, Project, Volume
 from rest_framework import serializers
-from ..summaries import ProjectSummarySerializer, VolumeSummarySerializer
+from ..summaries import ProjectSummarySerializer
+from ..details import VolumeSerializer
+
+
+class ProjectRelatedField(serializers.PrimaryKeyRelatedField):
+
+    def get_queryset(self):
+        return Project.objects.all()
+
+    def to_representation(self, value):
+        project = Project.objects.get(pk=value.pk)
+        serializer = ProjectSummarySerializer(project, context=self.context)
+        return serializer.data
+
+
+class VolumeRelatedField(serializers.PrimaryKeyRelatedField):
+
+    def get_queryset(self):
+        return Volume.objects.all()
+
+    def to_representation(self, value):
+        instance = Volume.objects.get(pk=value.pk)
+        serializer = VolumeSerializer(instance, context=self.context)
+        return serializer.data
 
 
 class ProjectVolumeSerializer(serializers.HyperlinkedModelSerializer):
-    project = ProjectSummarySerializer()
-    volume = VolumeSummarySerializer()
+    project = ProjectSummarySerializer(queryset=Project.objects.none())
+    volume = VolumeRelatedField(queryset=Volume.objects.none())
 
     class Meta:
-        model = Project
-        view_name = 'api_v2:project-detail'
+        model = ProjectVolume
+        view_name = 'api_v2:projectvolume-detail'
         fields = (
             'id',
             'url',
