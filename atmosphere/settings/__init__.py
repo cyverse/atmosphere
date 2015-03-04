32403854351bb68d08f931469137cb517a27adb4
Steve Gregory <contact@steve-gregory.com>
2015-03-04 09:22:59 -0700

    SECRET_KEY deployment moved to settings/__init__.py

--- atmosphere/settings/__init__.py
+++ atmosphere/settings/__init__.py
@@ -125,10 +125,6 @@ STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, "resources"),
 )
 
-# Make this unique, and don't share it with anybody.
-# NOTE: This value is not used, check local.py!
-SECRET_KEY = None
-
 # This key however should stay the same, and be shared with all Atmosphere
 ATMOSPHERE_NAMESPACE_UUID = UUID("40227dff-dedf-469c-a9f8-1953a7372ac1")
 
@@ -488,6 +484,31 @@ CELERY_ROUTES += ({
 
 import djcelery
 djcelery.setup_loader()
+
+"""
+For generating a unique SECRET_KEY -- Used by Django in various ways.
+"""
+#This Method will generate SECRET_KEY and write it to file..
+def generate_secret_key(secret_key_path):
+    from django.utils.crypto import get_random_string
+    from datetime import datetime
+    chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+    secret_value = get_random_string(50, chars)
+    comment_block = "\"\"\"\nThis file was Auto-Generated on %s\n\"\"\"\n" % datetime.now()
+    with open(secret_key_path,"w") as key_file:
+        key_file.write(comment_block)
+        key_file.write("SECRET_KEY=\"%s\"\n" % secret_value)
+
+#This import will Use an existing SECRET_KEY, or Generate your SECRET_KEY if it doesn't exist yet.
+try:
+    from .secret_key import SECRET_KEY
+except ImportError:
+    SETTINGS_DIR=os.path.abspath(os.path.dirname(__file__))
+    generate_secret_key(os.path.join(SETTINGS_DIR, 'secret_key.py'))
+    try:
+        from .secret_key import SECRET_KEY
+    except ImportError:
+        raise Exception("__init__.py could not generate a SECRET_KEY in secret_key.py")
 
 """
 Import local settings specific to the server, and secrets not checked into Git.
