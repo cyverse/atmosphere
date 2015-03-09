1605f21ceb30e9727587fb0bcd98f9eb43ff0a07
Evan Briones <erbriones@gmail.com>
2015-03-09 10:20:54 -0700

    remove old import
    	modified:   core/factories/__init__.py

--- core/factories/__init__.py
+++ core/factories/__init__.py
@@ -4,5 +4,5 @@ from core.factories.account import AccountProviderFactory
 from core.factories.group import GroupWithDataFactory
 from core.factories.user import AtmosphereUserFactory
 from core.factories.identity import IdentityFactory
-from core.factories.provider import TraitFactory, PlatformTypeFactory,\
+from core.factories.provider import PlatformTypeFactory,\
     ProviderTypeFactory, ProviderFactory
