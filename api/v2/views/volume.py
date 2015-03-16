e5e71c43e344506bd84442208cd13113eebd05d6
Steve Gregory <contact@steve-gregory.com>
2015-03-16 10:49:43 -0700

    Removed django_filter while using Django >1.8

--- api/v2/views/volume.py
+++ api/v2/views/volume.py
@@ -1,17 +1,17 @@
-import django_filters
 from rest_framework import viewsets
 from core.models import Volume
 from api.v2.serializers.details import VolumeSerializer
 from core.query import only_current_source
+# NOTE: Re-Add code when django_filters is >1.8 Compatible
+#import django_filters
 
-
-class VolumeFilter(django_filters.FilterSet):
-    min_size = django_filters.NumberFilter(name="size", lookup_type='gte')
-    max_size = django_filters.NumberFilter(name="size", lookup_type='lte')
-
-    class Meta:
-        model = Volume
-        fields = ['min_size', 'max_size', 'projects']
+#class VolumeFilter(django_filters.FilterSet):
+#    min_size = django_filters.NumberFilter(name="size", lookup_type='gte')
+#    max_size = django_filters.NumberFilter(name="size", lookup_type='lte')
+#
+#    class Meta:
+#        model = Volume
+#        fields = ['min_size', 'max_size', 'projects']
 
 
 class VolumeViewSet(viewsets.ModelViewSet):
@@ -20,7 +20,7 @@ class VolumeViewSet(viewsets.ModelViewSet):
     """
     queryset = Volume.objects.all()
     serializer_class = VolumeSerializer
-    filter_class = VolumeFilter
+    #filter_class = VolumeFilter
     http_method_names = ['get', 'put', 'patch', 'head', 'options', 'trace']
 
     def get_queryset(self):
