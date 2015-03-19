974739b0c65ba0d0bece5f762d4da1431f480496
jchansen <jason_hansen@outlook.com>
2015-03-19 14:45:10 -0700

    make version endpoint public again

--- api/version.py
+++ api/version.py
@@ -4,11 +4,13 @@ Atmosphere API version service.
 """
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from atmosphere.version import get_version
 
 
 class Version(APIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
     def get(self, request, format=None):
         """
         This request will retrieve Atmosphere's version,
