f16ed41c0f4b8350db5d6cdc7d6069032d14e5df
jchansen <jason_hansen@outlook.com>
2015-03-25 11:18:17 -0700

    add v2 status type summary serializer

---
+++ api/v2/serializers/summaries/status_type.py
@@ -0,0 +1,16 @@
+from core.models.status_type import StatusType
+from rest_framework import serializers
+
+
+class StatusTypeSummarySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = StatusType
+        view_name = 'api_v2:statustype-detail'
+        fields = (
+            'id',
+            'url',
+            'name',
+            'description',
+            'start_date',
+            'end_date'
+        )
