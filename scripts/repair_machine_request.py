391c1a9a5c9ff8d9fc16fac78a3ed8d6e9170755
Steve Gregory <contact@steve-gregory.com>
2015-03-25 18:01:08 -0700

    Both scripts verified and ready for calico

--- scripts/repair_machine_request.py
+++ scripts/repair_machine_request.py
@@ -9,41 +9,60 @@ from core.models import Provider, ProviderMachine, Identity, MachineRequest, App
 from core.models.application import _generate_app_uuid
 from core.models.machine_request import _update_application, _create_new_application
 
+Force = False
+
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--force", action="store_true",
+                        help="Force-repair requests that appear to be 'OK'")
     parser.add_argument("--provider", type=int,
                         help="Atmosphere provider ID"
                         " to use.")
     parser.add_argument("--request_ids", required=True,
                         help="Machine Request DB ID(s) to be repaired. (Comma-Separated)")
+
     args = parser.parse_args()
+    if args.force:
+        Force = True
+
     if not args.provider:
         provider = Provider.objects.get(location='iPlant Cloud - Tucson')
     else:
         provider = Provider.objects.get(id=args.provider)
+
     requests = args.request_ids.split(",")
     fix_requests(provider, requests)
 
 
-def fix_requests(provider, request=[]):
+def fix_requests(provider, requests=[]):
     accounts = get_account_driver(provider)
     for request_id in requests:
         try:
-            mr = MachineRequest.objects.get(id=request_id)
+            machine_request = MachineRequest.objects.get(id=request_id)
         except MachineRequest.DoesNotExist:
             print "Warn: MachineRequest by this ID could not be found: %s" % request_id
             continue
-        if mr.new_machine_provider != provider:
-            raise ValueError("MachineRequest ID:%s is for Provider:%s. Testing Provider is:%s" % (request_id, mr.new_machine_provider, provider))
+        if machine_request.new_machine_provider != provider:
+            raise ValueError("MachineRequest ID:%s is for Provider:%s. Testing Provider is:%s" % (request_id, machine_request.new_machine_provider, provider))
         try:
-            if mr.new_machine:
-                continue
-            except 
-        if mr.new_machine.provider != provider:
-            print and mr.new_machine
-        actual_machine = _find_machine_by_request(mr)
-        mr.new_machine = actual_machine
-        mr.save()
+            new_machine = ProviderMachine.objects.get(id=machine_request.new_machine_id)
+            if new_machine.provider == provider:
+                if not Force:
+                    print "OK: MachineRequest: %s 'new_machine' %s matches provider. use --force to continue fixing this request" % (machine_request, new_machine)
+                    continue
+            print "OK: This MachineRequest: %s has a BAD 'new_machine':%s" \
+                  "\tProvider should be %s." % (machine_request, new_machine, provider)
+        except ProviderMachine.DoesNotExist, no_match:
+            print "OK: This MachineRequest has a BAD 'new_machine' (DoesNotExist)"
+            new_machine = None
+        actual_image = _find_machine_by_request(machine_request)
+        actual_machine = ProviderMachine.objects.get(identifier=actual_image.id, provider=provider)
+        print "FIXED: Found correct 'new_machine': %s-->%s" % (new_machine, actual_machine)
+        machine_request.new_machine = actual_machine
+        machine_request.save()
+        if 'ramdisk_id' not in actual_image.properties or 'kernel_id' not in actual_image.properties:
+            print "WARN: Kernel/Ramdisk potentially missing. Run './scripts/repair_provider_machine.py --image_ids %s"\
+                % (actual_image.id)
 
 
 def _find_machine_by_request(machine_request):
@@ -54,12 +73,12 @@ def _find_machine_by_request(machine_request):
     lookup_name = machine_request.new_machine_name
     started = machine_request.start_date
     completed = machine_request.end_date
-    potential_matches = _match_by_date_range(image_list, started, completed)
+    potential_matches = _match_by_date_range(images, started, completed)
     count = len(potential_matches)
     if count < 1:
         raise Exception("Could not find an image to match this MachineRequest!")
     elif count == 1:
-        return pm
+        return potential_matches[0]
     else:
         return _match_by_name(potential_matches, lookup_name)
 
@@ -76,7 +95,7 @@ def _match_by_name(image_list, lookup_name):
     if count < 1:
         raise Exception("Could not find an image named %s!" % lookup_name)
     elif count == 1:
-        return pm
+        return matches[0]
     else:
         raise Exception("Ambiguity Error! Multiple images named %s: %s" % (lookup_name, matches))
 
