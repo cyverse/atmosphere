47c96cc5e5e02166b930a2b3ec24e8c5353461d0
Steve Gregory <contact@steve-gregory.com>
2015-04-29 12:08:38 -0700

    Stronger queryset for ProviderMachine

--- api/v2/views/provider_machine.py
+++ api/v2/views/provider_machine.py
@@ -1,5 +1,9 @@
+from django.db.models import Q
 from rest_framework import viewsets
+
 from core.models import ProviderMachine
+from core.query import only_current_source
+
 from api.v2.serializers.details import ProviderMachineSerializer
 
 
@@ -11,3 +15,19 @@ class ProviderMachineViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ProviderMachineSerializer
     search_fields = ('application__id', 'instance_source__created_by__username')
     filter_fields = ('application__id', 'instance_source__created_by__username')
+
+    def get_queryset(self):
+        request_user = self.request.user
+
+        #Showing non-end dated, public ProviderMachines
+        public_pms_set = ProviderMachine.objects.filter(only_current_source(), application__private=False)
+
+        #Showing non-end dated, public ProviderMachines
+        shared_pms_set = ProviderMachine.objects.filter(only_current_source(), members__in=request_user.group_set.values('id'))
+
+        #NOTE: Showing 'my pms' EVEN if they are end-dated.
+        my_pms_set = ProviderMachine.objects.filter(
+                Q(application__created_by=request_user) | Q(instance_source__created_by=request_user)
+            )
+        #Order them by date, make sure no dupes.
+        return (public_pms_set | shared_pms_set | my_pms_set).distinct().order_by('-instance_source__start_date')
