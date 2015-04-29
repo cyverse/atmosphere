221dc64079706eb7e81c739bf6bc216b42eb8bfa
Steve Gregory <contact@steve-gregory.com>
2015-04-29 09:54:27 -0700

    Added new fields to provider_machine summary&detail serializers.\n\nChanges related to the 'image_creator' code being developed on Troposphere.

--- api/v2/serializers/details/provider_machine.py
+++ api/v2/serializers/details/provider_machine.py
@@ -10,8 +10,12 @@ class ProviderMachineSerializer(serializers.HyperlinkedModelSerializer):
     created_by = UserSummarySerializer(source='instance_source.created_by')
     start_date = serializers.DateTimeField(source='instance_source.start_date')
     end_date = serializers.DateTimeField(source='instance_source.end_date')
+    #licenses = LicenseSerializer(many=True) #NEW
+    #members = MachineMembershipSerializer(many=True) #NEW
 
     class Meta:
         model = ProviderMachine
         view_name = 'api_v2:providermachine-detail'
-        fields = ('id', 'uuid', 'url', 'image', 'provider', 'created_by', 'start_date', 'end_date')
+        fields = ('id', 'uuid', 'url', 'image', 'provider',
+                'licenses', 'allow_imaging',
+                'created_by', 'start_date', 'end_date')
