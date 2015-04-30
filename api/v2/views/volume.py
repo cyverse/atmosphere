f81a6e503320e02e3c74bffc2bbbb435bf5af3a8
J. Matt Peterson <jmatt@jmatt.org>
2015-04-30 15:38:12 -0700

    Uncomment django_filters. Use new api.v2.base ViewSets.
    	  * PEP8 Compliance
    
    	modified:   api/v2/views/volume.py

--- api/v2/views/volume.py
+++ api/v2/views/volume.py
@@ -1,26 +1,28 @@
-from rest_framework import viewsets
+import django_filters
+
 from core.models import Volume
-from api.v2.serializers.details import VolumeSerializer
 from core.query import only_current_source
-# NOTE: Re-Add code when django_filters is >1.8 Compatible
-#import django_filters
 
-#class VolumeFilter(django_filters.FilterSet):
-#    min_size = django_filters.NumberFilter(name="size", lookup_type='gte')
-#    max_size = django_filters.NumberFilter(name="size", lookup_type='lte')
-#
-#    class Meta:
-#        model = Volume
-#        fields = ['min_size', 'max_size', 'projects']
+from api.v2.serializers.details import VolumeSerializer
+from api.v2.base import AuthViewSet
 
 
-class VolumeViewSet(viewsets.ModelViewSet):
+class VolumeFilter(django_filters.FilterSet):
+    min_size = django_filters.NumberFilter(name="size", lookup_type='gte')
+    max_size = django_filters.NumberFilter(name="size", lookup_type='lte')
+
+    class Meta:
+        model = Volume
+        fields = ['min_size', 'max_size', 'projects']
+
+
+class VolumeViewSet(AuthViewSet):
     """
     API endpoint that allows providers to be viewed or edited.
     """
     queryset = Volume.objects.all()
     serializer_class = VolumeSerializer
-    #filter_class = VolumeFilter
+    filter_class = VolumeFilter
     http_method_names = ['get', 'put', 'patch', 'head', 'options', 'trace']
 
     def get_queryset(self):
@@ -28,4 +30,5 @@ class VolumeViewSet(viewsets.ModelViewSet):
         Filter projects by current user
         """
         user = self.request.user
-        return Volume.objects.filter(only_current_source(), instance_source__created_by=user)
+        return Volume.objects.filter(only_current_source(),
+                                     instance_source__created_by=user)
