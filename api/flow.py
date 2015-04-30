e24f6a3798ca91f154861d31f889cbfdaadd6a12
J. Matt Peterson <jmatt@jmatt.org>
2015-04-29 18:28:43 -0700

    Refactor to use api.views.
    	modified:   api/flow.py

--- api/flow.py
+++ api/flow.py
@@ -1,28 +1,23 @@
 """
 Atmosphere api flow.
 """
-from rest_framework.views import APIView
 from rest_framework.response import Response
-
 
 from core.models.size import convert_esh_size
 
-from api.serializers import ProviderSizeSerializer
-
-from api import invalid_creds
-from api.permissions import InMaintenance, ApiAuthRequired
 from service.driver import prepare_driver
 
+from api import invalid_creds
+from api.serializers import ProviderSizeSerializer
+from api.views import AuthAPIView
 
-class FlowList(APIView):
+
+class FlowList(AuthAPIView):
     """
     List all active flows for a provider and identity.
     """
-    permission_classes = (ApiAuthRequired,)
 
     def get(self, request, provider_uuid, identity_uuid):
-        """
-        """
         user = request.user
         esh_driver = prepare_driver(request, provider_uuid, identity_uuid)
         if not esh_driver:
@@ -32,11 +27,10 @@ class FlowList(APIView):
         return response
 
 
-class Flow(APIView):
+class Flow(AuthAPIView):
     """
     View details on a flow.
     """
-    permission_classes = (ApiAuthRequired,)
 
     def get(self, request, provider_uuid, identity_uuid, flow_id):
         """
