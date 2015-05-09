16f531cb7e9d067725a4c25a4321773aada9616d
J. Matt Peterson <jmatt@jmatt.org>
2015-05-09 16:22:19 -0700

    Address @jchansen's requests. No dupes. POST for authorized users, PUT DELETE for cloud admins and staff.
    	modified:   api/v2/views/tag.py

--- api/v2/views/tag.py
+++ api/v2/views/tag.py
@@ -1,11 +1,13 @@
+from threepio import logger
 from core.models import Tag
 
-from api.permissions import CloudAdminRequired
+from api.permissions import ApiAuthRequired, CloudAdminRequired,\
+    InMaintenance
 from api.v2.serializers.summaries import TagSummarySerializer
-from api.v2.views.base import AuthReadOnlyViewSet
+from api.v2.views.base import AuthOptionalViewSet
 
 
-class TagViewSet(AuthReadOnlyViewSet):
+class TagViewSet(AuthOptionalViewSet):
     """
     API endpoint that allows tags to be viewed or edited.
     """
@@ -15,9 +17,19 @@ class TagViewSet(AuthReadOnlyViewSet):
     max_paginate_by = 1000
 
     def perform_create(self, serializer):
+        same_name_tags = Tag.objects.filter(
+            name__iexact=serializer.validated_data.get("name"))
+        if same_name_tags:
+            raise serializers.ValidationError(
+                "A tag with this name already exists: %s" %
+                same_name_tags.first().name)
         serializer.save(user=self.request.user)
 
     def get_permissions(self):
-        if self.request.method in ["POST", "PUT", "DELETE"]:
-            self.permission_classes = (CloudAdminRequired,)
+        if self.request.method is "":
+            self.permission_classes = (ApiAuthRequired,
+                                       InMaintenance,)
+        if self.request.method in ["PUT", "DELETE"]:
+            self.permission_classes = (CloudAdminRequired,
+                                       InMaintenance,)
         return super(TagViewSet, self).get_permissions()
