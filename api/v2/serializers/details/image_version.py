d5a017ea28ee07e46649648bd80b08eb52026184
Steve Gregory <contact@steve-gregory.com>
2015-05-10 12:31:33 -0700

    Add membership to summary/detail serializer for image_version; Remove queryset and place under 'read_only' for now.

--- api/v2/serializers/details/image_version.py
+++ api/v2/serializers/details/image_version.py
@@ -4,9 +4,6 @@ from api.v2.serializers.summaries import UserSummarySerializer, LicenseSerialize
 
 
 class ImageVersionSerializer(serializers.HyperlinkedModelSerializer):
-    def get_membership_queryset(self, *args, **kwargs):
-        import ipdb;ipdb.set_trace()
-        return ApplicationVersion.objects.all()
 
     # id, application
     fork_version = serializers.HyperlinkedRelatedField(
@@ -14,7 +11,7 @@ class ImageVersionSerializer(serializers.HyperlinkedModelSerializer):
         read_only=True)
     #name, description, icon, allow_imaging
     licenses = LicenseSerializer(many=True, read_only=True) #NEW
-    membership = serializers.SlugRelatedField(slug_field='name', queryset=get_membership_queryset, many=True) #NEW
+    membership = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True) #NEW
     created_by = UserSummarySerializer(source='application.created_by')
     start_date = serializers.DateTimeField()
     end_date = serializers.DateTimeField()
--- api/v2/serializers/summaries/image_version.py
+++ api/v2/serializers/summaries/image_version.py
@@ -1,12 +1,12 @@
 from core.models import Application as Image
 from rest_framework import serializers
 
-
 class ImageVersionSummarySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.CharField(read_only=True) # Required when its a uuid -- otherwise LONGINT
     icon = serializers.CharField(source='icon_url', read_only=True)
+    membership = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True) #NEW
 
     class Meta:
         model = Image
         view_name = 'api_v2:imageversion-detail'
-        fields = ('id', 'url', 'name', 'description', 'icon', 'start_date', 'end_date',)
+        fields = ('id', 'url', 'name', 'description', 'icon', 'start_date', 'end_date','membership')
