d195346fe6ace96116c23a6596acc1c86074d098
Steve Gregory <contact@steve-gregory.com>
2015-06-02 11:42:21 -0700

    Merged cherry-picking

--- api/v2/views/user.py
+++ api/v2/views/user.py
@@ -2,6 +2,34 @@ from core.models import AtmosphereUser
 from api.v2.serializers.details import UserSerializer
 from api.v2.views.base import AuthReadOnlyViewSet
 
+from rest_framework.filters import SearchFilter
+from django.utils import six
+from django.db.models import Q
+import operator
+
+
+class MinLengthRequiredSearchFilter(SearchFilter):
+    def filter_queryset(self, request, queryset, view):
+        search_fields = getattr(view, 'search_fields', None)
+        min_length = getattr(view, 'min_search_length', 3)
+        if not search_fields:
+            return queryset
+        orm_lookups = [self.construct_search(six.text_type(search_field))
+                       for search_field in search_fields]
+        for search_term in self.get_search_terms(request):
+            #Skip 'search_term' if its  too small to evaluate.
+            if len(search_term) < min_length:
+                #Forces 0 results if below the requirement.
+                queryset = queryset.none()
+            or_queries = [Q(**{orm_lookup: search_term})
+                          for orm_lookup in orm_lookups]
+            reduced_query = reduce(operator.or_, or_queries)
+            queryset = queryset.filter(reduced_query).distinct()
+        return queryset
+    class Meta:
+        model = AtmosphereUser
+        fields = ["username", "email"]
+
 
 class UserViewSet(AuthReadOnlyViewSet):
     """
@@ -10,5 +38,7 @@ class UserViewSet(AuthReadOnlyViewSet):
 
     queryset = AtmosphereUser.objects.all()
     serializer_class = UserSerializer
-    filter_fields = ('email',)
+    filter_backends = (MinLengthRequiredSearchFilter,)
     http_method_names = ['get', 'head', 'options', 'trace']
+    search_fields = ('^username',)  # NOTE: ^ == Startswith searching
+    min_search_length = 2
