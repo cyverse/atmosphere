dd6e6f0ea2e2812b1e09b91dfaa8219847512ca9
J. Matt Peterson <jmatt@jmatt.org>
2015-06-16 18:19:28 -0700

    [ATMO-864] Add python LogAdapter to improve Ansible logging.
    	modified:   atmosphere/settings/__init__.py
    	new file:   core/logging.py
    	modified:   service/deploy.py
    	modified:   service/tasks/driver.py

---
+++ core/logging.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+import logging
+
+
+class InstanceAdapter(logging.LoggerAdapter):
+     def process(self, msg, kwargs):
+         fields = "|".join([self.extra['instance_id'],
+                           self.extra['ip_address'],
+                           self.extra['username']])
+         return '%s %s' % (fields, msg), kwargs 
+
+
+def create_instance_logger(logger, ip_address, username, instance_id):
+    adapter = InstanceAdapter(logger, {'instance_id': instance_id,
+                                       'ip_address': ip_address,
+                                       'username': username})
+    return adapter
