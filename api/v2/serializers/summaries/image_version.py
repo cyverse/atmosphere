2fc0a70a5507d8064104cc3dd670258b8072c600
Steve Gregory <contact@steve-gregory.com>
2015-07-07 09:01:10 -0700

    Fixed error related to bad 'view_name' on HyperlinkedRelatedField

--- api/v2/serializers/summaries/image_version.py
+++ api/v2/serializers/summaries/image_version.py
@@ -2,10 +2,7 @@ from core.models import ApplicationVersion as ImageVersion
 from rest_framework import serializers
 
 class ImageVersionSummarySerializer(serializers.HyperlinkedModelSerializer):
-    id = serializers.CharField(read_only=True) # Required when its a uuid -- otherwise LONGINT
-    membership = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True) #NEW-Bad form?
-
     class Meta:
         model = ImageVersion
         view_name = 'api:v2:imageversion-detail'
-        fields = ('id', 'url', 'name', 'change_log', 'allow_imaging', 'membership', 'start_date', 'end_date')
+        fields = ('id', 'url', 'name',)
