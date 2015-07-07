b6e459bdf27bff41fb073e3437d40dfdc5c9c124
Steve Gregory <contact@steve-gregory.com>
2015-07-07 08:35:14 -0700

    Serializers field names changed

--- api/v2/serializers/summaries/image_version.py
+++ api/v2/serializers/summaries/image_version.py
@@ -3,10 +3,9 @@ from rest_framework import serializers
 
 class ImageVersionSummarySerializer(serializers.HyperlinkedModelSerializer):
     id = serializers.CharField(read_only=True) # Required when its a uuid -- otherwise LONGINT
-    icon = serializers.CharField(source='icon_url', read_only=True)
     membership = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True) #NEW-Bad form?
 
     class Meta:
         model = ImageVersion
         view_name = 'api:v2:imageversion-detail'
-        fields = ('id', 'url', 'name', 'description', 'icon', 'allow_imaging', 'membership', 'start_date', 'end_date')
+        fields = ('id', 'url', 'name', 'change_log', 'allow_imaging', 'membership', 'start_date', 'end_date')
