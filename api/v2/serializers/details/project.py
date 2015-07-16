0e8fbf7ce3cf14dd41734364e2bc6475aff6d0c0
J. Matt Peterson <jmatt@jmatt.org>
2015-07-16 14:06:18 -0700

    PEP8 Compliance.
    	modified:   api/v2/serializers/details/project.py
    	modified:   api/v2/views/image.py

--- api/v2/serializers/details/project.py
+++ api/v2/serializers/details/project.py
@@ -1,6 +1,7 @@
 from core.models import Project
 from rest_framework import serializers
-from api.v2.serializers.summaries import InstanceSummarySerializer, VolumeSummarySerializer, ImageSummarySerializer
+from api.v2.serializers.summaries import InstanceSummarySerializer,\
+    VolumeSummarySerializer, ImageSummarySerializer
 from api.v2.serializers.fields import UserRelatedField
 
 
@@ -11,9 +12,9 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
                                       many=True, read_only=True)
     images = ImageSummarySerializer(source='applications',
                                     many=True, read_only=True)
-    # note: both of these requests become a single DB query, but I'm choosing the
-    # owner.name route so the API doesn't break when we start adding users to groups
-    # owner = UserSerializer(source='owner.user_set.first')
+    # note: both of these requests become a single DB query, but I'm choosing
+    # the owner.name route so the API doesn't break when we start adding users
+    # to groups owner = UserSerializer(source='owner.user_set.first')
     owner = UserRelatedField(source='owner.name')
 
     class Meta:
