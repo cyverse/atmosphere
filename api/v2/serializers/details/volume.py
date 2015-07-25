09f4ddeb0782212f265b057ce7f57b9e056ea287
Evan Briones <erbriones@gmail.com>
2015-07-24 20:42:02 -0700

    [ATMO-917] add ability to create new volumes
    * Add `POST` support to volumes endpoint
    * Add custom volume creation
    * Add custom validation to serializer
    * Update serializer fields to support writing

--- api/v2/serializers/details/volume.py
+++ api/v2/serializers/details/volume.py
@@ -1,37 +1,95 @@
 from core.models import Volume
 from rest_framework import serializers
+from api.v2.serializers.fields import ModelRelatedField
 from api.v2.serializers.summaries import (
+    IdentitySummarySerializer,
     ProviderSummarySerializer,
-    UserSummarySerializer,
-    IdentitySummarySerializer
+    UserSummarySerializer
 )
+from core.models import Identity, Provider
+from core.models.instance_source import InstanceSource
 
 
 class VolumeSerializer(serializers.HyperlinkedModelSerializer):
-    provider = ProviderSummarySerializer(source='instance_source.provider')
-    identity = IdentitySummarySerializer(
-        source='instance_source.created_by_identity')
-    user = UserSummarySerializer(source='instance_source.created_by')
-    start_date = serializers.DateTimeField(source='instance_source.start_date')
-    end_date = serializers.DateTimeField(source='instance_source.end_date')
+    description = serializers.CharField(required=False)
+
+    identity = ModelRelatedField(source="instance_source.created_by_identity",
+                                 queryset=Identity.objects.all(),
+                                 serializer_class=IdentitySummarySerializer,
+                                 style={'base_template': 'input.html'})
+
+    provider = ModelRelatedField(source="instance_source.provider",
+                                 queryset=Provider.objects.all(),
+                                 serializer_class=ProviderSummarySerializer,
+                                 style={'base_template': 'input.html'})
+
+    user = UserSummarySerializer(source='instance_source.created_by',
+                                 read_only=True)
+
+    uuid = serializers.CharField(source='instance_source.identifier',
+                                 read_only=True)
+
     projects = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
-    uuid = serializers.CharField(source='instance_source.identifier')
+    snapshot_id = serializers.CharField(write_only=True, required=False)
+    image_id = serializers.CharField(write_only=True, required=False)
 
     class Meta:
         model = Volume
         view_name = 'api:v2:volume-detail'
+        read_only_fields = ("user", "uuid", "start_date", "end_date")
         fields = (
             'id',
             'uuid',
-            'url',
             'name',
-            'size',
+            'description',
+            'identity',
             'user',
             'provider',
-            'identity',
             'projects',
+            'size',
+            'url',
+
+            'snapshot_id',
+            'image_id',
+
             'start_date',
             'end_date')
+
+    def validate(self, data):
+        image_id = data.get('image')
+        snapshot_id = data.get('snapshot')
+
+        #: Only allow one at a time
+        if snapshot_id and image_id:
+            raise serializers.ValidationError(
+                "Use either `snapshot_id` or `image_id` not both.")
+        return data
+
+    def create(self, validated_data):
+        name = validated_data.get('name')
+        size = validated_data.get('size')
+        description = validated_data.get('description')
+        identifier = validated_data.get("identifier")
+        provider = validated_data.get("provider")
+        user = validated_data.get("user")
+        identity = validated_data.get("identity")
+        start_date = validated_data.get("created_on")
+
+        source = InstanceSource.objects.create(
+            identifier=identifier,
+            provider=provider,
+            created_by=user,
+            created_by_identity=identity)
+
+        kwargs = {
+            "name": name,
+            "size": size,
+            "description": description,
+            "instance_source": source,
+            "start_date": start_date
+        }
+
+        return Volume.objects.create(**kwargs)
 
     def update(self, instance, validated_data):
         if 'instance_source' in validated_data:
--- api/v2/views/volume.py
+++ api/v2/views/volume.py
@@ -1,14 +1,18 @@
-from django.db.models import Q
-from django.utils import timezone
-
 import django_filters
-
-from core.models import Volume
-from api.v2.serializers.details import VolumeSerializer
-from core.query import only_current_source
+import pytz
+from django.utils import timezone
+from libcloud.common.types import InvalidCredsError, MalformedResponseError
+from rest_framework import exceptions
 
 from api.v2.serializers.details import VolumeSerializer
 from api.v2.views.base import AuthViewSet
+from core.models import Volume
+from core.query import only_current_source
+from service.volume import create_volume_or_fail
+from service.exceptions import OverQuotaError
+from rtwo.exceptions import ConnectionFailure
+
+VOLUME_EXCEPTIONS = (OverQuotaError, ConnectionFailure, MalformedResponseError)
 
 
 class VolumeFilter(django_filters.FilterSet):
@@ -27,7 +31,8 @@ class VolumeViewSet(AuthViewSet):
     """
     serializer_class = VolumeSerializer
     filter_class = VolumeFilter
-    http_method_names = ['get', 'put', 'patch', 'head', 'options', 'trace']
+    http_method_names = ('get', 'post', 'put', 'patch', 'head', 'options',
+                         'trace')
 
     def get_queryset(self):
         """
@@ -37,3 +42,32 @@ class VolumeViewSet(AuthViewSet):
         return Volume.objects.filter(
             only_current_source(),
             instance_source__created_by=user)
+
+    def perform_create(self, serializer):
+        data = serializer.validated_data
+        name = data.get('name')
+        size = data.get('size')
+        description = data.get('description')
+        image_id = data.get('image')
+        snapshot_id = data.get('snapshot')
+        # FIXME: Determine what a sane default is
+        instance_source = data.get("instance_source")
+        identity = instance_source.get("created_by_identity")
+        provider = instance_source.get('provider')
+
+        try:
+            esh_volume = create_volume_or_fail(name, size, self.request.user,
+                                               provider, identity,
+                                               description=description,
+                                               image_id=image_id,
+                                               snapshot_id=snapshot_id)
+            created_on = esh_volume.extra.get("createTime", timezone.now())
+            serializer.save(identifier=esh_volume.id,
+                            name=esh_volume.name,
+                            created_on=pytz.utc.localize(created_on),
+                            user=self.request.user,
+                            identity=identity)
+        except InvalidCredsError as e:
+            raise exceptions.PermissionDenied(detail=e.message)
+        except VOLUME_EXCEPTIONS as e:
+            raise exceptions.ParseError(detail=e.message)
