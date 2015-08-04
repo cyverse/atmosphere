b058784145ef7edf497a2db3fbd2ab67b9d05a12
J. Matt Peterson <jmatt@jmatt.org>
2015-08-04 16:03:44 -0700

    Implement generate_configs method.
    	  * Generate configuration files from templates.
    
    	modified:   scripts/generate_configs.py

--- scripts/generate_configs.py
+++ scripts/generate_configs.py
@@ -6,9 +6,11 @@ from jinja2 import Environment, FileSystemLoader, meta, StrictUndefined,\
 
 
 config_files = {
-    'nginx': 'extras/nginx/Makefile.j2',
-    'apache': 'extras/apache/atmo.conf.j2',
-    'local': 'atmosphere/settings/local.py.j2'}
+    # semantic_name: (template_location, output_location)
+    'nginx': ('extras/nginx/Makefile.j2', 'extras/nginx/Makefile'),
+    'apache': ('extras/apache/atmo.conf.j2', 'extras/apache/atmo.conf'),
+    'local': ('atmosphere/settings/local.py.j2',
+              'atmosphere/settings/local.py')}
 
 
 projectpath = os.path.abspath(
@@ -22,9 +24,13 @@ env = Environment(loader=loader,
                   undefined=StrictUndefined)
 
 
+context = {}
+
+
 def handle_preconditions():
-    result = True
-    for file_location in config_files.values():
+    return True
+    success = True
+    for file_location, _ in config_files.values():
         try:
             file_path = os.path.join(projectpath,
                                      file_location)
@@ -34,16 +40,41 @@ def handle_preconditions():
             if ud_vars:
                 print 'Undeclared variables found in %s: %s' % (file_path,
                                                                 ud_vars)
-                result = False
+                success = False
         except TemplateNotFound:
             print 'Template not found: %s' % (file_path)
-            result = False
-    return result
+            success = False
+    return success
+
+
+def generate_configs():
+    success = True
+    for template_location, output_location in config_files.values():
+        try:
+            output_path = os.path.join(projectpath,
+                                       output_location)
+            template = env.get_template(template_location)
+            rendered = template.render(context)
+            with open(output_path, 'wb') as fh:
+                fh.write(rendered)
+            print 'From %s '\
+                'generated %s' % (template_location, output_location)
+        except Exception as e:
+            print 'Exception %s from template '\
+                'location %s and output location '\
+                '%s' % (e.message, template_location, output_location)
+            success = False
+    return success
 
 
 def main():
-    if not handle_preconditions():
-        exit(1)
+    i = 1
+    for step in [handle_preconditions, generate_configs]:
+        success = step()
+        if not success:
+            exit(i)
+        i += 1
+    exit(0)
 
 
 if __name__ == '__main__':
