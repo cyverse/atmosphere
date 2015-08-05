142e361d2bcfbdc15939ad33c600bf943025f7b1
Steve Gregory <contact@steve-gregory.com>
2015-08-05 16:29:58 -0700

    Remove final references to application

--- api/v1/serializers/no_project_serializer.py
+++ api/v1/serializers/no_project_serializer.py
@@ -1,21 +1,13 @@
 from core.models.user import AtmosphereUser
 from core.query import only_current, only_current_source
 from rest_framework import serializers
-from .application_serializer import ApplicationSerializer
 from .instance_serializer import InstanceSerializer
 from .volume_serializer import VolumeSerializer
 
 
 class NoProjectSerializer(serializers.ModelSerializer):
-    applications = serializers.SerializerMethodField('get_user_applications')
     instances = serializers.SerializerMethodField('get_user_instances')
     volumes = serializers.SerializerMethodField('get_user_volumes')
-
-    def get_user_applications(self, atmo_user):
-        return [ApplicationSerializer(
-            item,
-            context={'request': self.context.get('request')}).data for item in
-            atmo_user.application_set.filter(only_current(), projects=None)]
 
     def get_user_instances(self, atmo_user):
         return [InstanceSerializer(
@@ -34,4 +26,4 @@ class NoProjectSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AtmosphereUser
-        fields = ('applications', 'instances', 'volumes')
+        fields = ('instances', 'volumes')
