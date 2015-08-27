4f6b9eb30e53c3b2c3dbacb63ec8b70f2dd16ab0
Steve Gregory <contact@steve-gregory.com>
2015-08-27 09:49:59 -0700

    PEP8 + Replace ModelRelated for SummarySerializer (No updating necessary)

--- api/v2/serializers/details/instance_history.py
+++ api/v2/serializers/details/instance_history.py
@@ -1,36 +1,33 @@
 from rest_framework import serializers
 
-from core.models import Instance, InstanceStatusHistory, Size, Provider, Application as Image
+from core.models import Instance, InstanceStatusHistory, Size
 
 from api.v2.serializers.summaries import (
-    InstanceSuperSummarySerializer, ProviderSummarySerializer, ImageSummarySerializer)
+    InstanceSuperSummarySerializer,
+    ProviderSummarySerializer, ImageSummarySerializer)
 from api.v2.serializers.summaries.size import SizeRelatedField
-from api.v2.serializers.fields import ModelRelatedField
+
 
 class InstanceRelatedField(serializers.PrimaryKeyRelatedField):
 
     def to_representation(self, value):
         instance = Instance.objects.get(pk=value.pk)
-        # important! We have to use the SuperSummary because there are non-end_dated
+        # important! We have to use the SuperSummary
+        # because there are non-end_dated
         # instances that don't have a valid size (size='Unknown')
         serializer = InstanceSuperSummarySerializer(
             instance,
             context=self.context)
         return serializer.data
 
+
 class InstanceStatusHistorySerializer(serializers.HyperlinkedModelSerializer):
     instance = InstanceRelatedField(queryset=Instance.objects.none())
     size = SizeRelatedField(queryset=Size.objects.none())
-    provider = ModelRelatedField(
-        source='instance.provider_machine.provider',
-        queryset=Provider.objects.all(),
-        serializer_class=ProviderSummarySerializer,
-        style={'base_template': 'input.html'})
-    image = ModelRelatedField(
-        source='instance.provider_machine.application_version.application',
-        queryset=Image.objects.all(),
-        serializer_class=ImageSummarySerializer,
-        style={'base_template': 'input.html'})
+    provider = ProviderSummarySerializer(
+        source='instance.provider_machine.provider')
+    image = ImageSummarySerializer(
+        source='instance.provider_machine.application_version.application')
     status = serializers.SlugRelatedField(slug_field='name', read_only=True)
 
     class Meta:
