db742dbf035ef11fbb9a1f90ccec27bc82ecbe7e
prosif <prosif@gmail.com>
2015-09-11 16:24:07 -0700

    remove old commented code

--- api/v2/views/machine_request.py
+++ api/v2/views/machine_request.py
@@ -8,53 +8,15 @@ from api.v2.serializers.details import MachineRequestSerializer,\
 from api.v2.views.base import BaseRequestViewSet
 
 class MachineRequestViewSet(BaseRequestViewSet):
+    
     def get_queryset(self):
         assert self.model is not None, (
             "%s should include a `model` attribute."
             %self.__class__.__name__
         )
         return self.model.objects.all()
-    #pass
+
     queryset = MachineRequest.objects.none()
     model = MachineRequest
     serializer_class = UserMachineRequestSerializer
     admin_serializer_class = MachineRequestSerializer
-
-# class ResourceRequestViewSet(BaseRequestViewSet):
-
-#     """
-#     API endpoint that allows resource request to be viewed or edited.
-#     """
-#     queryset = ResourceRequest.objects.none()
-#     model = ResourceRequest
-#     serializer_class = UserResourceRequestSerializer
-#     admin_serializer_class = ResourceRequestSerializer
-#     filter_fields = ('status__id', 'status__name')
-
-#     def submit_action(self, instance):
-#         """
-#         Submits a resource request email
-#         """
-#         requested_resource = instance.request
-#         reason_for_request = instance.description
-#         username = self.request.user.username
-#         resource_request_email(self.request, username, requested_resource,
-#                                reason_for_request)
-
-#     def approve_action(self, instance):
-#         """
-#         Updates the resource for the request
-#         """
-#         membership = instance.membership
-#         membership.quota = instance.quota
-#         membership.allocation = instance.allocation
-#         membership.approve_quota(instance.id)
-#         membership.save()
-
-#     def deny_action(self, instance):
-#         """
-#         Notify the user that the request was denied
-#         """
-#         send_denied_resource_email(user=instance.created_by,
-#                                    request=instance.request,
-#                                    reason=instance.admin_message)
