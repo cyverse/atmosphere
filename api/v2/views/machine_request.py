d7103aa54adfaeea19739c13ebd0f916789a330a
prosif <prosif@gmail.com>
2015-09-14 12:14:45 -0700

    only show incomplete requests, allow filtering by username

--- api/v2/views/machine_request.py
+++ api/v2/views/machine_request.py
@@ -3,6 +3,8 @@ from core.email import send_denied_resource_email
 
 from web.emails import resource_request_email
 
+from django.db.models import Q
+
 from api.v2.serializers.details import MachineRequestSerializer,\
     UserMachineRequestSerializer
 from api.v2.views.base import BaseRequestViewSet
@@ -10,11 +12,14 @@ from api.v2.views.base import BaseRequestViewSet
 class MachineRequestViewSet(BaseRequestViewSet):
     
     def get_queryset(self):
+        username = self.request.query_params.get('username')
         assert self.model is not None, (
             "%s should include a `model` attribute."
             %self.__class__.__name__
         )
-        return self.model.objects.all()
+        if(username is not None):
+            return self.model.objects.filter(new_machine_owner__username=username)
+        return self.model.objects.filter(~Q(status__contains="completed"))
 
     queryset = MachineRequest.objects.none()
     model = MachineRequest
