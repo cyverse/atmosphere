ee1f958cb3611ecc3af0329deda7fde5d5281c32
Evan Briones <erbriones@gmail.com>
2015-09-14 14:32:08 -0700

    remove obsolete model creation

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+"""
+Collection of models
+"""
 from core.models.allocation_strategy import Allocation, AllocationStrategy
 from core.models.application import Application, ApplicationMembership,\
     ApplicationScore, ApplicationBookmark
@@ -32,66 +37,3 @@ from core.models.t import T
 from core.models.tag import Tag
 from core.models.user import AtmosphereUser
 from core.models.volume import Volume
-
-
-def get_or_create(Model, *args, **kwargs):
-    return Model.objects.get_or_create(*args, **kwargs)[0]
-
-
-def create_machine_model(name, provider, provider_alias,
-                         created_by, description):
-    name = _get_valid_name(name, provider_alias)
-    new_machine = get_or_create(Application,
-                                name=name,
-                                description=description,
-                                created_by=created_by)
-    provider_machine = get_or_create(ProviderMachine,
-                                     machine=new_machine,
-                                     provider=provider,
-                                     identifier=provider_alias)
-    return (new_machine, provider_machine)
-
-
-def get_or_create_instance_model(name, provider, provider_alias,
-                                 image_alias, ip_address, created_by):
-    name = _get_valid_name(name, provider_alias)
-    provider_machine = _get_or_create_provider_machine(
-        provider,
-        image_alias,
-        created_by
-    )
-    return get_or_create(Instance,
-                         name=name,
-                         provider_alias=provider_alias,
-                         provider_machine=provider_machine,
-                         ip_address=ip_address,
-                         created_by=created_by)
-
-
-def _get_valid_name(name, alias):
-    """
-    Make sure there is a good default name if no name exists.
-    """
-    if name is None or len(name) == 0:
-        name = alias
-    return name
-
-
-def _get_or_create_provider_machine(provider, image_alias, created_by):
-    """
-    Get or create a ProviderMachine.
-    If ProviderMachine does not already exist
-    create a new Machine and related ProviderMachine.
-    """
-    provider_machine = None
-    filtered_machines = ProviderMachine.objects.filter(identifier=image_alias)
-    if filtered_machines:
-        provider_machine = filtered_machines[0]
-    else:
-        (created, provider_machine) = create_machine_model(
-            None,
-            provider,
-            image_alias,
-            created_by,
-            "Created to support instanceModel")
-    return provider_machine
