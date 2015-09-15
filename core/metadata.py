05e9b8fefc66fee86f20a48b915ce386f4f333f2
Evan Briones <erbriones@gmail.com>
2015-09-15 14:25:17 -0700

    move private function into application
    Remove dead code in metadata and move private function into package
    its being used by.

--- core/metadata.py
+++ core/metadata.py
@@ -1,23 +1,9 @@
-from core.models.provider import AccountProvider, Provider
-from core.models.identity import Identity
+# -*- coding: utf-8 -*-
+"""
+Maintain objects metadata
+"""
+
 from threepio import logger
-
-
-def _get_machine_metadata(esh_driver, esh_machine):
-    if not hasattr(esh_driver._connection, 'ex_get_image_metadata'):
-        # NOTE: This can get chatty, only uncomment for debugging
-        # Breakout for drivers (Eucalyptus) that don't support metadata
-        # logger.debug("EshDriver %s does not have function 'ex_get_image_metadata'"
-        #            % esh_driver._connection.__class__)
-        return {}
-    try:
-        metadata = esh_driver._connection.ex_get_image_metadata(esh_machine)
-        return metadata
-    except Exception:
-        logger.exception(
-            'Warning: Metadata could not be retrieved for: %s' %
-            esh_machine)
-        return {}
 
 
 def update_machine_metadata(esh_driver, esh_machine, data={}):
@@ -48,33 +34,3 @@ def update_machine_metadata(esh_driver, esh_machine, data={}):
             return {}
         else:
             raise
-
-
-def _get_admin_owner(provider_uuid):
-    admin = AccountProvider.objects.filter(provider__uuid=provider_uuid)
-    if not admin:
-        logger.warn("AccountProvider could not be found for provider %s."
-                    " AccountProviders are necessary to claim ownership "
-                    " for identities that do not yet exist in the DB."
-                    % Provider.objects.get(uuid=provider_uuid))
-        return None
-    return admin[0].identity
-
-
-def _get_owner_identity(owner_name, provider_uuid):
-    original_owner = Identity.objects.filter(
-        provider__uuid=provider_uuid,
-        created_by__username=owner_name)
-    if original_owner:
-        owner_identity = original_owner[0]
-    else:
-        admin = _get_admin_owner(provider_uuid)
-        if not admin:
-            raise Exception(
-                "Original owner %s does not exist in DB and no "
-                "AccountProvider could be found to assume ownership."
-                "Select an Identity to be the AccountProvider or "
-                "create an Identity for %s."
-                % (owner_name, owner_name))
-        owner_identity = admin
-    return owner_identity
