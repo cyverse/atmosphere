678dd5bd2d70c56b4b59255f8310cc370d87d6e0
Steve Gregory <contact@steve-gregory.com>
2015-10-02 12:29:34 -0700

    Missing import

--- api/v1/views/email.py
+++ api/v1/views/email.py
@@ -5,6 +5,7 @@ from django.core import urlresolvers
 
 from rest_framework.response import Response
 from rest_framework import status
+from django.conf import settings
 
 from threepio import logger
 
