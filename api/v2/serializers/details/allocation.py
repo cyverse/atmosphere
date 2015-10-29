85c604225c08d94feb9fa51ecc4a4007d9cf08b0
Steve Gregory <contact@steve-gregory.com>
2015-10-29 08:36:57 -0700

    Add API v2 allocation support

--- api/v2/serializers/details/allocation.py
+++ api/v2/serializers/details/allocation.py
@@ -8,6 +8,18 @@ class AllocationSerializer(serializers.HyperlinkedModelSerializer):
         view_name='api:v2:allocation-detail',
     )
 
+    def validate_delta(self, value):
+        return self._is_positive_int('delta', value)
+
+    def validate_threshold(self, value):
+        return self._is_positive_int('threshold', value)
+
+    def _is_positive_int(self, key, value):
+        if type(value) != int or value < 1:
+            raise serializers.ValidationError(
+                "Value of %s should be >= 1" % key)
+        return value
+
     class Meta:
         model = Allocation
         fields = ('id', 'uuid', 'url', 'threshold', 'delta')
--- api/v2/views/allocation.py
+++ api/v2/views/allocation.py
@@ -3,13 +3,18 @@ from core.models import Allocation
 from api.v2.serializers.details import AllocationSerializer
 from api.v2.views.base import AuthViewSet
 from api.v2.views.mixins import MultipleFieldLookup
-
+from api.permissions import CanEditOrReadOnly
 class AllocationViewSet(MultipleFieldLookup, AuthViewSet):
 
     """
     API endpoint that allows providers to be viewed or edited.
+    NOTE: we have *INTENTIONALLY* left out the ability to *UPDATE* or *DELETE* a allocation.
+    This can have *disasterous cascade issues* on other fields. DONT DELETE or UPDATE allocation!
     """
     lookup_fields = ("id", "uuid")
     queryset = Allocation.objects.all()
     serializer_class = AllocationSerializer
-    http_method_names = ['get', 'head', 'options', 'trace']
+    permission_classes = (
+        CanEditOrReadOnly,
+    )
+    http_method_names = ['get', 'post', 'head', 'options', 'trace']
