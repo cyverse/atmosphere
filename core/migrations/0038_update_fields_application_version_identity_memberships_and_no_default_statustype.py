b60f5b655e5aa5ebec70a0d73e7242e98b3ddfde
Steve Gregory <contact@steve-gregory.com>
2015-11-06 10:32:08 -0700

    Re-organized migration so it would actually work

--- core/migrations/0038_update_fields_application_version_identity_memberships_and_no_default_statustype.py
+++ core/migrations/0038_update_fields_application_version_identity_memberships_and_no_default_statustype.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import uuid
 from django.db import migrations, models
 
 
@@ -11,11 +11,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
+        migrations.AlterField(
             model_name='applicationversionmembership',
-            name='image_version',
-            field=models.ForeignKey(db_column=b'application_version_id', default=-1, to='core.ApplicationVersion'),
+            name='application_version',
+            field=models.ForeignKey(db_column=b'application_version_id', default=uuid.uuid4, to='core.ApplicationVersion'),
             preserve_default=False,
+        ),
+        migrations.RenameField(
+            model_name='applicationversionmembership',
+            old_name='application_version',
+            new_name='image_version',
         ),
         migrations.AlterField(
             model_name='identitymembership',
@@ -40,9 +45,5 @@ class Migration(migrations.Migration):
         migrations.AlterUniqueTogether(
             name='applicationversionmembership',
             unique_together=set([('image_version', 'group')]),
-        ),
-        migrations.RemoveField(
-            model_name='applicationversionmembership',
-            name='application_version',
         ),
     ]
