4c1ea6dfe6c12669a5062a84ebce99a10cac5a1a
Andre Mercer <amercer1@email.arizona.edu>
2015-11-06 15:26:30 -0700

    Fixed API v2 machine_request serializer and viewset to accept arguments that are not part of the serializers validated data

--- api/v2/views/machine_request.py
+++ api/v2/views/machine_request.py
@@ -1,11 +1,15 @@
+from api import exceptions
 from api.v2.serializers.details import MachineRequestSerializer,\
     UserMachineRequestSerializer
 from api.v2.views.base import BaseRequestViewSet
+
+from core import exceptions as core_exceptions
 from core.email import send_denied_resource_email
-from core.models import MachineRequest
+from core.models import MachineRequest, IdentityMembership
 from core.models.status_type import StatusType
-from service.tasks.machine import start_machine_imaging
 from core.email import requestImaging
+
+from service.tasks.machine import start_machine_imaging
 
 
 class MachineRequestViewSet(BaseRequestViewSet):    
@@ -14,6 +18,49 @@ class MachineRequestViewSet(BaseRequestViewSet):
     serializer_class = UserMachineRequestSerializer
     admin_serializer_class = MachineRequestSerializer
     filter_fields = ('status__id', 'status__name', 'new_machine_owner__username')
+
+    def perform_create(self, serializer):
+        # NOTE: An identity could possible have multiple memberships
+        # It may be better to directly take membership rather than an identity
+        identity_id = serializer.initial_data.get("identity")
+        new_provider_id= serializer.initial_data['new_machine_provider']
+        new_owner_id=self.request.user.id
+        parent_machine_id = serializer.validated_data['instance'].provider_machine.id
+        status, _ = StatusType.objects.get_or_create(name="pending")
+        try:
+            membership = IdentityMembership.objects.get(identity=identity_id)
+            import ipdb;ipdb.set_trace()
+            instance = serializer.save(
+                membership=membership,
+                status=status,
+                old_status="processing",
+                created_by=self.request.user,
+                new_machine_provider_id = new_provider_id,
+                new_machine_owner_id = new_owner_id,
+                parent_machine_id=parent_machine_id
+            )
+            self.submit_action(instance)
+        except (core_exceptions.ProviderLimitExceeded,
+                core_exceptions.RequestLimitExceeded):
+            message = "Only one active request is allowed per provider."
+            raise exceptions.MethodNotAllowed('create', detail=message)
+        except core_exceptions.InvalidMembership:
+            message = (
+                "The user '%s' is not a valid member."
+                % self.request.user.username
+            )
+            raise exceptions.ParseError(detail=message)
+        except IdentityMembership.DoesNotExist:
+            message = (
+                "The identity '%s' does not have a membership"
+                % identity_id
+            )
+            raise exceptions.ParseError(detail=message)
+        except Exception as e:
+            message = {
+                "An error was encoutered when submitting the request."
+            }
+            raise exceptions.ParseError(detail=message)
 
     def submit_action(self, instance):
         """
