f91295137a0f824c1dcbc4e417d6771b47ed0014
Steve Gregory <contact@steve-gregory.com>
2015-12-04 13:15:49 -0500

    Change default ADMINS -- this shouldnt be handled in __init__.py ..

--- atmosphere/settings/__init__.py
+++ atmosphere/settings/__init__.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = [unicode(SERVER_URL.replace('https://', ''))]
 
 # NOTE: first admin will be sender of atmo emails.
 ADMINS = (
-    ('Atmosphere Admin', 'atmo@iplantcollaborative.org'),
+    #('Atmosphere Admin', 'atmo@iplantcollaborative.org'),
     ('Steven Gregory', 'esteve@iplantcollaborative.org'),
     ('Atmosphere Alerts', 'atmo-alerts@iplantcollaborative.org'),
 )
