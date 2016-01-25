97a0b51eb7517e5e343089ff30d966f74d404217
Joseph Garcia <prosif@gmail.com>
2016-01-25 16:34:42 -0700

    better help for script, removed mickey reference

--- service/management/commands/celery_start.py
+++ service/management/commands/celery_start.py
@@ -2,8 +2,7 @@ from django.core.management.base import BaseCommand
 from subprocess import call
 
 class Command(BaseCommand):
-    args = 'Arguments is not needed'
-    help = 'Django admin custom command'
+    help = 'Custom manage.py command to start celery.'
 
     def handle(self, *args, **options):
-        call(["celery", "worker", "--app=atmosphere", "-n", "atmosphere-node_1@mickey.iplantc.org", "--loglevel=INFO", "-O", "fair", "-Q", "default", "-c", "5", "--logfile=/var/log/celery/atmosphere-node_1.log", "--pidfile=/var/run/celery/atmosphere-node1.pid"])
+        call(["celery", "worker", "--app=atmosphere", "-n", "atmosphere-node_1@local", "--loglevel=INFO", "-O", "fair", "-Q", "default", "-c", "5", "--logfile=/var/log/celery/atmosphere-node_1.log", "--pidfile=/var/run/celery/atmosphere-node1.pid"])
