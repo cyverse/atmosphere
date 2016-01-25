ab7f565499831d3d1f82094a01bffd55e2b3501e
Joseph Garcia <prosif@gmail.com>
2016-01-25 16:30:10 -0700

    properly start celery using subprocess

--- service/management/commands/celery_start.py
+++ service/management/commands/celery_start.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand
+from subprocess import call
 
 class Command(BaseCommand):
     args = 'Arguments is not needed'
     help = 'Django admin custom command'
 
     def handle(self, *args, **options):
-        self.stdout.write("HEY")
+        call(["celery", "worker", "--app=atmosphere", "-n", "atmosphere-node_1@mickey.iplantc.org", "--loglevel=INFO", "-O", "fair", "-Q", "default", "-c", "5", "--logfile=/var/log/celery/atmosphere-node_1.log", "--pidfile=/var/run/celery/atmosphere-node1.pid"])
