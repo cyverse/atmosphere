7030e512cced9f35281cc9216f87b87a6384c754
Joseph Garcia <prosif@gmail.com>
2016-01-27 16:00:55 -0700

    create log file if it doesn't exist

--- service/management/commands/start_celery.py
+++ service/management/commands/start_celery.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 from django.core.management.base import BaseCommand
 from subprocess import call
 
@@ -6,4 +6,7 @@ class Command(BaseCommand):
     help = 'Custom manage.py command to start celery.'
 
     def handle(self, *args, **options):
+        if not os.path.isfile("celery_node.log"):
+            with open("celery_node.log", 'w+') as f:
+                f.close()
         call("celery worker --app=atmosphere --loglevel=INFO -c 5 --logfile='celery_node.log".split())
