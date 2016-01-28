9398b40110569433e5751a6dd5041e03cc942f54
Joseph Garcia <prosif@gmail.com>
2016-01-27 17:42:21 -0700

    change log file to variable

--- service/management/commands/start_celery.py
+++ service/management/commands/start_celery.py
@@ -6,7 +6,8 @@ class Command(BaseCommand):
     help = 'Custom manage.py command to start celery.'
 
     def handle(self, *args, **options):
-        if not os.path.isfile("celery_node.log"):
-            with open("celery_node.log", 'w+') as f:
+        logfile = "celery_node.log"
+        if not os.path.isfile(logfile):
+            with open(logfile, 'w+') as f:
                 f.close()
-        call("celery worker --app=atmosphere --loglevel=INFO -c 5 --logfile='celery_node.log".split())
+        call(("celery worker --app=atmosphere --loglevel=INFO -c 5 --logfile=%s" % logfile).split())
