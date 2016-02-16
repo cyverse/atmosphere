2d8259cc1b66c2dd21ada5c6b9fd17d0e8995bd4
Steve Gregory <contact@steve-gregory.com>
2016-02-16 18:12:14 -0500

    Typo

--- core/models/user.py
+++ core/models/user.py
@@ -144,7 +144,7 @@ def get_default_identity(username, provider=None):
             if settings.AUTO_CREATE_NEW_ACCOUNTS:
                 new_identities = create_new_accounts(username, provider=provider)
                 if not new_identities:
-                    logger.error("%s has no identities. Functionality will be severely limeted." % username)
+                    logger.error("%s has no identities. Functionality will be severely limited." % username)
                     return None
                 return new_identites[0]
             else:
