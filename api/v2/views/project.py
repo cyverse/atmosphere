a1f5ac23f9edce6316fc26c1389f41d9af888e28
Steve Gregory <contact@steve-gregory.com>
2016-03-03 13:50:57 -0700

    Do *not* Delete projects that contain links or applications

--- api/v2/views/project.py
+++ api/v2/views/project.py
@@ -27,6 +27,16 @@ class ProjectViewSet(MultipleFieldLookup, AuthViewSet):
                 "Cannot delete a project when it contains instances."
                 " To delete a project, all instances must be moved "
                 "to another project or deleted")
+        elif project.applications.filter(end_date__isnull=True).count() > 0:
+            raise ValidationError(
+                "Cannot delete a project when it contains instances."
+                " To delete a project, all Images must be moved "
+                "to another project or deleted")
+        elif project.links.all().count() > 0:
+            raise ValidationError(
+                "Cannot delete a project when it contains instances."
+                " To delete a project, all External Links must be moved "
+                "to another project or deleted")
         elif project.volumes.filter(
                 instance_source__end_date__isnull=True).count() > 0:
             raise ValidationError(
