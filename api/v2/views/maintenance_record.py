14c41706d6437247bbe69e0e574c03863fbe5bda
Steve Gregory <contact@steve-gregory.com>
2016-03-03 11:29:21 -0700

    [ATMO-1200] Add '?active=' filter for Maintenance Record

--- api/v2/views/maintenance_record.py
+++ api/v2/views/maintenance_record.py
@@ -1,11 +1,27 @@
+import django_filters
+
+from rest_framework import filters
 from rest_framework.serializers import ValidationError
 
-from core.models import MaintenanceRecord
+from core.models import AtmosphereUser, MaintenanceRecord
+from core.query import only_current
 
 from api.permissions import CanEditOrReadOnly
 from api.v2.serializers.details import MaintenanceRecordSerializer
 from api.v2.views.base import AuthOptionalViewSet
 
+
+class MaintenanceRecordFilterBackend(filters.BaseFilterBackend):
+    """
+    Filter MaintenanceRecords using the request_user and 'query_params'
+    """
+    def filter_queryset(self, request, queryset, view):
+        request_params = request.query_params
+        active = request_params.get('active')
+        if isinstance(active, basestring) and active.lower() == 'true'\
+                or isinstance(active, bool) and active:
+            queryset = MaintenanceRecord.active()
+        return queryset
 
 class MaintenanceRecordViewSet(AuthOptionalViewSet):
 
@@ -16,3 +32,4 @@ class MaintenanceRecordViewSet(AuthOptionalViewSet):
     queryset = MaintenanceRecord.objects.order_by('-start_date')
     permission_classes = (CanEditOrReadOnly,)
     serializer_class = MaintenanceRecordSerializer
+    filter_backends = (filters.DjangoFilterBackend, filters.SearchFilter, MaintenanceRecordFilterBackend)
