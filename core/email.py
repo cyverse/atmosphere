877d0b87c508aefbf7d24a2f2d4c3499297703c4
Steve Gregory <contact@steve-gregory.com>
2016-03-07 11:15:46 -0700

    [ATMO-1223] Replace @iplantcollaborative.org with 'default_email_domain' settings

--- core/email.py
+++ core/email.py
@@ -236,7 +236,7 @@ def email_to_admin(
             username = username.username
         user_email = lookupEmail(username)
         if not user_email:
-            user_email = "%s@iplantcollaborative.org" % username
+            user_email = "%s@%s" % (username, settings.DEFAULT_EMAIL_DOMAIN)
     elif not username:  # user_email provided
         username = 'Unknown'
     if request_tracker or not cc_user:
@@ -259,7 +259,7 @@ def email_from_admin(username, subject, message, html=False):
     from_name, from_email = admin_address()
     user_email = lookupEmail(username)
     if not user_email:
-        user_email = "%s@iplantcollaborative.org" % username
+        user_email = "%s@%s" % (username, settings.DEFAULT_EMAIL_DOMAIN)
     return send_email(subject, message,
                       from_email=email_address_str(from_name, from_email),
                       to=[email_address_str(username, user_email)],
