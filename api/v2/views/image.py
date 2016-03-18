61cce2cd23c798a8604274335d9637e8ebce1385
Steve Gregory <contact@steve-gregory.com>
2016-03-18 18:12:44 -0400

    Add 'Machine Identifier' for easy support lookups in Troposphere

--- api/v2/views/image.py
+++ api/v2/views/image.py
@@ -25,7 +25,9 @@ class ImageViewSet(MultipleFieldLookup, AuthOptionalViewSet):
     serializer_class = ImageSerializer
 
     search_fields = ('id', 'name', 'versions__change_log', 'tags__name',
-                     'tags__description', 'created_by__username', 'versions__machines__instance_source__provider__location')
+                     'tags__description', 'created_by__username',
+                     'versions__machines__instance_source__identifier',
+                     'versions__machines__instance_source__provider__location')
 
     def get_queryset(self):
         request_user = self.request.user
