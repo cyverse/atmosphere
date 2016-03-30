c9156bdeeea840503dc87c438e129f0f66e43dd3
Steve Gregory <contact@steve-gregory.com>
2016-03-30 13:57:32 -0400

    Avoid 500-crash when the size an instance was created with no longer exists

--- core/models/instance.py
+++ core/models/instance.py
@@ -872,7 +872,9 @@ def _esh_instance_size_to_core(esh_driver, esh_instance, provider_uuid):
         # so a lookup on the size is required to get accurate
         # information.
         # TODO: Switch to 'get_cached_size!'
-        esh_size = esh_driver.get_size(esh_size.id)
+        new_size = esh_driver.get_size(esh_size.id)
+    if new_size:
+        esh_size = new_size
     core_size = convert_esh_size(esh_size, provider_uuid)
     return core_size
 
