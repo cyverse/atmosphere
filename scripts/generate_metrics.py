62e754735792a00d5cb99b12c6c646955076e064
Steve Gregory <contact@steve-gregory.com>
2016-03-31 01:39:01 -0400

    Make script more portable for Excel, more error-resistant

--- scripts/generate_metrics.py
+++ scripts/generate_metrics.py
@@ -3,14 +3,24 @@
 This script is for the accounting purposes of Jetstream
 The goal:
     Print a CSV of:
-<instance_id>,<featured_image_name>,<went_active>,<went_deploy_error>,<went_error>
 """
-import django; django.setup()
-from core.models import Provider, Instance, InstanceStatusHistory
+CSV_HEADER="Instance ID, Instance Start Date, Image Name, Active, Deploy Error, Error"
 
-for inst in Instance.objects.order_by('-id'):
+import django; django.setup()
+from core.models import Provider, Instance, InstanceStatusHistory, ObjectDoesNotExist
+
+print CSV_HEADER
+inst_list = Instance.objects.all()
+inst_list = Instance.objects.filter(source__providermachine__application_version__application__tags__name__icontains='featured')
+for inst in inst_list.order_by('id'):
     first_history = inst.get_first_history()
-    image_name = inst.source.providermachine.application_version.application.name
+    try:
+        application = inst.source.providermachine.application_version.application
+        image_name = application.name
+        featured_image = application.tags.filter(name__icontains='featured').count() > 0
+    except ObjectDoesNotExist:
+        image_name = "Deleted Image"
+        featured_image = False
     last_history = inst.get_last_history()
     history = first_history
     hit_active = hit_deploy_error = hit_error = False
@@ -27,9 +37,17 @@ for inst in Instance.objects.order_by('-id'):
             #NOTE: These value-errors are important debugging tools. but they can be ignored if you aren't hunting down problems.
             #raise
             break
+    featured_image = 1 if featured_image else 0
+    #Magic goes here.
+    if hit_active:
+        hit_error = 0
+        hit_deploy_error = 0
+    if hit_error and hit_deploy_error:
+        hit_error = 0
+    #
     hit_active = 1 if hit_active else 0
     hit_error = 1 if hit_error else 0
     hit_deploy_error = 1 if hit_deploy_error else 0
-    arg_list = [inst.id, image_name, hit_active, hit_deploy_error, hit_error]
+    arg_list = [inst.id, inst.start_date.strftime("%x %X"), image_name, hit_active, hit_deploy_error, hit_error]
     print ",".join(map(str,arg_list))
 
