387873ef2a953f89d7b98cade3a87b1339ff9ca6
tharon-c <design@tharoncarlson.com>
2016-07-25 18:46:53 -0700

    Add a script to call for allocation result externally

---
+++ scripts/external_test_allocation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from django import setup
+setup()
+from service.monitoring import get_allocation_result_for
+from core.models import Identity, Instance, AtmosphereUser
+from django.utils.timezone import timedelta, datetime
+import pytz, sys
+from dateutil.parser import parse
+
+if len(sys.argv) < 3:
+    print "Invalid # of args"
+    print "Usage: %s <username> <start> <end>" % sys.argv[0]
+    print "All times assumed to be in utc"
+    sys.exit(1)
+
+def utc_parse(date_str):
+    dt = parse(date_str)
+    return pytz.utc.localize(dt)
+
+username=sys.argv[1]
+start = utc_parse(sys.argv[2])
+end = utc_parse(sys.argv[3])
+print "Testing %s from %s - %s" % (username, start, end)
+ident = Identity.objects.get(provider__id=4, created_by__username=username)
+print get_allocation_result_for(ident.provider, ident.created_by, True, start, end)
