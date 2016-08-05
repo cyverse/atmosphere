1353eeaaa6ce29ccbab246b1554fc2f1ad48bd82
Steve Gregory <contact@steve-gregory.com>
2016-08-04 17:44:34 -0700

    PEP8 + fix 500 error on EventSerializer

--- api/v2/serializers/details/event.py
+++ api/v2/serializers/details/event.py
@@ -1,7 +1,6 @@
-from core.models import EventTable
+from core.models import EventTable, AllocationSource, Instance
 from rest_framework import serializers
 from api.v2.serializers.fields.base import UUIDHyperlinkedIdentityField
-from api.v2.serializers.summaries import UserSummarySerializer
 
 
 class EventSerializer(serializers.HyperlinkedModelSerializer):
@@ -10,14 +9,19 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
     )
 
     def _validate_ias_event(self, payload):
-        allocation_source_id = payload.get('allocation_source_id','')
-        instance_id = payload.get('instance_id','')
-        allocation_source = AllocationSource.objects.filter(source_id=allocation_source_id).first()
+        allocation_source_id = payload.get('allocation_source_id', '')
+        instance_id = payload.get('instance_id', '')
+        allocation_source = AllocationSource.objects.filter(
+            source_id=allocation_source_id).first()
         instance = Instance.objects.filter(provider_alias=instance_id).first()
         if not allocation_source:
-            raise serializers.ValidationError("AllocationSource with source_id=%s DoesNotExist" %  allocation_source_id)
+            raise serializers.ValidationError(
+                "AllocationSource with source_id=%s DoesNotExist"
+                % allocation_source_id)
         if not instance:
-            raise serializers.ValidationError("Instance with provider_alias=%s DoesNotExist" %  instance_id)
+            raise serializers.ValidationError(
+                "Instance with provider_alias=%s DoesNotExist"
+                % instance_id)
         return True
 
     def validate(self, data):
@@ -25,7 +29,7 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
         payload = data['payload']
         if name.lower() == 'instance_allocation_source_changed':
             self._validate_ias_event(payload)
-        return super(EventSerializer, self).create(validated_data)
+        return super(EventSerializer, self).create(data)
 
     class Meta:
         model = EventTable
