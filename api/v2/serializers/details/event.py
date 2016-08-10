847cf4742c51b002403e85bb20136c76bcad1535
Steve Gregory <contact@steve-gregory.com>
2016-08-10 12:18:12 -0700

    Validate the entity_id, include 'username' in the payload of 'instance_allocation_source_changed' event

--- api/v2/serializers/details/event.py
+++ api/v2/serializers/details/event.py
@@ -8,10 +8,17 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
         view_name='api:v2:event-detail',
     )
 
-    def _validate_ias_event(self, payload):
+    def _validate_ias_event(self, entity_id, payload):
         allocation_source_id = payload.get('allocation_source_id', '')
         instance_id = payload.get('instance_id', '')
+        if entity_id != instance_id:
+            raise serializers.ValidationError(
+                "Expected entity ID to be the Instance ID: %s, Received: %s"
+                % (instance_id, entity_id))
         user = self._get_request_user()
+        if not user:
+            raise serializers.ValidationError("Request user was not found")
+        payload['username'] = user.username
         allocation_source = AllocationSource.for_user(user=user).filter(
             source_id=allocation_source_id).first()
         instance = Instance.for_user(user=user).filter(provider_alias=instance_id).first()
@@ -32,9 +39,10 @@ class EventSerializer(serializers.HyperlinkedModelSerializer):
 
     def validate(self, data):
         name = data['name']
+        entity_id = data['entity_id']
         payload = data['payload']
         if name.lower() == 'instance_allocation_source_changed':
-            self._validate_ias_event(payload)
+            self._validate_ias_event(entity_id, payload)
         else:
             raise serializers.ValidationError(
                 "Unknown event type %s" % name
