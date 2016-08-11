1986104320a9c82ea63c688efc87e58ff3b73751
Steve Gregory <contact@steve-gregory.com>
2016-08-11 20:24:00 +0000

    Bugfixes found (in celery) while debugging celery-1

--- service/tasks/driver.py
+++ service/tasks/driver.py
@@ -1173,6 +1173,7 @@ def check_web_desktop_task(driverCls, provider, identity,
             return False
         # USE ANSIBLE
         username = identity.user.username
+        hostname = build_host_name(instance.id, instance.ip)
         playbooks = run_utility_playbooks(instance.ip, username, instance_alias, ["atmo_check_web_desktop.yml"], raise_exception=False)
         result = False if execution_has_failures(playbooks, hostname) or execution_has_unreachable(playbooks, hostname)  else True
 
@@ -1209,6 +1210,7 @@ def check_process_task(driverCls, provider, identity,
         instance = driver.get_instance(instance_alias)
         if not instance:
             return False
+        hostname = build_host_name(instance.id, instance.ip)
         # USE ANSIBLE
         username = identity.user.username
         playbooks = run_utility_playbooks(instance.ip, username, instance_alias, ["atmo_check_vnc.yml"], raise_exception=False)
