ef37d84bfc8ba28f2d26fa23b927dd416b0f6057
Connor Osborn <cdosborn@email.arizona.edu>
2016-09-07 23:26:40 +0000

    Improve email template
    - Always list a users ssh keys
    - Indicate keys are for root user
    - Comma separate rather than (numbered list)

--- core/email.py
+++ core/email.py
@@ -335,8 +335,7 @@ def send_instance_email(username, instance_id, instance_name,
     instance = Instance.objects.get(provider_alias=instance_id)
     author = instance.created_by
     provider_location = instance.provider.location
-    ssh_keys = author.sshkey_set.all()
-    use_ssh_keys = author.userprofile.use_ssh_keys and ssh_keys.count() > 0
+    ssh_keys = [key.name for key in author.sshkey_set.all()]
     username, user_email, user_name = user_email_info(username)
     launched_at = launched_at.replace(tzinfo=None)
     utc_date = django_timezone.make_aware(launched_at,
