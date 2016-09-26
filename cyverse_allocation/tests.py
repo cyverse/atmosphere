653099e72f8de68db23089d2af1a5f89401d698e
Amit Juneja <amitj@email.arizona.edu>
2016-09-26 21:15:27 +0000

    cyverse allocation plugin created

---
+++ cyverse_allocation/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.conf import settings
+
+# Create your tests here.
+
+class CyVerseAllocationTests(TestCase):
+
+    def setUp(self):
+        if "cyverse_allocation" not in settings.INSTALLED_APPS:
+            self.skipTest("CyVerse Allocation plugin is not enabled")
+
+    def test_failing(self):
+        pass
+        #raise NotImplementedError
