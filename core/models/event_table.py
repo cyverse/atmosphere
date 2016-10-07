d32655c16b6703490913cfeaf93241fc8ad8b4e0
Julian Pistorius <julian@julianpistorius.com>
2016-10-07 16:44:06 -0700

    Problem: Invalid events can be created, causing bugs
    Solution: Added basic schemas. WIP.
    
    TODO: Use built-in Django Serializer instead of Avro

--- core/models/event_table.py
+++ core/models/event_table.py
@@ -2,6 +2,9 @@
 from uuid import uuid4
 from datetime import timedelta
 
+import avro
+import avro_json_serializer
+from django.core import exceptions
 from django.db import models, transaction, DatabaseError
 from django.db.models import ObjectDoesNotExist
 from django.db.models.signals import post_save, pre_save
@@ -19,6 +22,117 @@ from core.hooks.allocation_source import (
     listen_for_user_allocation_source_assigned
 )
 
+ALLOCATION_SOURCE_CREATED = avro.schema.make_avsc_object(
+    {
+        'namespace': 'org.cyverse.atmosphere.event',
+        'type': 'record',
+        'name': 'allocation_source_created',
+        'fields': [
+            {
+                'name': 'source_id',
+                'type': 'string'
+            },
+            {
+                'name': 'name',
+                'type': 'string'
+            },
+            {
+                'name': 'compute_allowed',
+                'type': 'float'
+            }
+        ]
+    }
+)
+USER_ALLOCATION_SOURCE_ASSIGNED = avro.schema.make_avsc_object(
+    {
+        'namespace': 'org.cyverse.atmosphere.event',
+        'type': 'record',
+        'name': 'user_allocation_source_assigned',
+        'fields': [
+            {
+                'name': 'source_id',
+                'type': 'string'
+            },
+            {
+                'name': 'username',
+                'type': 'string'
+            }
+        ]
+    }
+)
+
+ALLOCATION_SOURCE_SNAPSHOT_SCHEMA = avro.schema.make_avsc_object(
+    {
+        'namespace': 'org.cyverse.atmosphere.event',
+        'type': 'record',
+        'name': 'allocation_source_snapshot',
+        'fields': [
+            {
+                'name': 'allocation_source_id',
+                'type': 'string'
+            },
+            {
+                'name': 'compute_used',
+                'type': 'float'
+            },
+            {
+                'name': 'global_burn_rate',
+                'type': 'float'
+            }
+        ]
+    }
+)
+
+ALLOCATION_SOURCE_THRESHOLD_MET = avro.schema.make_avsc_object(
+    {
+        'namespace': 'org.cyverse.atmosphere.event',
+        'type': 'record',
+        'name': 'allocation_source_threshold_met',
+        'fields': [
+            {
+                'name': 'allocation_source_id',
+                'type': 'string'
+            },
+            {
+                'name': 'actual_value',
+                'type': 'float'
+            },
+            {
+                'name': 'threshold',
+                'type': 'float'
+            }
+        ]
+    }
+)
+
+EVENT_SCHEMAS = {
+    'allocation_source_snapshot': ALLOCATION_SOURCE_SNAPSHOT_SCHEMA,
+    'allocation_source_threshold_met': ALLOCATION_SOURCE_THRESHOLD_MET,
+    'allocation_source_created': ALLOCATION_SOURCE_CREATED,
+    'user_allocation_source_assigned': USER_ALLOCATION_SOURCE_ASSIGNED
+}
+
+
+def validate_event_schema(event_name, event_payload):
+    code = 'event_schema'
+    try:
+        event_schema = EVENT_SCHEMAS[event_name]
+    except KeyError:
+        limit_value = EVENT_SCHEMAS.keys()
+        message = 'Unrecognized event name: {}'.format(event_name)
+        params = {'limit_value': limit_value, 'show_value': event_name, 'value': event_name}
+        raise exceptions.ValidationError(message, code=code, params=params)
+    try:
+        serializer = avro_json_serializer.AvroJsonSerializer(event_schema)
+        serialized_payload = serializer.to_ordered_dict(event_payload)
+        return serialized_payload
+    except Exception as e:
+        logger.warn(e)
+        message = 'Does not comply with event schema'
+        limit_value = event_schema
+        params = {'limit_value': limit_value, 'show_value': event_payload, 'value': event_payload}
+        raise exceptions.ValidationError(message, code=code, params=params)
+
 
 class EventTable(models.Model):
 
@@ -34,11 +148,16 @@ class EventTable(models.Model):
 
     @classmethod
     def create_event(cls, name, payload, entity_id):
+        serialized_payload = validate_event_schema(name, payload)
+
         return EventTable.objects.create(
             name=name,
             entity_id=entity_id,
-            payload=payload
+            payload=serialized_payload
         )
+
+    def clean(self):
+        validate_event_schema(self.name, self.payload)
 
     def __str__(self):
         return "%s" % self.name
--- core/tests/models/test_event_table.py
+++ core/tests/models/test_event_table.py
@@ -1,35 +1,172 @@
 from django.test import TestCase, override_settings
+from django.core import exceptions
 
 from api.tests.factories import UserFactory
 from core.models import EventTable, AllocationSource
 from core.models import UserAllocationSource
 
-
 class EventTableTest(TestCase):
     def setUp(self):
-        pass
+        user = UserFactory.create()
+        self.alloc_src = AllocationSource.objects.create(name='DefaultAllocation', source_id='37623',
+                                                         compute_allowed=1000)
+        UserAllocationSource.objects.create(user=user, allocation_source=self.alloc_src)
 
     @override_settings(ALLOCATION_SOURCE_WARNINGS=[10, 25, 50, 75, 90])
-    def test_create_event(self):
+    def test_basic_create_event(self):
         event_count = EventTable.objects.count()
         self.assertEqual(event_count, 0)
-        user = UserFactory.create()
-        alloc_src = AllocationSource.objects.create(name='DefaultAllocation', source_id='37623',
-                                                    compute_allowed=1000)
-        UserAllocationSource.objects.create(user=user, allocation_source=alloc_src)
+
         event_payload = {
-            'allocation_source_id': alloc_src.source_id,
+            'allocation_source_id': self.alloc_src.source_id,
             'compute_used': 100.00,  # 100 hours used ( a number, not a string!)
             'global_burn_rate': 2.00,  # 2 hours used each hour
         }
+        print(event_payload)
         new_event = EventTable.create_event(name='allocation_source_snapshot', payload=event_payload,
-                                            entity_id=alloc_src.source_id)
+                                            entity_id=self.alloc_src.source_id)
         event_count = EventTable.objects.count()
         self.assertEqual(event_count, 2)
         events = EventTable.objects.all()
         self.assertEqual(new_event, events[1])
         self.assertEqual(events[0].name, 'allocation_source_threshold_met')
-        self.assertEqual(events[0].entity_id, alloc_src.source_id)
+        self.assertEqual(events[0].entity_id, self.alloc_src.source_id)
         self.assertEqual(events[0].payload, {'actual_value': 10,
-                                             'allocation_source_id': alloc_src.source_id,
+                                             'allocation_source_id': self.alloc_src.source_id,
                                              'threshold': 10})
+
+    def test_valid_events(self):
+        event_count = EventTable.objects.count()
+        self.assertEqual(event_count, 0)
+
+        valid_events = (
+            {
+                'description': 'Basic sanity test',
+                'entity_id': 'some_entity_id',
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_source_id': self.alloc_src.source_id,
+                        'compute_used': 10.00,
+                        'global_burn_rate': 2.00
+                    }
+            },
+            {
+                'description': 'Extra field should be stripped out by serializer',
+                'entity_id': 'some_entity_id',
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_source_id': self.alloc_src.source_id,
+                        'compute_used': 10.00,
+                        'global_burn_rate': 2.00,
+                        'extra_field': 'some_value'
+                    },
+                'expected_serialized_payload':
+                    {
+                        'allocation_source_id': self.alloc_src.source_id,
+                        'compute_used': 10.00,
+                        'global_burn_rate': 2.00
+                    }
+            }
+        )
+        for event_data in valid_events:
+            raw_payload = event_data['raw_payload']
+            print('Testing: {}'.format(event_data['description']))
+            new_event = EventTable.create_event(name=event_data['name'], payload=raw_payload,
+                                                entity_id=event_data['entity_id'])
+            self.assertEqual(new_event.name, event_data['name'])
+            self.assertEqual(new_event.entity_id, event_data['entity_id'])
+            expected_serialized_payload = event_data.get('expected_serialized_payload') or raw_payload
+            self.assertEqual(new_event.payload, expected_serialized_payload)
+
+        event_count = EventTable.objects.count()
+        self.assertEqual(event_count, len(valid_events))
+
+
+class EventTableFailTests(TestCase):
+    def setUp(self):
+        user = UserFactory.create()
+        self.alloc_src = AllocationSource.objects.create(name='DefaultAllocation', source_id='37623',
+                                                         compute_allowed=1000)
+
+        UserAllocationSource.objects.create(user=user, allocation_source=self.alloc_src)
+
+    def test_fail_schema(self):
+        """Ensure event creation fails if it does not match schema"""
+
+        invalid_events = (
+            {
+                'description': 'Typo in field name',
+                'entity_id': self.alloc_src.source_id,
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_souce_id': self.alloc_src.source_id,  # Case: Typo in field name
+                        'compute_used': 10.00,
+                        'global_burn_rate': 2.00,
+                    }
+            },
+            {
+                'description': 'Number instead of string ID',
+                'entity_id': self.alloc_src.source_id,
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_source_id': 37623,  # Case: Number instead of string ID
+                        'compute_used': 10.00,
+                        'global_burn_rate': 2.00
+                    }
+            },
+            {
+                'description': 'String instead of float',
+                'entity_id': self.alloc_src.source_id,
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_source_id': self.alloc_src.source_id,
+                        'compute_used': '10.00',  # Case: String instead of float
+                        'global_burn_rate': 2.00
+                    }
+            },
+            {
+                'description': 'Missing field',
+                'entity_id': self.alloc_src.source_id,
+                'name': 'allocation_source_snapshot',
+                'raw_payload':
+                    {
+                        'allocation_source_id': self.alloc_src.source_id,
+                        'compute_used': '10.00',
+                        # Case: Missing field
+                    }
+            },
+        )
+
+        for event_data in invalid_events:
+            raw_payload = event_data['raw_payload']
+            print('Testing: {}'.format(event_data['description']))
+            with self.assertRaises(exceptions.ValidationError) as validation_error:
+                EventTable.create_event(name='allocation_source_snapshot', payload=raw_payload,
+                                        entity_id=self.alloc_src.source_id)
+            self.assertEqual(validation_error.exception.code, 'event_schema')
+            self.assertEqual(validation_error.exception.message, 'Does not comply with event schema')
+
+            event_count = EventTable.objects.count()
+            self.assertEqual(event_count, 0)
+
+    def test_fail_unknown_event(self):
+        """Ensure event creation fails if it does not match known events"""
+        event_payload = {
+            'allocation_source_id': '37623',  # Typo in field name
+            'compute_used': 100.00,
+            'global_burn_rate': 2.00
+        }
+        invalid_event_name = 'allocation_souce_snapshot'  # Test for typo
+        with self.assertRaises(exceptions.ValidationError) as validation_error:
+            EventTable.create_event(name=invalid_event_name, payload=event_payload,
+                                    entity_id=self.alloc_src.source_id)
+        self.assertEqual(validation_error.exception.code, 'event_schema')
+        self.assertEqual(validation_error.exception.message, 'Unrecognized event name: allocation_souce_snapshot')
+
+        event_count = EventTable.objects.count()
+        self.assertEqual(event_count, 0)
