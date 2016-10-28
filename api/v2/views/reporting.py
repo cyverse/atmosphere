163d547d397bd517443d2eb4e690e6d152446ae6
Steve Gregory <contact@steve-gregory.com>
2016-10-28 10:57:44 -0700

    Clean up excel worksheets, add more formatting

--- api/v2/views/reporting.py
+++ api/v2/views/reporting.py
@@ -42,13 +42,13 @@ class PandasExcelRenderer(CSVRenderer):
         table = self.tablize(data, header=header_list)
         #table = self.tablize(data)
         headers = table.pop(0)
-        df = pandas.DataFrame(table, columns=headers)
+        raw_dataframe = pandas.DataFrame(table, columns=headers)
         #Save to StringIO
         sio = StringIO()
         writer = pandas.ExcelWriter(sio, engine='xlsxwriter')
         if 'writer_callback' in renderer_context:
             callback = renderer_context.get('writer_callback')
-            df, writer = callback(df, writer, sheetname)
+            raw_dataframe, writer = callback(raw_dataframe, writer, sheetname)
         # Get access to the workbook and sheet
 
         writer.save()
@@ -71,43 +71,73 @@ class ReportingViewSet(AuthViewSet):
     class Meta:
         model = Instance
 
-    def write_excel(self, df, writer, sheetname):
-        if len(df.index) <= 1:
-            return (df, writer)
-        df['start_date'] = df['start_date'].apply(pandas.to_datetime)
-        df['end_date'] = df['end_date'].apply(pandas.to_datetime)
-        new_df = df.set_index(['start_date', 'image_name'])
-        new_df = new_df.groupby([pandas.Grouper(freq='MS', level=0), new_df.index.get_level_values(1)]).mean()
-        new_df = new_df.query('is_featured_image == 1')
-        new_df = new_df.drop(['is_featured_image', 'hit_active', 'hit_aborted', 'hit_deploy_error', 'hit_error', 'id', 'size.active', 'size.cpu', 'size.disk', 'size.id', 'size.mem'], axis=1)
-        new_df.index.rename(['Start Date', 'Image Name'], inplace=True)
-        new_df.columns = ['Average of Active/Aborted', 'Average of Active/Aborted/Error']
+    def write_excel(self, raw_dataframe, writer, sheetname):
+        if len(raw_dataframe.index) <= 1:
+            return (raw_dataframe, writer)
+        raw_dataframe['start_date'] = raw_dataframe['start_date'].apply(pandas.to_datetime)
+        raw_dataframe['end_date'] = raw_dataframe['end_date'].apply(pandas.to_datetime)
 
+        user_summary_data = raw_dataframe.set_index(['start_date', 'username'])
+        user_summary_data = user_summary_data.groupby([pandas.Grouper(freq='MS', level=0), user_summary_data.index.get_level_values(1)]).sum()
+        user_summary_data = user_summary_data.query('is_featured_image == 1')
+        user_summary_data = user_summary_data.drop(['is_featured_image', 'hit_error', 'id', 'size.active', 'size.cpu', 'size.disk', 'size.id', 'size.mem'], axis=1)
+        user_summary_data.index.rename(['Start Date', 'Username'], inplace=True)
+        user_summary_data.columns = ['Sum of Active', 'Sum of Aborted', 'Sum of Deploy Error', 'Sum of Active/Aborted', 'Sum of Active/Aborted/Error']
+
+        image_summary_data = raw_dataframe.set_index(['start_date', 'image_name'])
+        image_summary_data = image_summary_data.groupby([pandas.Grouper(freq='MS', level=0), image_summary_data.index.get_level_values(1)]).mean()
+        image_summary_data = image_summary_data.query('is_featured_image == 1')
+        image_summary_data = image_summary_data.drop(['is_featured_image', 'hit_active', 'hit_aborted', 'hit_deploy_error', 'hit_error', 'id', 'size.active', 'size.cpu', 'size.disk', 'size.id', 'size.mem'], axis=1)
+        image_summary_data.index.rename(['Start Date', 'Image Name'], inplace=True)
+        image_summary_data.columns = ['Average of Active/Aborted', 'Average of Active/Aborted/Error']
+        raw_dataframe = raw_dataframe.drop(['size.id', 'size.uuid', 'size.alias', 'size.active', 'size.start_date', 'size.end_date','size.url'], axis=1)
         #Write to excel.
         writer.datetime_format = 'mmm yyyy'
-        new_df.to_excel(writer, 'Summary')
+        image_summary_data.to_excel(writer, 'Image Summary')
+        user_summary_data.to_excel(writer, 'User Summary')
 
         writer.datetime_format = 'mmm d yyyy hh:mm:ss'
-        df.to_excel(writer, sheet_name=sheetname)
-
-        import ipdb;ipdb.set_trace()
+        raw_dataframe.to_excel(writer, sheet_name=sheetname)
 
         # Set workbook formatting after writing
-        workbook  = writer.book
+        workbook = writer.book
         pct_format = workbook.add_format({'num_format': '0.00%'})
-        name_format = workbook.add_format({'align': 'left'})
+        name_format = workbook.add_format()
+        name_format.set_align('left')
 
         raw_ws = writer.sheets[sheetname]
-        summary_ws = writer.sheets['Summary']
+        image_summary_ws = writer.sheets['Image Summary']
+        user_summary_ws = writer.sheets['User Summary']
 
         # Set worksheet formatting
-        raw_ws.autofilter(0,0,len(df.values),len(df.columns))
+        raw_ws.autofilter(0, 0, len(raw_dataframe.values), len(raw_dataframe.columns))
+        raw_ws.set_column('C:C', 32)
+        raw_ws.set_column('D:D', 13)
+        raw_ws.set_column('F:F', 23)
+        raw_ws.set_column('G:G', 17)
+        raw_ws.set_column('H:H', 17)
+        raw_ws.set_column('I:I', 34)
+        raw_ws.set_column('J:J', 17)
+        raw_ws.set_column('K:K', 14)
+        raw_ws.set_column('L:L', 14)
+        raw_ws.set_column('M:M', 14)
+        raw_ws.set_column('N:N', 14)
+        raw_ws.set_column('O:O', 14)
+        
+        image_summary_ws.set_column('A2:A', 34)
+        image_summary_ws.set_column('B:B', 36, name_format)
+        image_summary_ws.set_column('C:C', 21, pct_format)
+        image_summary_ws.set_column('D:D', 26, pct_format)
 
-        summary_ws.set_column('A2:A', 34)
-        summary_ws.set_column('B:B', 36, name_format)
-        summary_ws.set_column('C:C', 21, pct_format)
-        summary_ws.set_column('D:D', 26, pct_format)
-        return (new_df, writer)
+        user_summary_ws.set_column('A2:A', 34)
+        user_summary_ws.set_column('B:B', 13, name_format)
+        user_summary_ws.set_column('C:C', 17)
+        user_summary_ws.set_column('D:D', 17)
+        user_summary_ws.set_column('E:E', 17)
+        user_summary_ws.set_column('F:F', 21)
+        user_summary_ws.set_column('G:G', 21)
+
+        return (image_summary_data, writer)
 
     def list(self, request, *args, **kwargs):
         """
@@ -134,7 +164,11 @@ class ReportingViewSet(AuthViewSet):
         }
 
     def _filter_by_request(self):
-        instances_qs = Instance.objects.all()
+        request_user = self.request.user
+        if request_user.is_staff or request_user.is_superuser:
+            instances_qs = Instance.objects.all()
+        else:
+            instances_qs = Instance.for_user(request_user)
         query_params = self.request.query_params
         query = Q()
 
