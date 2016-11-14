c295376435a07afae5731993eccf660f5b113db8
Steve Gregory <contact@steve-gregory.com>
2016-11-14 16:13:41 -0700

    Cleanup the validation and creation steps on the serializer
    Additionally, avoids creating User, Group until *validation* step is
    :100:%

--- api/v2/serializers/post/account.py
+++ api/v2/serializers/post/account.py
@@ -22,9 +22,72 @@ class AccountSerializer(serializers.Serializer):
     credentials = CredentialSerializer(many=True, write_only=True)
     # Optional fields
     quota = serializers.UUIDField(required=False, allow_null=True)
-    #Remove? allocation_source_id = serializers.CharField(required=False, allow_null=True)
+    #allocation_source_id = serializers.CharField(required=False, allow_null=True)  NOTE: Uncomment when feature is completed
+
+    def validate(self, data):
+        """
+        Validation will:
+        - Ensure that user/group exists (Or create it)
+        - 
+        """
+        validated_data = data
+        self.validate_user(data['provider'])
+
+        validated_data['atmo_user'], validated_data['atmo_group'] = self._validate_user_group(data)
+        validated_data['provider'] = self._validate_provider(data['provider'])
+
+        # Using the validated data, ensure the user *should* be able to request these things.
+
+        # Validate that the quota exists (Or set a default)
+        validated_data['quota'] = self._validate_quota(data)
+
+        # Validate that the allocation source exists (Or set a default)
+        # validated_data['allocation_source'] = self._validate_allocation(data)  NOTE: Uncomment when feature is completed
+
+        # Validate the credentials (?)
+
+        # NOTE: This method is OpenStack specific. Update this method when adding new provider types.
+        required_keys = self._get_required_keys(
+            validated_data['provider'], validated_data['create_account'])
+        validated_data['credentials'] = self._validate_credentials(
+            validated_data['provider'], data['credentials'], required_keys)
+
+        if not validated_data['admin_account']:
+            self.validate_account_driver(validated_data)
+        return validated_data
+
+    def create(self, validated_data):
+        username = validated_data['atmo_user']
+        groupname = validated_data['atmo_group']
+        atmo_user, atmo_group = Group.create_usergroup(
+            username, groupname)
+
+        provider = validated_data['provider']
+        provider_type = provider.get_type_name().lower()
+        if provider_type == 'openstack':
+            new_identity = self.create_openstack_identity(atmo_user, provider, validated_data)
+        else:
+            raise Exception("Cannot create accounts for provider of type %s" % provider_type)
+
+        # Always share identity with group (To enable Troposphere access)
+        new_identity.share(atmo_group)
+
+        admin_account = validated_data['admin_account']
+        if admin_account:
+            AccountProvider.objects.get_or_create(
+                provider=new_identity.provider,
+                identity=new_identity)
+
+        # TODO: When the refactor of rtwo/get_esh_driver is complete, validate_identity should be call-able without the django model (to avoid create-then-delete)
+        validate_identity(new_identity)
+        return new_identity
+
+    ###
+    # Private validation methods
+    ###
 
     def _validate_user_group(self, data):
+        create_account = data['create_account']
         username = data['atmo_user']
         groupname = data['atmo_group']
         atmo_user = AtmosphereUser.objects.filter(username=username).first()
@@ -32,16 +95,27 @@ class AccountSerializer(serializers.Serializer):
             atmo_group = Group.objects.filter(user=atmo_user).filter(name=groupname).first()
         else:
             atmo_group = None
-        if not atmo_user or not atmo_group:
-            atmo_user, atmo_group = Group.create_usergroup(username, groupname)
-        return (atmo_user, atmo_group)
+        if not atmo_user and not create_account:
+            raise serializers.ValidationError("User %s does not exist, and 'create_account' is False." % username)
+        if not atmo_group and not create_account:
+            raise serializers.ValidationError("Group %s does not exist, and 'create_account' is False." % groupname)
+        return username, groupname
 
-    def _validate_provider(self, data):
-        provider_uuid = data.get('provider', '')
-        provider = Provider.objects.filter(only_current(), active=True).filter(uuid=provider_uuid).first()
+    def _validate_provider(self, provider_uuid):
+        """
+        Validate that this provider is 'visible' w.r.t. the current user
+        """
+        request_user = self._get_request_user()
+        # NOTE: With this validation, *ONLY* the creator of the provider can admininster accounts
+        # To allow anyone with staff/superuser to create, replace provider_manager
+        # provider_manager = Provider.objects
+        provider_manager = request_user.admin_providers
+        prov_qs = provider_manager.filter(
+            only_current(), active=True)
+        provider = prov_qs.filter(uuid=provider_uuid).first()
         if not provider:
             raise serializers.ValidationError(
-                "Provider %s does not exist" % provider)
+                "Cannot create an account for provider with UUID %s" % provider_uuid)
         return provider
 
     def _validate_quota(self, data):
@@ -52,32 +126,38 @@ class AccountSerializer(serializers.Serializer):
         quota = Quota.objects.filter(uuid=quota_uuid).first()
         if not quota:
             raise serializers.ValidationError(
-                "Quota %s does not exist" % quota)
+                "Quota '%s' not found" % quota_uuid)
         return quota
 
     def _validate_allocation(self, data):
-        # FIXME: Validation of allocation source logic goes *here*
+        # FIXME: Creation & Validation of allocation source logic goes *here* (post-CyVerse+AS)
         return None
 
-    def _validate_openstack_credentials(self, data):
-        credentials = data['credentials']
-        create_account = data['create_account']
+    def _validate_openstack_credentials(self, credentials, required_keys):
+        """
+        Note: If this can be reused elsewhere, we can move this to a class/staticmethod for service.accounts.openstack.AccountDriver
+        """
         keys = [c['key'] for c in credentials]
-        if create_account:
-            required_keys = ['key']
-        else:
-            required_keys = ['key', 'secret', 'ex_project_name']
         missing_keys = [key for key in required_keys if key not in keys]
         if missing_keys:
             raise serializers.ValidationError("Missing required key(s) for Openstack creation: %s" % missing_keys)
         return credentials
 
-    def _validate_credentials(self, provider, data):
-        # FIXME: Validation of credentials based on the selected provider
+    def _get_required_keys(self, provider, create_account):
+        has_account_provider = provider.accountprovider_set.exists()
+        # If an account provider exists, only 'key' is required to create an account
+        if create_account and has_account_provider:
+            required_keys = ['key']
+        else:  # Otherwise require the 'full-chain' credentials
+            required_keys = ['key', 'secret', 'ex_project_name']
+        return required_keys
+
+    def _validate_credentials(self, provider, credentials, required_keys):
         provider_type = provider.get_type_name().lower()
-        credentials = data['credentials']
+
+        # NOTE: Looking to add a new provider type? validate the credentials here!
         if provider_type == 'openstack':
-            valid_creds = self._validate_openstack_credentials(data)
+            valid_creds = self._validate_openstack_credentials(credentials, required_keys)
         else:
             valid_creds = credentials
         return valid_creds
@@ -90,28 +170,6 @@ class AccountSerializer(serializers.Serializer):
         else:
             raise ValueError("Expected 'request/user' to be passed in via context for this serializer")
 
-    def validate(self, data):
-        validated_data = data
-        # Validate that user/group *does not exist* and *create_account* is True, or that user/group is 'visible'
-        atmo_user, atmo_group = self._validate_user_group(data)
-        validated_data['atmo_user'] = atmo_user
-        validated_data['atmo_group'] = atmo_group
-
-        # Validate that provider is 'visible' for the user
-        validated_data['provider'] = self._validate_provider(data)
-        # Validate that the quota exists (Or set a default)
-        validated_data['quota'] = self._validate_quota(data)
-        # Validate that the allocation source exists (Or set a default)
-        validated_data['allocation_source'] = self._validate_allocation(data)
-        # Validate the credentials (?)
-        validated_data['credentials'] = self._validate_credentials(validated_data['provider'], data)
-
-        # Using the validated data, ensure the user *should* be able to request these things.
-        self.validate_user(validated_data)
-        if not validated_data['admin_account']:
-            self.validate_account_driver(validated_data)
-        return validated_data
-
     def validate_account_driver(self, validated_data):
         try:
             provider = validated_data['provider']
@@ -120,14 +178,18 @@ class AccountSerializer(serializers.Serializer):
         except Exception as exc:
             raise serializers.ValidationError("Attempting to create an account for provider %s failed. Message: %s" % (provider, exc.message))
 
-    def validate_user(self, validated_data):
+    def validate_user(self, provider_uuid):
         request_user = self._get_request_user()
         # Tests or restrictions on request-user go here
         if request_user.is_staff or request_user.is_superuser:
             return
-        if request_user.admin_providers.filter(id=validated_data['provider'].id).count() > 0:
+        if request_user.admin_providers.filter(uuid=provider_uuid).count() > 0:
             return
         raise serializers.ValidationError("Only the Cloud Administrators can create new accounts")
+
+    ###
+    # Private creation methods
+    ###
 
     def _create_openstack_account(self, provider, credentials):
         """
@@ -149,58 +211,41 @@ class AccountSerializer(serializers.Serializer):
             raise serializers.ValidationError("Error creating the account - %s")
         return username, password, project
 
-    def create_openstack_identity(self, provider, validated_data):
-        admin_account = validated_data['admin_account']
+    def create_openstack_identity(self, user, provider, validated_data):
         create_account = validated_data['create_account']
         provider = validated_data['provider']
-        quota = validated_data['quota']
         user = validated_data['atmo_user']
         credentials_list = validated_data['credentials']
-        credentials = {c['key']: c['value'] for c in credentials_list}
+        credentials_dict = {c['key']: c['value'] for c in credentials_list}
+        has_account_provider = provider.accountprovider_set.exists()
+        # Do not create accounts without a valid AccountProvider for the Provider
+        if create_account and has_account_provider:
+            (username, password, project) = self._create_openstack_account(provider, credentials_dict)
+            credentials_dict['key'] = username
+            credentials_dict['secret'] = password
+            credentials_dict['ex_project_name'] = project.name
+        identity = self._get_or_create_identity(user, provider, validated_data['quota'])
+        for key, value in credentials_dict.items():
+            identity.credential_set.get_or_create(
+                key=key, value=value)
 
-        if create_account:
-            (username, password, project) = self._create_openstack_account(provider, credentials)
-            credentials['key'] = username
-            credentials['secret'] = password
-            credentials['ex_project_name'] = project.name
+        # allocation_source = validated_data['allocation_source']  NOTE: Uncomment when feature is completed
+        return identity
+
+    def _get_or_create_identity(self, user, provider, quota):
         try:
             identity = Identity.objects.get(
                 contains_credential('key', 'username'),
                 created_by=user,
-                provider=provider,
-                quota=quota)
+                provider=provider)
+            if identity.quota != quota:
+                identity.quota = quota
+                identity.save()
         except Identity.DoesNotExist:
             identity = Identity.objects.create(
                 created_by=user,
                 provider=provider,
                 quota=quota)
-        # Identity-container at-most-one per account (per user creating it)
-        for key, value in credentials.items():
-            identity.credential_set.get_or_create(
-                key=key, value=value)
-        return identity
-
-    def create(self, validated_data):
-        ident_kwargs = validated_data.copy()
-        admin_account = ident_kwargs.pop('admin_account')
-        group = ident_kwargs.pop('atmo_group')
-        provider = ident_kwargs['provider']
-        quota = ident_kwargs['quota']
-        provider_type = provider.get_type_name().lower()
-        if provider_type == 'openstack':
-            new_identity = self.create_openstack_identity(provider, validated_data)
-        else:
-            raise Exception("Cannot create accounts for provider of type %s" % provider_type)
-        # Add the credentials to identity post-creation
-        new_identity.share(group, quota=quota)
-        # If admin_account, create an AccountProvider
-        if admin_account:
-            AccountProvider.objects.get_or_create(
-                provider=new_identity.provider,
-                identity=new_identity)
-        #TODO: When the refactor of rtwo/get_esh_driver is complete, validate_identity should be call-able without the django model (to avoid create-then-delete)
-        validate_identity(new_identity)
-        return new_identity
 
     class Meta:
         fields = (
