d00e52e4a1235cd25b5c8f57f9dc5c7e9f56b4a3
Steve Gregory <contact@steve-gregory.com>
2016-11-15 09:06:18 -0700

    Fix typo

--- core/models/user.py
+++ core/models/user.py
@@ -237,7 +237,7 @@ def get_default_identity(username, provider=None):
         group = get_user_group(username)
         if not group or not group.current_identities.all().count():
             if settings.AUTO_CREATE_NEW_ACCOUNTS:
-                new_identities = create_new_accounts(username, provider=provider)
+                new_identities = create_new_accounts(username, selected_provider=provider)
                 if not new_identities:
                     logger.error("%s has no identities. Functionality will be severely limited." % username)
                     return None
