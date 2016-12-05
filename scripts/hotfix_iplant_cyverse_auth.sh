0e4f567f90acf40afa919a66fb0d28e1a15512cb
Steve Gregory <contact@steve-gregory.com>
2016-12-05 10:40:47 -0700

    One-off hotfix script for those with existing databases who have trouble with the migration from 'iplantauth' to 'django_cyverse_auth'

--- scripts/hotfix_iplant_cyverse_auth.sh
+++ scripts/hotfix_iplant_cyverse_auth.sh
@@ -3,4 +3,4 @@
 ATMO_DB='atmo_prod'
 #Fix the migrations in atmosphere db
 psql_command="update django_migrations set app='django_cyverse_auth' where app='iplantauth';"
-su - postgres -c "psql -d $ATMO_DB -c $psql_command"
+su - postgres -c "psql -d $ATMO_DB -c \"$psql_command\""
