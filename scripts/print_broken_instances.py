a4fdb94ea0e2ed13073c7fc9d432a844e6deaad8
Connor Osborn <cdosborn@email.arizona.edu>
2017-01-19 16:55:41 +0000

    Add argparse library

--- scripts/print_broken_instances.py
+++ scripts/print_broken_instances.py
@@ -1,31 +1,55 @@
-import django; django.setup()
+import argparse
+import sys
 
-from core.models import *
+def main():
 
-#
-# Print a list of instances stuck in deploy_error or networking, that have
-# never reached active
-#
+    description = (
+        "Print instances in deploy_error/networking that never "
+        "went active"
+    )
 
-instances = []
-for inst in Instance.objects.filter(end_date=None):
-    statuses = inst.instancestatushistory_set
-    last_status = statuses.last()
+    parser = argparse.ArgumentParser(description=description)
+    args = parser.parse_args()
 
-    # Instance is either in deploy_error or networking
-    if last_status.status.name in ["deploy_error", "networking"]:
+    # Check environment
+    try:
+        import django; django.setup()
+        from core.models import Instance
+    except:
+        print "\n".join([
+            "ERROR! This script requires a proper environment! Try:",
+            "",
+            "   export PYTHONPATH=\"/opt/dev/atmosphere:$PYTHONPATH\"",
+            "   export DJANGO_SETTINGS_MODULE='atmosphere.settings'",
+            "   . /opt/env/atmo/bin/activate"
+        ])
+        sys.exit(1)
 
-        # Instance never went active
-        if not statuses.filter(status__name="active").exists():
-            instances.append(inst)
+
+    # Filter instances
+    instances = []
+    for inst in Instance.objects.filter(end_date=None):
+        statuses = inst.instancestatushistory_set
+        last_status = statuses.last()
+
+        # Instance is either in deploy_error or networking
+        if last_status.status.name in ["deploy_error", "networking"]:
+
+            # Instance never went active
+            if not statuses.filter(status__name="active").exists():
+                instances.append(inst)
 
 
-print "UUID, PROVIDER, START_DATE, LAST_STATUS, USERNAME"
-for inst in instances:
-    uuid = inst.provider_alias
-    provider = inst.provider.location
-    start_date = str(inst.start_date)
-    last_status = inst.instancestatushistory_set.last().status.name
-    username = inst.created_by.username
+    # Print csv
+    print "UUID, PROVIDER, START_DATE, LAST_STATUS, USERNAME"
+    for inst in instances:
+        uuid = inst.provider_alias
+        provider = inst.provider.location
+        start_date = str(inst.start_date)
+        last_status = inst.instancestatushistory_set.last().status.name
+        username = inst.created_by.username
 
-    print ",".join([uuid, provider, start_date, last_status, username])
+        print ",".join([uuid, provider, start_date, last_status, username])
+
+if __name__ == "__main__":
+    main()
