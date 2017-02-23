9fa9248220e410a6f22dc9128da937448192c1fa
Steve Gregory <contact@steve-gregory.com>
2017-02-23 16:08:48 -0700

    app.log.setup() to colorize scripts log-output

--- atmosphere/celery_init.py
+++ atmosphere/celery_init.py
@@ -4,6 +4,7 @@ import os
 
 from celery import Celery
 from django.conf import settings
+
 cwd_path = os.path.dirname(os.path.dirname(__file__))
 os.environ.setdefault('PYTHONPATH', cwd_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'atmosphere.settings')
@@ -12,7 +13,7 @@ os.environ.setdefault('PYTHONOPTIMIZE', '1')  #NOTE: Required to run ansible2 + 
 app = Celery('atmosphere')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
-#app.log.setup()
+app.log.setup()
 
 @app.task(bind=True)
 def debug_task(self):
