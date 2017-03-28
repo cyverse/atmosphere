9d646ad2c4ae080e6af79e153cee0eec603a0a7f
Amit Juneja <amitj@email.arizona.edu>
2017-03-27 23:14:53 -0700

    ATMO 1775 : Added scripts to create a parallel event table and fill it with instance_allocation_changed events and threshold_met events
    ATMO 1775: Added scripts to create allocation_renewal_event for renewed events

---
+++ scripts/update_event_table.py
@@ -0,0 +1,28 @@
+import sys,os
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, root_dir)
+os.environ["DJANGO_SETTINGS_MODULE"] = "atmosphere.settings"
+
+import django; django.setup()
+from django.db.models.query import Q
+from core.models import EventTable, EventTableUpdated, AllocationSource
+
+all_events = EventTable.objects.filter(
+    Q(name="instance_allocation_source_changed") | Q(name="allocation_source_threshold_met"))
+
+#Fill EventTableUpdated with these events
+
+for event in all_events:
+    source_name = AllocationSource.objects.get(source_id=event.payload['allocation_source_id']).name
+
+    if event.name=="allocation_source_threshold_met":
+        entity_id =source_name
+        new_payload = {"allocation_source_name":source_name,"threshold":event.payload["threshold"],"actual_value":event.payload["actual_value"]}
+    else:
+        entity_id = event.entity_id
+        new_payload = {"allocation_source_name": source_name, "instance_id": event.payload["instance_id"]}
+
+    e = EventTableUpdated(
+        name=event.name, payload=new_payload, entity_id=entity_id, timestamp=event.timestamp, uuid=event.uuid)
+
+    e.save()
