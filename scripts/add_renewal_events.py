67a650f1fa64ba99bb45357080c88151a128e89a
Amit Juneja <amitj@email.arizona.edu>
2017-04-03 12:34:36 -0700

    ATMO 1775: Script to ensure backward compatibility for renewal and supplement events AND changes to update_snapshot()

--- scripts/add_renewal_events.py
+++ scripts/add_renewal_events.py
@@ -1,38 +1,43 @@
-import sys,os
+import sys
+import uuid
+
+import os
+
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, root_dir)
 os.environ["DJANGO_SETTINGS_MODULE"] = "atmosphere.settings"
 
-import django; django.setup()
-from jetstream.models import TASAPIDriver
-from django.db.models import Count
-from core.models import EventTableUpdated, AllocationSource
+import django
 
+django.setup()
+from jetstream.models import TASAPIDriver
+from core.models import EventTableUpdated
 
 # fetch values from TACC
 api = TASAPIDriver()
 allocations = api.get_all_projects()
 
-# Find duplicate allocation source names
-
-dupes = AllocationSource.objects.values('name').annotate(Count('id')).order_by().filter(id__count__gt=1)
-
-duplicate_values = AllocationSource.objects.filter(name__in=[item['name'] for item in dupes]).distinct('name').values_list('name', flat=True)
-
 for source in allocations:
-    if source['chargeCode'] in duplicate_values:
-        details = source['allocations'][-1]
+    for details in source['allocations']:
+
+        hashstring = '%s_%s' % (source['chargeCode'], details['id'])
+        hashed_uuid = uuid.uuid5(uuid.NAMESPACE_DNS, str(hashstring))
+
         payload = {
-            "start_date" : details['start'],
-            "compute_allocated" : details['computeAllocated'],
-            "allocation_source_name" : source['chargeCode'],
+            "start_date": details['start'],
+            "end_date": details['end'],
+            "compute_allocated": details['computeAllocated'],
+            "allocation_source_name": source['chargeCode'],
         }
 
-        e = EventTableUpdated(
-                name="allocation_source_renewed",
+        try:
+
+            e = EventTableUpdated(
+                uuid=hashed_uuid,
+                name="allocation_source_created_or_renewed",
                 payload=payload,
                 entity_id=payload['allocation_source_name'],
                 timestamp=payload['start_date'])
-
-        e.save()
-
+            e.save()
+        except:
+            continue
