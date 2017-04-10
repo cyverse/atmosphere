27b08793040d0d1024976344af53cbac8b240769
Amit Juneja <amitj@email.arizona.edu>
2017-04-10 02:16:55 -0700

    ATMO 1484 / ATMO 1805 : Return Compute Allowed and other Renewal Strategy fields via Renewal Strategy endpoint
    ATMO 1484 / ATMO 1806 : Automate the process of CyVerse Rules creation based on Renewal Strategy configurations

--- api/v2/serializers/details/renewal_strategy.py
+++ api/v2/serializers/details/renewal_strategy.py
@@ -1,7 +1,17 @@
 from core.models.renewal_strategy import RenewalStrategy
 from rest_framework import serializers
+from cyverse_allocation.cyverse_rules_engine_setup import renewal_strategies
 
 class RenewalStrategySerializer(serializers.HyperlinkedModelSerializer):
+
+    compute_allowed = serializers.SerializerMethodField()
+    renewed_after_days = serializers.SerializerMethodField()
+
+    def get_compute_allowed(self,strategy):
+        return renewal_strategies[strategy]['compute_allowed']
+
+    def get_renewed_after_days(self,strategy):
+        return renewal_strategies[strategy]['renewed_after_days']
 
     class Meta:
         model = RenewalStrategy
@@ -9,4 +19,6 @@ class RenewalStrategySerializer(serializers.HyperlinkedModelSerializer):
             'id',
             'name',
             'description',
+            'compute_allowed',
+            'renewed_after_days'
         )
--- cyverse_allocation/cyverse_rules_engine_setup.py
+++ cyverse_allocation/cyverse_rules_engine_setup.py
@@ -1,7 +1,7 @@
-from business_rules.variables import BaseVariables, boolean_rule_variable, numeric_rule_variable, string_rule_variable
 from business_rules.actions import BaseActions, rule_action
 from business_rules.fields import FIELD_NUMERIC
-from dateutil.parser import parse
+from business_rules.variables import BaseVariables, boolean_rule_variable, numeric_rule_variable, string_rule_variable
+
 from core.models.allocation_source import AllocationSource,AllocationSourceSnapshot
 from core.models.event_table import EventTable
 
@@ -20,6 +20,7 @@ class CyverseTestRenewalVariables(BaseVariables):
         if (not self.allocation_source.end_date or (self.allocation_source.end_date > self.current_time)):
             return True
         return False
+
     #
     # @boolean_rule_variable
     # def is_at_full_capacity(self):
@@ -32,27 +33,29 @@ class CyverseTestRenewalVariables(BaseVariables):
 
         source_id = self.allocation_source.uuid
         last_renewal_event = EventTable.objects.filter(
-             name='allocation_source_renewed',
-             payload__source_id__exact=str(source_id)).order_by('timestamp')
+            name='allocation_source_renewed',
+            payload__source_id__exact=str(source_id)).order_by('timestamp')
         if not last_renewal_event:
-            #remove microseconds for an approximate difference, otherwise a difference of 0.999999 will also be counted as 0 days
-            return (self.current_time.replace(microsecond=0) - self.allocation_source.start_date.replace(microsecond=0)).days
+            # remove microseconds for an approximate difference, otherwise a difference of 0.999999 will also be counted as 0 days
+            return (
+                self.current_time.replace(microsecond=0) - self.allocation_source.start_date.replace(
+                    microsecond=0)).days
         return (last_renewal_event.last().timestamp - self.current_time).days
 
 
-
 class CyverseTestRenewalActions(BaseActions):
-    def __init__(self, allocation_source,current_time):
+    def __init__(self, allocation_source, current_time):
         if not isinstance(allocation_source, AllocationSource):
             raise Exception('Please provide Allocation Source instance for renewal')
         self.allocation_source = allocation_source
         self.current_time = current_time
 
     @rule_action(params={"compute_allowed": FIELD_NUMERIC})
-    def renew_allocation_source(self,compute_allowed):
+    def renew_allocation_source(self, compute_allowed):
         source_snapshot = AllocationSourceSnapshot.objects.filter(allocation_source=self.allocation_source)
         if not source_snapshot:
-            raise Exception('Allocation Source %s cannot be renewed because no snapshot is available'%(self.allocation_source.name))
+            raise Exception('Allocation Source %s cannot be renewed because no snapshot is available' % (
+                self.allocation_source.name))
         source_snapshot = source_snapshot.last()
         remaining_compute = 0 if source_snapshot.compute_allowed - source_snapshot.compute_used < 0 else source_snapshot.compute_allowed - source_snapshot.compute_used
         source_snapshot.compute_used = 0
@@ -66,9 +69,9 @@ class CyverseTestRenewalActions(BaseActions):
         source_id = self.allocation_source.uuid
 
         payload = {
-            "source_id" : str(source_id),
-            "name" : self.allocation_source.name,
-            "compute_allowed" : total_compute_allowed
+            "source_id": str(source_id),
+            "name": self.allocation_source.name,
+            "compute_allowed": total_compute_allowed
         }
 
         EventTable.objects.create(name='allocation_source_renewed',
@@ -80,62 +83,72 @@ class CyverseTestRenewalActions(BaseActions):
     def cannot_renew_allocation_source(self):
         pass
 
-cyverse_rules = [
-    #if strategy_name == 'default' AND isValid(allocation_source) AND days_since_renewed >= 30 THEN renew
-    {"conditions": {"all": [
-        {"name": "renewal_strategy",
-         "operator": "equal_to",
-         "value": 'default',
-         },
-        {"name": "is_valid",
-         "operator": "is_true",
-         "value": True
-         },
-        {"name": "days_since_renewed",
-         "operator": "greater_than_or_equal_to",
-         "value": 3,
-         },
-    ]},
-        "actions": [
-            {"name": "renew_allocation_source",
-             "params": {"compute_allowed": 250}
-             },
-        ],
-    },
 
-    #if strategy_name == 'workshop' THEN cannot renew
-    {"conditions": {"all": [
-        {"name": "renewal_strategy",
-         "operator": "equal_to",
-         "value": 'workshop',
-         },
-    ]},
-        "actions": [
-            {"name": "cannot_renew_allocation_source",
-             },
-        ],
-    },
+def parse_cyverse_rules(renewal_strategies):
+    cyverse_rules = []
+    for strategy, config in renewal_strategies.iteritems():
+        rule = {}
+        name, compute_allowed, renewed_in_days = strategy, \
+                                                 config['compute_allowed'], \
+                                                 config['renewed_in_days']
+        conditions = _create_conditions_for(name, renewed_in_days)
+        actions = _create_actions_for(compute_allowed, renewed_in_days)
+        rule['conditions'] = {"all": conditions}
+        rule['actions'] = actions
+        cyverse_rules.append(rule)
 
-    #if strategy_name == 'bi-weekly' AND isValid(allocation_source) AND days_since_renewed >= 14 THEN renew
-    {"conditions": {"all": [
-        {"name": "renewal_strategy",
-         "operator": "equal_to",
-         "value": 'bi-weekly',
-         },
-        {"name": "is_valid",
-         "operator": "is_true",
-         "value": True
-         },
-        {"name": "days_since_renewed",
-         "operator": "greater_than_or_equal_to",
-         "value": 14,
-         },
-    ]},
-        "actions": [
-            {"name": "renew_allocation_source",
-             "params": {"compute_allowed": 150}
-             },
-        ],
-    },
+    return cyverse_rules
 
-]
+
+def _create_conditions_for(name, renewed_in_days):
+    conditions = []
+
+    # condition 1
+    conditions.append({"name": "renewal_strategy",
+                       "operator": "equal_to",
+                       "value": name})
+
+    # condition 2
+    conditions.append({"name": "is_valid",
+                       "operator": "is_true",
+                       "value": True})
+
+    # condition 3 if strategy is renewable
+    if renewed_in_days > 0:
+        conditions.append({"name": "days_since_renewed",
+                           "operator": "greater_than_or_equal_to",
+                           "value": renewed_in_days})
+
+    return conditions
+
+
+def _create_actions_for(compute_allowed, renewed_in_days):
+    actions = []
+
+    if renewed_in_days > 0:
+        actions.append({"name": "renew_allocation_source",
+                        "params": {"compute_allowed": compute_allowed}
+                        })
+
+    else:
+        actions.append({"name": "cannot_renew_allocation_source"})
+
+    return actions
+
+
+# RENEWAL STRATEGY CONFIGURATION
+renewal_strategies = {
+
+    'default': {'compute_allowed': 250,
+                'renewed_in_days': 3},
+
+    'bi-weekly': {'compute_allowed': 150,
+                  'renewed_in_days': 14},
+
+    'workshop': {'compute_allowed': 100,
+                 'renewed_in_days': 0}
+
+}
+
+# MAIN RULES JSON
+cyverse_rules = parse_cyverse_rules(renewal_strategies)
