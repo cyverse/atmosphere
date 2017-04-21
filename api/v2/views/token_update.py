367c9887e4ae52054e16533c63e6400dbb8dd47f
Steve Gregory <contact@steve-gregory.com>
2017-04-21 13:40:51 -0700

    Fix broken test cases and resolve #337

--- api/v2/views/token_update.py
+++ api/v2/views/token_update.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import AnonymousUser
 from core.models import Identity
 
 from api.v2.serializers.post import TokenUpdateSerializer
-from api.v2.views.base import AuthViewSet
+from api.v2.views.base import AuthModelViewSet
 
 
-class TokenUpdateViewSet(AuthViewSet):
+class TokenUpdateViewSet(AuthModelViewSet):
     """
     API endpoint that allows providers to be viewed or edited.
     """
