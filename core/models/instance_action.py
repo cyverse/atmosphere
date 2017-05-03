56be68196cb53a9e81ac1a354da55191e8682672
Andrew J Lenards <andrew.lenards@gmail.com>
2017-05-03 15:24:45 +0000

    Include "Image" as an action for shutoff instances
    In the current documentation for Jetstream, authoring an Image while an
    instance is in Active or Stopped state is _encouraged_ [0]. However, we
    currently only offer "Start" along with *basic* actions for a shutoff
    instance [1].
    
    This commit includes the "Image" action, the -ing name for the action is
    used, "Imaging", because that is the `key` attribute for the model, even
    though the action is referred to in the user interface as "Image". A fix
    for that would require Troposphere to be modified now, as well (NOTE)
    
    [0] https://iujetstream.atlassian.net/wiki/display/JWT/Customizing+and+saving+a+VM
    [1] https://iujetstream.atlassian.net/browse/JETSTREAM-149
    
    (cherry picked from commit af9737c3adcc816d8207588759e41e76ffe6eaa1)
    
    Conflicts:
    	core/models/instance_action.py

--- core/models/instance_action.py
+++ core/models/instance_action.py
@@ -134,10 +134,11 @@ class InstanceAction(models.Model):
             all_actions.append('Reboot')
             all_actions.append('Hard Reboot')
         elif last_status == "shutoff":
-            # Suspended instances can be started + <Basic Actions>
+            # Stopped instances can be started, imaged + <Basic Actions>
             all_actions.append('Reboot')
             all_actions.append('Hard Reboot')
             all_actions.append('Start')
+            all_actions.append('Imaging')
         elif last_status == "shelved":
             # Shelved instances can be unshelved, offloaded, or terminated
             if not last_activity:
