d52f0d51418ed4f692acee560801dbeab072abe7
Julian Pistorius <julian@julianpistorius.com>
2017-05-16 14:57:29 -0700

    Problem: DeviceBusyException method references uninitialized attribute
    This caused an exception when printing the exception. Very meta.
    
    Solution: Set the missing attribute (`process_list`)
    
    Fixes #360

---
+++ service/tests/test_exceptions.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+import service.exceptions
+
+
+class ExceptionTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_device_busy_exception(self):
+        mount_point = '/vol1'
+        process_list = [('/bin/bash', 1067), ('/bin/vim', 2129)]
+
+        new_exception = service.exceptions.DeviceBusyException(mount_point, process_list)
+        expected_message = 'Volume mount location is: /vol1\nRunning processes that are accessing that directory ' \
+                           'must be closed before unmounting. All offending processes names and IDs are listed ' \
+                           'below:\nProcess name:/bin/bash process id:1067\nProcess name:/bin/vim process id:2129'
+        self.assertEqual(new_exception.message, expected_message)
+
+        expected_str = "Volume mount location is: /vol1\nRunning processes that are accessing that directory must be " \
+                       "closed before unmounting. All offending processes names and IDs are listed below:\n" \
+                       "Process name:/bin/bash process id:1067\nProcess name:/bin/vim process id:2129:\n" \
+                       "[('/bin/bash', 1067), ('/bin/vim', 2129)]"
+        self.assertEqual(str(new_exception), expected_str)
