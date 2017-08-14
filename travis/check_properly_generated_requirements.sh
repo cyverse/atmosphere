5ea1f8e4520c1fb35fe12bcd047a9f382ed81acd
Connor Osborn <cdosborn@email.arizona.edu>
2017-08-14 19:43:50 +0000

    Version the requirements script for greater reproducibility

--- travis/check_properly_generated_requirements.sh
+++ travis/check_properly_generated_requirements.sh
@@ -9,11 +9,24 @@
 #
 # Notes:
 # This script properly generates the requirements and performs a diff with the
-# current requirements. There is some noise but thats basically it. This
-# script tries to be pretty quiet, if you're debugging you should remove the
-# silencing file redirections.
+# current requirements.
+
+PIP_TOOLS_VERSION=1.9.0
 
 function main {
+
+    # Ensure that pip-tools is installed
+    which pip-compile &>/dev/null;
+    if [[ $? -ne 0 ]]; then
+        warn_missing_pip_tools;
+        exit 1;
+    fi;
+
+    # Ensure the proper version of pip-tools
+    if [[ $(pip-tools-version) != $PIP_TOOLS_VERSION ]]; then
+        warn_improper_pip_tools_version;
+        exit 1;
+    fi;
 
     # Ensure that requirements.txt was generated properly
     diff <(generate_requirements | normalize) <(normalize < requirements.txt);
@@ -46,11 +59,22 @@ function generate_requirements {
     pip-compile --dry-run -o requirements.txt requirements.in 2>/dev/null;
 }
 
+function pip-tools-version {
+    pip-compile --version | grep -Po '\d\.\d\.\d';
+}
+
+function warn_improper_pip_tools_version {
+    warn "pip-tools is the wrong version." >&2;
+    echo "To install run: pip install pip-tools==$PIP_TOOLS_VERSION" >&2;
+}
+
+function warn_missing_pip_tools {
+    warn "pip-tools is missing." >&2;
+    echo "To install run: pip install pip-tools==$PIP_TOOLS_VERSION" >&2;
+}
+
 function warn_bad_requirements {
-    local default="\e[0m" red="\e[31m";
-    printf "${red}";
-    echo "requirements.txt was not generated properly!" >&2;
-    printf "${default}";
+    warn "requirements.txt was not generated properly!" >&2;
     echo "This likely means the author didn't edit the requirements.in file!" >&2;
     echo "See REQUIREMENTS.md" >&2;
 }
@@ -61,20 +85,14 @@ function generate_dev_requirements {
 }
 
 function warn_bad_dev_requirements {
-    local default="\e[0m" red="\e[31m";
-    printf "${red}";
-    echo "dev_requirements.txt was not generated properly!" >&2;
-    printf "${default}";
+    warn "dev_requirements.txt was not generated properly!" >&2;
     echo "This file must be generated after requirements.txt is generated," \
          "because it uses that file as an input." >&2;
     echo "See REQUIREMENTS.md" >&2;
 }
 
 function warn_bad_dev_requirements {
-    local default="\e[0m" red="\e[31m";
-    printf "${red}";
-    echo "dev_requirements.txt was not generated properly!" >&2;
-    printf "${default}";
+    warn "dev_requirements.txt was not generated properly!" >&2;
     echo "This file must be generated after requirements.txt is generated," \
          "because it uses that file as an input." >&2;
     echo "See REQUIREMENTS.md" >&2;
@@ -84,6 +102,13 @@ function echo_success_message {
     local default="\e[0m" green="\e[32m";
     printf "${green}";
     echo "requirements.txt and dev_requirements.txt passed the test!" >&2;
+    printf "${default}";
+}
+
+function warn {
+    local default="\e[0m" red="\e[31m";
+    printf "${red}";
+    echo "$@";
     printf "${default}";
 }
 
