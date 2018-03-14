28225eca25c219b3e60a92e8d87e63e3ff29461f
Connor Osborn <cdosborn@email.arizona.edu>
2018-03-14 11:51:22 -0700

    Change ./manage.py maintenance to be easier to test
    I was making changes to git_branch, and found that this command would be
    affected. I wanted to write some tests to ensure that the command would still
    work. However, the previous command would not allow you to pass some arguments and not
    others. If you omitted any, it would go into an interactive mode. I couldn't
    test whether the defaults were working, because of this mode.
    
    I solved this by adding a flag for --dry-run and removing the interactive
    mode.
    
    Other changes:
    The start_date format accepts timezones now.
    The --start-now flag was removed, this is the default if no --start-date is
    specified.
    The show command will print a current maintenance if one exists

---
+++ core/management/commands/test_maintenance.py
@@ -0,0 +1,50 @@
+import StringIO
+from django.core.management import call_command
+from django.test import TestCase
+import datetime
+import pytz
+
+from core.management.commands import maintenance
+from core.models import MaintenanceRecord
+
+class MaintenanceTest(TestCase):
+    def test_start(self):
+        records_before = set(MaintenanceRecord.objects.all())
+        call_command(
+            maintenance.Command(),
+            "start",
+            title="title",
+            message="message",
+            start_date="2038-01-19T03:15:00Z")
+        records_after = set(MaintenanceRecord.objects.all())
+
+        created_records = records_after - records_before
+        self.assertEqual(len(created_records), 1, "One record was created")
+
+        record = next(iter(created_records))
+        self.assertEqual(record.title, "title")
+        self.assertEqual(record.message, "message")
+        self.assertEqual(record.start_date,
+            datetime.datetime(2038, 1, 19, 3, 15, tzinfo=pytz.utc))
+        self.assertIsNone(record.end_date)
+
+    def test_start_dry_run(self):
+        """
+        The start command with --dry-run shouldn't create a record
+        """
+        num_records_before = MaintenanceRecord.objects.count()
+        call_command(maintenance.Command(), "start", "--dry-run")
+        num_records_after = MaintenanceRecord.objects.count()
+        self.assertEqual(
+            num_records_before, num_records_after,
+            "The number of records before and after shouldn't change")
+
+    def test_stop(self):
+        records_before = set(MaintenanceRecord.objects.all())
+        call_command(maintenance.Command(), "start")
+        records_after = set(MaintenanceRecord.objects.all())
+        created_record = next(iter(records_after - records_before))
+
+        call_command(maintenance.Command(), "stop")
+        updated_record = MaintenanceRecord.objects.get(id=created_record.id)
+        self.assertIsNotNone(updated_record.end_date, "Record has an enddate")
