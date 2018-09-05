76b09a09c5f53796481c981559e7ac26d8a7a158
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-05 16:16:38 -0700

    Remove unused class

--- api/v2/serializers/fields/__init__.py
+++ api/v2/serializers/fields/__init__.py
@@ -1,11 +1,10 @@
 from .image_version import ImageVersionRelatedField
 from .provider_machine import ProviderMachineRelatedField
-from .user import UserRelatedField
 from .tag import TagRelatedField
 from .base import ModelRelatedField, filter_current_user_queryset
 from .identity import IdentityRelatedField
 from .status_type import StatusTypeRelatedField
 
 __all__ = ("ImageVersionRelatedField", "ProviderMachineRelatedField",
-           "UserRelatedField", "ModelRelatedField", "TagRelatedField",
-           "IdentityRelatedField", "StatusTypeRelatedField", "filter_current_user_queryset")
+           "ModelRelatedField", "TagRelatedField", "IdentityRelatedField",
+           "StatusTypeRelatedField", "filter_current_user_queryset")
