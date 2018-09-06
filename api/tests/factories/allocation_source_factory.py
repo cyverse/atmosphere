233617077064c28024f7086a07aa9bc39d03f5ea
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-06 11:10:35 -0700

    Make creating UserAllocationSources in tests a bit friendlier

--- api/tests/factories/allocation_source_factory.py
+++ api/tests/factories/allocation_source_factory.py
@@ -1,13 +1,23 @@
+from django.utils import timezone
 import factory
+from api.tests.factories.user_factory import UserFactory
 
 from core.models import AllocationSource, UserAllocationSource
 
 
 class AllocationSourceFactory(factory.DjangoModelFactory):
+    name = factory.fuzzy.FuzzyText()
+    compute_allowed = 168
+    start_date = factory.fuzzy.FuzzyDateTime(timezone.now())
+    end_date = None
+    renewal_strategy = "default"
+
     class Meta:
         model = AllocationSource
 
 
 class UserAllocationSourceFactory(factory.DjangoModelFactory):
+    allocation_source = factory.SubFactory(AllocationSourceFactory)
+    user = factory.SubFactory(UserFactory)
     class Meta:
         model = UserAllocationSource
