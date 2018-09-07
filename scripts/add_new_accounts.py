1c85ff0da9b9b7238fa481823809d9e81b0c2989
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 15:19:05 -0700

    Fix [F401(pyflakes), None] 'core.plugins.ValidationPluginManager' imported but unused

--- scripts/add_new_accounts.py
+++ scripts/add_new_accounts.py
@@ -12,7 +12,7 @@ django.setup()
 
 from core.models import AtmosphereUser as User
 from core.models import Provider, Identity
-from core.plugins import ValidationPluginManager, ExpirationPluginManager, AccountCreationPluginManager
+from core.plugins import ExpirationPluginManager, AccountCreationPluginManager
 
 from django_cyverse_auth.protocol.ldap import get_members
 from service.driver import get_account_driver
