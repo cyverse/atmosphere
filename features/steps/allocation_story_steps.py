e5f819b077f6c93c88284567bf0118bd37f9fba1
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 16:01:02 -0700

    Fix [E501(pep8), None] line too long (169 > 159 characters)

--- features/steps/allocation_story_steps.py
+++ features/steps/allocation_story_steps.py
@@ -254,67 +254,6 @@ def one_off_renewal_task_is_run_without_rules_engine(context):
         assert float(allocation_source.snapshot.compute_allowed) == float(row['current compute allowed'])
 
 
-# @when('Users added to allocation source launch instance (at the same time)')
-# def step_impl(context):
-#     user = [context.user_1,context.user_2]
-#     count = 0
-#     time_created = timezone.now()
-#     context.time_user_launched_instance = time_created
-#     for row in context.table:
-#         # add user to allocation source
-#         context.client.post('/api/v2/user_allocation_sources',
-#                             {"username": user[count].username,
-#                              "source_id": context.uuid})
-#         provider_alias = launch_instance(user[count],time_created,int(row["cpu"]))
-#         # Add instance to allocation source
-#         context.client.post('/api/v2/instance_allocation_source',
-#                             {"instance_id": provider_alias,
-#                              "source_id": context.uuid
-#                              })
-#         time_stopped = time_created + timedelta(days=int(row["days_instance_is_active"]))
-#         stop_instance(user,provider_alias,time_stopped)
-#         count+=1
-#
-#
-# @then('after days = {no_of_days} Allocation source used = {compute_used} and remaining compute = {compute_remaining}')
-# def step_impl(context, no_of_days, compute_used, compute_remaining):
-#     #find how often renewal is fired for this strategy
-#     current_time = timezone.now()
-#     allocation_source = AllocationSource.objects.filter(uuid=context.uuid).last()
-#     start_date = current_time
-#     end_date = current_time+timedelta(days=int(no_of_days))
-#     celery_iterator = list(rrule(HOURLY, interval=12, dtstart=start_date, until=end_date))
-#
-#     prev_time = ''
-#
-#     for current_time in celery_iterator:
-#         # update AllocationSourceSnapshot with the current compute_used
-#         if not prev_time:
-#             prev_time = current_time
-#         else:
-#             snapshot = AllocationSourceSnapshot.objects.filter(allocation_source=allocation_source).last()
-#             snapshot.compute_used = float(snapshot.compute_used) + get_compute_used(allocation_source,current_time,prev_time)
-#             snapshot.save()
-#
-#             run_all(rule_list=cyverse_rules,
-#                     defined_variables=CyverseTestRenewalVariables(allocation_source, current_time),
-#                     defined_actions=CyverseTestRenewalActions(allocation_source, current_time),
-#                     )
-#             prev_time=current_time
-#
-#     #find compute_used
-#     compute_used_total = 0
-#     for user in allocation_source.all_users:
-#         compute_used_total += total_usage(user.username,start_date=context.time_user_launched_instance,end_date=end_date,allocation_source_name=allocation_source.name)
-#     #find compute_remaining
-#     compute_allowed = AllocationSourceSnapshot.objects.filter(allocation_source=allocation_source).last().compute_allowed
-#     compute_used_from_snapshot = AllocationSourceSnapshot.objects.filter(
-#         allocation_source=allocation_source).last().compute_used
-#     assert int(compute_used) == compute_used_total
-#     assert (float(compute_allowed)-float(compute_used_from_snapshot))==float(compute_remaining)
-
-########### Helpers ###############
-
 def launch_instance(user, time_created, cpu, before=False):
     # context.user is admin and regular user
     provider = ProviderFactory.create()
