14d478f7e0361bc0c2d79074c929adcd76139ab9
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 13:11:12 -0700

    Fix [F401(pyflakes), None] 'rest_framework.response.Response' imported but unused

--- api/v2/views/machine_request.py
+++ api/v2/views/machine_request.py
@@ -1,5 +1,4 @@
 from rest_framework import exceptions as rest_exceptions
-from rest_framework.response import Response
 from api.v2.serializers.details import MachineRequestSerializer,\
     UserMachineRequestSerializer
 from api.v2.views.base import BaseRequestViewSet
