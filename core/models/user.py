d1bcf9f1408c289b0fa65878b514354c5ee8d25a
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 14:44:56 -0700

    Fix [F401(pyflakes), None] 'core.exceptions.InvalidUser' imported but unused

--- core/models/user.py
+++ core/models/user.py
@@ -10,7 +10,6 @@ from django.db.models.signals import post_save
 from django.utils import timezone
 from core.plugins import ValidationPluginManager, ExpirationPluginManager, DefaultQuotaPluginManager, AccountCreationPluginManager
 from core.query import only_current
-from core.exceptions import InvalidUser
 from threepio import logger
 from django.utils.translation import ugettext_lazy as _
 
