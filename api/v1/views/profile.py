3676e2b1a3916743161922131484f474c7a0282f
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 10:46:11 -0700

    Fix [F401(pyflakes), None] django.utils.timezone imported but unused

--- api/v1/views/profile.py
+++ api/v1/views/profile.py
@@ -5,7 +5,6 @@ Atmosphere service instance rest api.
 from rest_framework.response import Response
 from rest_framework import status
 from django.conf import settings
-from django.utils import timezone
 
 from core.exceptions import InvalidUser
 from api.v1.views.base import AuthAPIView
