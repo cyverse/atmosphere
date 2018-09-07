26ba1aec06f58090fc445a227f6a78c9ff70d1ff
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-07 13:09:12 -0700

    Fix [F401(pyflakes), None] 'core.models.Group' imported but unused

--- api/v2/views/identity_membership.py
+++ api/v2/views/identity_membership.py
@@ -2,7 +2,7 @@ from rest_framework import filters
 import django_filters
 
 from core.models.cloud_admin import admin_provider_list
-from core.models import IdentityMembership, Group
+from core.models import IdentityMembership
 from core.query import only_active_provider_memberships
 
 from api.v2.serializers.details import IdentityMembershipSerializer
