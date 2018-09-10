73e85c1d5c66199464fcb6e4e350044f63e83a17
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-10 09:26:45 -0700

    Fix [F401(pyflakes), None] 'api.tests.factories.GroupFactory' imported but unused

--- api/tests/v2/test_instances.py
+++ api/tests/v2/test_instances.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 
 from rest_framework.test import APITestCase
 from api.tests.factories import (
-    GroupFactory, UserFactory, AnonymousUserFactory, InstanceFactory,
+    UserFactory, AnonymousUserFactory, InstanceFactory,
     InstanceHistoryFactory, InstanceStatusFactory, ImageFactory,
     ApplicationVersionFactory, InstanceSourceFactory, ProviderMachineFactory,
     IdentityFactory, ProviderFactory)
