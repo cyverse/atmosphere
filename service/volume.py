2a43c77257ac066da18860b4c932f56e397d6525
Connor Osborn <cdosborn@email.arizona.edu>
2018-09-10 09:49:57 -0700

    Fix [F401(pyflakes), None] 'core.models.quota.has_storage_quota' imported but unused

--- service/volume.py
+++ service/volume.py
@@ -1,8 +1,7 @@
 from threepio import logger
 
 from django.core.exceptions import ValidationError
-from core.models.quota import get_quota, has_storage_count_quota,\
-    has_storage_quota
+from core.models.quota import get_quota, has_storage_count_quota
 from core.models.identity import Identity
 from core.models.volume import Volume
 from core.models.instance_source import InstanceSource
