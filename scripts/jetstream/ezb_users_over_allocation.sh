3f00e795e860f6b53371306b11232ea2ad714266
Will Simpson <wsimpson@email.arizona.edu>
2019-09-13 11:22:05 -0700

    Add Jetstream specific scripts

---
+++ scripts/jetstream/ezb_users_over_allocation.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "# EZ Button users over threshold (2K SU)"
+echo ""
+
+WEEK_AGO=$(date --date='a week ago' +%F)
+. /opt/env/atmo/bin/activate
+cd /opt/dev/atmosphere
+cat scripts/ezb_users_over_allocation.sql | sed "s/2017-12-01/${WEEK_AGO}/" | ./manage.py dbshell
