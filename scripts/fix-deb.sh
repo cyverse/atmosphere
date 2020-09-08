2784982526c24762b0dba128d671f2cd3e621366
Edwin Skidmore <edwin@cyverse.org>
2020-09-08 14:14:49 -0700

    check to ensure .deb file isn't 0 bytes

--- scripts/fix-deb.sh
+++ scripts/fix-deb.sh
@@ -3,7 +3,7 @@
 deb_url='https://github.com/cyverse/atmosphere-ansible/raw/master/ansible/roles/atmo-vnc/files/VNC-Server-5.2.3-Linux-x64.deb'
 deb_path='/opt/dev/atmosphere-ansible/ansible/roles/atmo-vnc/files/VNC-Server-5.2.3-Linux-x64.deb'
 
-if [ ! -f "$deb_path" ]; then
+if [ ! -f "$deb_path" -o ! -s "$deb_path" ]; then
 	echo "$(date): '$deb_path' does not exist"
 	wget --quiet $deb_url -O $deb_path
 
